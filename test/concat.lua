a8d7e9533250c20cd7c6e387579d515c8ed78175
chgibb <chris.gibb@outlook.com>
2020-02-02 21:48:54 +0000

    wip port concat test

---
+++ test/concat.lua
@@ -0,0 +1,6 @@
+local x = "potato"
+local y = "walrus"
+local z = "fizz"
+local t = "buzz"
+
+print(x .. y .. z .. t)
---
+++ test/concat_test.dart
@@ -0,0 +1,27 @@
+import 'package:flua/src/5_2/context.dart';
+import 'package:flua/src/5_2/state.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  test('', () async {
+    var state = LuaState();
+
+    List<String> outLines = [];
+
+    state.context.env["print"] = (List<dynamic> args) {
+      outLines
+          .add(args.map((a) => Context.luaToString(a).toString()).join("\t"));
+    };
+
+    List<String> expectedOutLines = ["potatowalrusfizzbuzz"];
+
+    var res = await state.doFile("concat.lc");
+
+    expect(res.success, true);
+    expect(outLines.length, expectedOutLines.length);
+
+    for (var i = 0; i != outLines.length; ++i) {
+      expect(outLines[i], expectedOutLines[i]);
+    }
+  });
+}
