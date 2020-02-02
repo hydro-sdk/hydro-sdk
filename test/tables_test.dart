b533ef2d247ece6630c40ba85e588415e9db80a5
chgibb <chris.gibb@outlook.com>
2020-02-02 22:00:21 +0000

    wip port tables test

---
+++ test/tables_test.dart
@@ -0,0 +1,34 @@
+import 'package:flua/src/5_2/context.dart';
+import 'package:flua/src/5_2/state.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  test('', () async {
+    var state = LuaState();
+
+    List<String> outLines = [];
+
+    state.context.env["print"] = (List<dynamic> args) {
+      outLines
+          .add(args.map((a) => Context.luaToString(a).toString()).join("\t"));
+    };
+
+    List<String> expectedOutLines = [
+      "1	12	123	1234	12345",
+      "42",
+      "420",
+      "nil",
+      "420",
+      "69",
+    ];
+
+    var res = await state.doFile("tables.lc");
+
+    expect(res.success, true);
+    expect(outLines.length, expectedOutLines.length);
+
+    for (var i = 0; i != outLines.length; ++i) {
+      expect(outLines[i], expectedOutLines[i]);
+    }
+  });
+}
