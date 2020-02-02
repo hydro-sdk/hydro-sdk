65b01da6416cf738709d1a2be7fb15a22150fe64
chgibb <chris.gibb@outlook.com>
2020-02-02 21:57:12 +0000

    wip port sha256 test

---
+++ test/sha256_test.dart
@@ -0,0 +1,29 @@
+import 'package:flua/src/5_2/context.dart';
+import 'package:flua/src/5_2/state.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  test('', () async {
+    var state = LuaState();
+
+    List<String> outLines = [];
+
+    state.context.env["print"] = (List<dynamic> args) {
+      outLines
+          .add(args.map((a) => Context.luaToString(a).toString()).join("\t"));
+    };
+
+    List<String> expectedOutLines = [
+      "e91c254ad58860a02c788dfb5c1a65d6a8846ab1dc649631c7db16fef4af2dec"
+    ];
+
+    var res = await state.doFile("sha256.lc");
+
+    expect(res.success, true);
+    expect(outLines.length, expectedOutLines.length);
+
+    for (var i = 0; i != outLines.length; ++i) {
+      expect(outLines[i], expectedOutLines[i]);
+    }
+  });
+}
