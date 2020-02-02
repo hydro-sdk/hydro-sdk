3fe8423fc03c1160b88bf8c41e27c3044ebd19b4
chgibb <chris.gibb@outlook.com>
2020-02-02 21:53:16 +0000

    wip port pairs test

---
+++ test/pairs.lua
@@ -0,0 +1,9 @@
+local x = {
+    1, 2, 3, 4,
+}
+
+for k, v in pairs(x) do
+    print(k, v)
+end
+
+print("done")
---
+++ test/pairs_test.dart
@@ -0,0 +1,33 @@
+import 'package:flua/src/5_2/context.dart';
+import 'package:flua/src/5_2/state.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  test('', () async {
+    var state = LuaState();
+
+    List<String> outLines = [];
+
+    state.context.env["print"] = (List<dynamic> args) {
+      outLines
+          .add(args.map((a) => Context.luaToString(a).toString()).join("\t"));
+    };
+
+    List<String> expectedOutLines = [
+      "1	1",
+      "2	2",
+      "3	3",
+      "4	4",
+      "done",
+    ];
+
+    var res = await state.doFile("pairs.lc");
+
+    expect(res.success, true);
+    expect(outLines.length, expectedOutLines.length);
+
+    for (var i = 0; i != outLines.length; ++i) {
+      expect(outLines[i], expectedOutLines[i]);
+    }
+  });
+}
