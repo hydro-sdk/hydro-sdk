470eefdb0646e679df725ccef0049a8b20208926
chgibb <chris.gibb@outlook.com>
2020-02-05 23:09:27 +0000

    implement more of bit32

--- lib/src/5_2/lualib/bit32.dart
+++ lib/src/5_2/lualib/bit32.dart
@@ -1,20 +1,45 @@
 import 'package:flua/src/5_2/context.dart';
 import 'package:flua/src/5_2/table.dart';
 
+//https://www.lua.org/manual/5.2/manual.html#6.7
 void loadBit32(Context ctx) {
   var bit32 = Table();
 
-  bit32["band"] = (List<dynamic> args) {
+  bit32["band"] = makeLuaDartFunc(func: (List<dynamic> args) {
     int r = ~0;
     for (var i = 0; i != args.length; ++i) {
       r &= args[i];
     }
     return [r];
-  };
+  });
 
-  bit32["bnot"] = (List<dynamic> args) {
+  bit32["bnot"] = makeLuaDartFunc(func: (List<dynamic> args) {
     return [~args[0]];
-  };
+  });
+
+  bit32["bor"] = makeLuaDartFunc(func: (List<dynamic> args) {
+    int r = 0;
+    for (var i = 0; i != args.length; ++i) {
+      r |= args[i];
+    }
+    return [r];
+  });
+
+  bit32["btest"] = makeLuaDartFunc(func: (List<dynamic> args) {
+    int r = 0;
+    for (var i = 0; i != args.length; ++i) {
+      r &= args[i];
+    }
+    return [r == 0];
+  });
+
+  bit32["bxor"] = makeLuaDartFunc(func: (List<dynamic> args) {
+    int r = 0;
+    for (var i = 0; i != args.length; ++i) {
+      r ^= args[i];
+    }
+    return [r];
+  });
 
   ctx.env["bit32"] = bit32;
 }
