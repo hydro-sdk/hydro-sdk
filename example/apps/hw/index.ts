cb5e6fbe64e0cf4326efaa5f59010906a545d22e
chgibb <chris.gibb@outlook.com>
2020-02-14 18:25:50 -0500

    wip

--- example/apps/hw/index.ts
+++ example/apps/hw/index.ts
@@ -1,52 +1,41 @@
 import { Scaffold, AppBar, Text, Widget } from 'flutter/index';
 import { FlatButton } from "flutter/material/flatButton";
 import { Center } from "flutter/widgets/index";
+import { console } from 'ts/console';
 
 declare let buildResult: Widget;
 
-declare class StatelessWidget implements Widget {
+abstract class StatelessWidget implements Widget {
     tag: string;
-    public build(): Widget;
-    public constructor();
+    public abstract build(): Widget;
+    public constructor() {
+        this.tag = "";
+    }
+}
+
+class MyWidget extends StatelessWidget {
+    public label: string;
+    public constructor(label: string) {
+        super();
+
+        this.label = label;
+    }
+
+    public build(): Widget {
+        return ((): Widget => {
+            return FlatButton({
+                child: Text(`Hello from ${this.label}`),
+                onPressed: () => { print(`Hello from ${this.label}`); }
+            });
+        })();
+    }
 }
 
 
 
-buildResult = Scaffold({ appBar: AppBar({ title: Text("") }) });
-
-let stateless = new StatelessWidget();
-
-print("StatelessWidget");
-print(StatelessWidget as any);
-
-print("stateless")
-print(stateless as any);
-
-// class MyWidget extends StatelessWidget {
-//     public label: string;
-//     public constructor(label: string) {
-//         super();
-//         print("Called new mywidget");
-//         this.label = label;
-//     }
-
-//     public build(): Widget {
-//         print("called mywdiget build");
-//         return Text(this.label);
-//     }
-// }
-
-// let myWidget: MyWidget = new MyWidget("hello world");
-
-
 buildResult = Scaffold({
     appBar: AppBar({
-        title: Text("")
+        title: Text("Hello")
     }),
-    body: Center({
-        child: FlatButton({
-            child: stateless,
-            onPressed: () => { print(""); }
-        })
-    })
+    body: new MyWidget("MyWidget")
 });
