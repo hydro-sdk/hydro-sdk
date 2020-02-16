d453e975aa9cf0d04d93743896f585aeec460681
chgibb <chris.gibb@outlook.com>
2020-02-16 18:24:24 -0500

    reinit as regular flutter project and move everything from example folder up

--- example/scripts/buildAll.bash
+++
@@ -1,8 +0,0 @@
-for f in apps/*; 
-do
-    printf "$f\n"
-    ./node_modules/.bin/tstl -p $f/tsconfig.json
-    ./../luac.5.2.4 -o assets/$f.lc $f/index.lua
-    printf "    $(du -sh assets/$f.lc)\n"
-done
-
