1ba713eaadbe2727ff60c81a70737831f466cdae
chgibb <chris.gibb@outlook.com>
2020-02-16 19:51:03 -0500

    wip ts linting

--- scripts/lint.bash
+++ scripts/lint.bash
@@ -1,3 +1,3 @@
 flutter format lib test test_driver
-./node_modules/.bin/eslint apps/**/*.ts --fix
+./node_modules/.bin/eslint apps/**/*.ts --ext .ts
 ./node_modules/.bin/eslint runtime/**/*.ts --fix
