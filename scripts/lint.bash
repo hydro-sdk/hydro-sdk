1a3f7284737420258cdabd37072d533124021777
Chris Gibb <chgibb@Chriss-MacBook-Pro.local>
2020-02-28 21:39:14 -0500

    try to fix linter

--- scripts/lint.bash
+++ scripts/lint.bash
@@ -1,3 +1,2 @@
 flutter format lib test test_driver
-./node_modules/.bin/eslint apps/**/*.ts --ext .ts
-./node_modules/.bin/eslint runtime/**/*.ts --fix
+./node_modules/.bin/eslint "apps/**/*.ts" "runtime/**/*.ts" --ext .ts
