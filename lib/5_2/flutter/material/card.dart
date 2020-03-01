3c0ff32f113d927c665284a3fef67a676e6d3e38
chgibb <chris.gibb@outlook.com>
2020-03-01 15:10:51 -0500

    fixed linter

--- lib/5_2/flutter/material/card.dart
+++ lib/5_2/flutter/material/card.dart
@@ -3,8 +3,8 @@ import 'package:flua/5_2/flutter/syntheticBox.dart';
 import 'package:flua/5_2/table.dart' as l;
 import 'package:flutter/material.dart';
 
-loadCard(l.Table table){
-  table["card"] = makeLuaDartFunc(func: (List<dynamic>args){
+loadCard(l.Table table) {
+  table["card"] = makeLuaDartFunc(func: (List<dynamic> args) {
     return [
       Card(
         borderOnForeground: args[0]["borderOnForeground"],
