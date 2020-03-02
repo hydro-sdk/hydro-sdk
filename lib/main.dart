359965c50bce70c3444cf8b2dcd36cca9a2e737b
chgibb <chris.gibb@outlook.com>
2020-03-02 17:43:51 -0500

    fixed main

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flua/runFromBundle.dart';
 import 'package:flutter/material.dart';
 
-void main(String path) {
+void main() {
   WidgetsFlutterBinding.ensureInitialized();
-  runApp(RunFromBundle(path: path));
+  runApp(RunFromBundle(path: "assets/apps/animated-list.lc"));
 }
