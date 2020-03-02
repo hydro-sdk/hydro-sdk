ef7186c9f1e023a75ea48a81071a55dfa495ebf3
chgibb <chris.gibb@outlook.com>
2020-03-02 18:38:32 -0500

    allow passing undefined to debugger

--- runtime/ts/debugger.ts
+++ runtime/ts/debugger.ts
@@ -2,7 +2,7 @@ declare const ts: {
     debugger: (this: void, args: any) => void;
 };
 
-export function pauseInDebugger(args: any): void 
+export function pauseInDebugger(args: any | undefined): void 
 {
     ts.debugger(args);
 }
