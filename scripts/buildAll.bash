c5458f864532276601bb0af8a7fd941a6280d04f
chgibb <chris.gibb@outlook.com>
2020-03-05 19:04:28 -0500

    start widget tests

--- scripts/buildAll.bash
+++ scripts/buildAll.bash
@@ -3,3 +3,7 @@ do
     node compiler -t "$f/index.ts" -m "$(basename $f)" -d "assets/examples" -p $1
 done
 
+for f in test/widget/*;
+do
+    node compiler -t "$f" -m "$(basename $f)" -d "assets/test/widget" -p $1
+done
---
+++ test/widget/center-1.ts
@@ -0,0 +1,5 @@
+import {runApp} from "./../../runtime/flutter/runApp";
+import {Center} from "./../../runtime/flutter/widgets/center";
+import {SizedBox} from "./../../runtime/flutter/widgets/sizedBox";
+
+runApp(() => new Center({child: new SizedBox({})}));
