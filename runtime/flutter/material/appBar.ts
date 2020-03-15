b1d8c602f097ea5378d4e25f7d65b3ba0eb9338a
chgibb <chris.gibb@outlook.com>
2020-03-15 14:47:29 -0400

    lint

--- runtime/flutter/material/appBar.ts
+++ runtime/flutter/material/appBar.ts
@@ -1,8 +1,8 @@
-import { JITAllocatingRTManagedBox } from "../../syntheticBox";
+import {JITAllocatingRTManagedBox} from "../../syntheticBox";
 
-import { DartObject } from "./../../dart/core/object";
-import { Widget } from "./../widget";
-import { PreferredSize } from "./../widgets/preferredSize";
+import {DartObject} from "./../../dart/core/object";
+import {Widget} from "./../widget";
+import {PreferredSize} from "./../widgets/preferredSize";
 
 interface AppBarProps {
     title: Widget;
@@ -21,12 +21,14 @@ export class AppBar extends JITAllocatingRTManagedBox<AppBarProps, Widget> imple
 {
     public readonly runtimeType = "AppBar";
     public props: AppBarProps;
-    public constructor(props: AppBarProps) {
+    public constructor(props: AppBarProps) 
+    {
         super();
         this.props = props;
     }
 
-    public unwrap(): Widget {
+    public unwrap(): Widget 
+    {
         return flutter.material.appBar(this.props);
     }
 }
