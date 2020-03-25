4a4430d8c0ce7437fbcc4abe28107efe516e8872
chgibb <chris.gibb@outlook.com>
2020-03-25 18:22:26 -0400

    test for 2 reloads in a row

--- test/hot/stateful/statefulHotReload_test.dart
+++ test/hot/stateful/statefulHotReload_test.dart
@@ -6,41 +6,85 @@ import 'package:flutter_test/flutter_test.dart';
 
 void main() {
   testWidgets('stateful hot reload', (WidgetTester tester) async {
-    String hashPath = "test/hot/stateful/counter1.lc.sha256";
-    String bytecodePath = "test/hot/stateful/counter1.lc";
+    await tester.runAsync(() async {
+      String hashPath = "test/hot/stateful/counter1.lc.sha256";
+      String bytecodePath = "test/hot/stateful/counter1.lc";
 
-    WidgetsFlutterBinding.ensureInitialized();
-    await tester.pumpWidget(RunFromNetwork(
-      baseUrl: "http://127.0.0.1:3000/test/hot/stateful/counter.lc",
-      downloadHash: (String uri) async {
-        var file = File(hashPath);
-        var res = file.readAsStringSync();
-        return res;
-      },
-      downloadByteCodeImage: (String uri) async {
-        var file = File(bytecodePath);
-        var res = file.readAsBytesSync();
-        return res;
-      },
-    ));
+      WidgetsFlutterBinding.ensureInitialized();
+      await tester.pumpWidget(RunFromNetwork(
+        baseUrl: "http://127.0.0.1:3000/test/hot/stateful/counter.lc",
+        downloadHash: (String uri) async {
+          var file = File(hashPath);
+          var res = file.readAsStringSync();
+          return res;
+        },
+        downloadByteCodeImage: (String uri) async {
+          var file = File(bytecodePath);
+          var res = file.readAsBytesSync();
+          return res;
+        },
+      ));
 
-    expect(tester.takeException(), isNull);
+      expect(tester.takeException(), isNull);
+      await Future.delayed(Duration(seconds: 5));
 
-    await tester.pump(Duration(seconds: 2));
+      await tester.pump();
 
-    await tester.pump();
+      expect(find.byKey(Key("counter")), findsOneWidget);
+      expect(find.byKey(Key("increment")), findsOneWidget);
+      expect(find.text("You have pushed the button this many times"),
+          findsOneWidget);
 
-    expect(find.byKey(Key("counter")), findsOneWidget);
-    expect(find.byKey(Key("increment")), findsOneWidget);
-    expect(find.text("You have pushed the button this many times"),
-        findsOneWidget);
+      expect(find.text("0"), findsOneWidget);
+      await tester.tap(find.byKey(Key("increment")));
+      await tester.pumpAndSettle();
+      expect(find.text("1"), findsOneWidget);
+      await tester.tap(find.byKey(Key("increment")));
+      await tester.pumpAndSettle();
+      expect(find.text("2"), findsOneWidget);
 
-    expect(find.text("0"), findsOneWidget);
-    await tester.tap(find.byKey(Key("increment")));
-    await tester.pumpAndSettle();
-    expect(find.text("1"), findsOneWidget);
-    await tester.tap(find.byKey(Key("increment")));
-    await tester.pumpAndSettle();
-    expect(find.text("2"), findsOneWidget);
+      //Switch out files in response to polling
+      //Should trigger a hot reload
+      hashPath = "test/hot/stateful/counter2.lc.sha256";
+      bytecodePath = "test/hot/stateful/counter2.lc";
+
+      expect(tester.takeException(), isNull);
+
+      await Future.delayed(Duration(seconds: 5));
+
+      await tester.pump();
+
+      expect(find.byKey(Key("counter")), findsOneWidget);
+      expect(find.byKey(Key("increment")), findsOneWidget);
+
+      //Old text, should have been replaced
+      expect(find.text("You have pushed the button this many times"),
+          findsNothing);
+      //Old text should have been replaced by new text
+      expect(find.text("The text is different this time"), findsOneWidget);
+      //Previously tapped twice. Should still display 2
+      expect(find.text("2"), findsOneWidget);
+
+      //should still be able to tap and effect state
+      await tester.tap(find.byKey(Key("increment")));
+      await tester.pumpAndSettle();
+      expect(find.text("3"), findsOneWidget);
+
+      //Switch back to original files
+      hashPath = "test/hot/stateful/counter1.lc.sha256";
+      bytecodePath = "test/hot/stateful/counter1.lc";
+
+      expect(tester.takeException(), isNull);
+
+      await Future.delayed(Duration(seconds: 5));
+
+      await tester.pump();
+
+      expect(find.byKey(Key("counter")), findsOneWidget);
+      expect(find.byKey(Key("increment")), findsOneWidget);
+      expect(find.text("You have pushed the button this many times"),
+          findsOneWidget);
+      expect(find.text("3"), findsOneWidget);
+    });
   });
 }
