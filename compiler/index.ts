11fa0d5ecfdffd2cdbe3c2c002273770f8ddfe6c
chgibb <chris.gibb@outlook.com>
2020-03-25 17:55:56 -0400

    fixed not working if no w switch passed

--- compiler/index.ts
+++ compiler/index.ts
@@ -5,11 +5,11 @@ import * as chalk from "chalk";
 import * as rimraf from "rimraf";
 import * as chokidar from "chokidar";
 
-import {emit} from "./src/ts/emit";
-import {BuildOptions} from "./src/ts/buildOptions";
-import {transpile} from "./src/ts/transpile";
-import {squishAndCopy} from "./src/ts/squishAndCopy";
-import {configHash} from "./src/ts/configHash";
+import { emit } from "./src/ts/emit";
+import { BuildOptions } from "./src/ts/buildOptions";
+import { transpile } from "./src/ts/transpile";
+import { squishAndCopy } from "./src/ts/squishAndCopy";
+import { configHash } from "./src/ts/configHash";
 
 const argv = minimist(process.argv.slice(2));
 
@@ -20,64 +20,64 @@ const profile = argv.p;
 
 const clean = argv.clean;
 
-if(clean){
+if (clean) {
     rimraf.sync(".hydroc");
     process.exit(0);
 }
 
 const watch = argv.w;
 
-console.log(watch);
-
-if (!entry) 
-{
+if (!entry) {
     console.log("Entry file must be specified with -t switch");
     process.exit(1);
 }
-else 
-{
-    if (!fs.statSync(entry)) 
-    {
+else {
+    if (!fs.statSync(entry)) {
         console.log("Entry file does not exist");
         process.exit(1);
     }
 }
 
-if (!modName) 
-{
+if (!modName) {
     console.log("Output module name must be specified with -m switch");
     process.exit(1);
 }
 
-if (!outDir) 
-{
+if (!outDir) {
     console.log("Output directory must be specified with -d switch");
     process.exit(1);
 }
 
-if (!fs.existsSync(".hydroc")) 
-{
+if (!fs.existsSync(".hydroc")) {
     fs.mkdirSync(".hydroc");
 }
 
-function transpileTS(config: BuildOptions): void
-{
+function transpileTS(config: BuildOptions): void {
     const hash = configHash(config);
     console.log(`Build ${chalk.yellow(hash)}`);
     const res = transpile(config);
-    emit(config,res);
+    emit(config, res);
     console.log(`${chalk.green(config.entry)} ----> ${chalk.blue(`${config.outDir}/${config.modName}.lc`)}`);
     squishAndCopy(config);
 }
 
-if(watch!== undefined){
+if (watch !== undefined) {
 
-    chokidar.watch(watch).on("all",()=>{
-transpileTS({
-    entry:entry,
-    modName:modName,
-    outDir:outDir,
-    profile:profile
-});
-});
+    chokidar.watch(watch).on("all", () => {
+        transpileTS({
+            entry: entry,
+            modName: modName,
+            outDir: outDir,
+            profile: profile
+        });
+    });
+}
+
+else {
+    transpileTS({
+        entry: entry,
+        modName: modName,
+        outDir: outDir,
+        profile: profile
+    });
 }
