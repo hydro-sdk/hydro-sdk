33e529efc9844ce0339a331147cf586343fe4d08
chgibb <chris.gibb@outlook.com>
2020-04-03 16:47:52 -0400

    wip resizeimage

---
+++ runtime/flutter/painting/resizeImage.ts
@@ -0,0 +1,36 @@
+import { ImageProvider } from "./imageProvider";
+import { JITAllocatingRTManagedBox } from "../../syntheticBox";
+
+interface ResizeImageProps {
+    width?: number | undefined;
+    height?: number | undefined;
+}
+
+declare const flutter: {
+    painting: {
+        resizeImage: (this: void, imageProvider: ImageProvider, props: ResizeImageProps) => ResizeImage;
+    }
+}
+
+export class ResizeImage extends JITAllocatingRTManagedBox<ResizeImageProps, ResizeImage> implements ImageProvider {
+    public readonly runtimeType = "ImageProvider";
+    public imageProvider: ImageProvider;
+    public props: ResizeImageProps;
+    private constructor(imageProvider: ImageProvider, props: ResizeImageProps) {
+        super();
+
+        this.imageProvider = imageProvider;
+        this.props = props;
+    }
+
+    public static resizeIfNeeded(cacheWidth: number | undefined, cacheHeight: number | undefined, provider: ImageProvider): ImageProvider {
+        if (cacheWidth !== undefined || cacheHeight !== undefined) {
+            return new ResizeImage(provider, { width: cacheWidth, height: cacheHeight });
+        }
+        return provider;
+    }
+
+    public unwrap(): ResizeImage {
+        return flutter.painting.resizeImage(this.imageProvider, this.props);
+    }
+}
