113a089d7103d6790c4f3a7fefdefc51cc2ca146
chgibb <chris.gibb@outlook.com>
2020-04-07 22:54:14 -0400

    guard null on prop

--- lib/builtins/flutter/package/textStyle.dart
+++ lib/builtins/flutter/package/textStyle.dart
@@ -8,7 +8,7 @@ loadTextStyle({@required LuaState luaState, @required HydroTable table}) {
   table["textStyle"] = makeLuaDartFunc(func: (List<dynamic> args) {
     return [
       TextStyle(
-          fontSize: args[0]["fontSize"].toDouble(),
+          fontSize: args[0]["fontSize"]?.toDouble(),
           color: maybeUnwrapAndBuildArgument<Color>(args[0]["color"],
               parentState: luaState),
           fontWeight: maybeUnwrapAndBuildArgument<FontWeight>(
