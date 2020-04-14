53fd2d90aff128f6757151ebaf6e66fd46289cd9
chgibb <chris.gibb@outlook.com>
2020-04-13 23:14:33 -0400

    try to use dist compiler and install and run build first

--- scripts/buildAll.bash
+++ scripts/buildAll.bash
@@ -1,9 +1,9 @@
 for f in examples/*; 
 do
-    node compiler -t "$f/index.ts" -m "$(basename $f)" -d "assets/examples" -p $1
+    node dist/compiler -t "$f/index.ts" -m "$(basename $f)" -d "assets/examples" -p $1
 done
 
 for f in test/widget/*.ts;
 do
-    node compiler -t "$f" -m "$(basename $f)" -d "assets/test/widget" -p $1
+    node dist/compiler -t "$f" -m "$(basename $f)" -d "assets/test/widget" -p $1
 done
