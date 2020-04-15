9bac937c0e85807b6e467506161ccce99574b1e2
chgibb <chris.gibb@outlook.com>
2020-04-14 20:04:20 -0400

    try to debug resource paths

--- compiler/src/ts/reconcileResourcePath.ts
+++ compiler/src/ts/reconcileResourcePath.ts
@@ -5,13 +5,19 @@ export function reconcileResourcePath(resPath: string): string | undefined {
     const devPath = path.resolve(`compiler/${resPath}`);
 
     if (fs.existsSync(devPath)) {
+        console.log(`Found ${devPath}`);
         return devPath;
     }
+
+    console.log(`Couldn't find ${devPath}`);
     const prodPath = path.resolve(`./node_modules/hydro-sdk/dist/compiler/${resPath}`);
 
     if (fs.existsSync(prodPath)) {
+        console.log(`Found ${prodPath}`);
         return prodPath;
     }
+
+    console.log(`Couldn't find ${prodPath}`);
 
     return undefined;
 }
--- dist/compiler/src/ts/reconcileResourcePath.js
+++ dist/compiler/src/ts/reconcileResourcePath.js
@@ -5,12 +5,16 @@ var path = require("path");
 function reconcileResourcePath(resPath) {
     var devPath = path.resolve("compiler/" + resPath);
     if (fs.existsSync(devPath)) {
+        console.log("Found " + devPath);
         return devPath;
     }
+    console.log("Couldn't find " + devPath);
     var prodPath = path.resolve("./node_modules/hydro-sdk/dist/compiler/" + resPath);
     if (fs.existsSync(prodPath)) {
+        console.log("Found " + prodPath);
         return prodPath;
     }
+    console.log("Couldn't find " + prodPath);
     return undefined;
 }
 exports.reconcileResourcePath = reconcileResourcePath;
