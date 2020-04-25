85dfa9fc23521fdc261429b5405e95e243525960
chgibb <chris.gibb@outlook.com>
2020-04-25 17:50:51 -0400

    separate build scripts

---
+++ scripts/buildAllHx.bash
@@ -0,0 +1,2 @@
+haxe-4.0.5/haxe -p test -p runtime -m test.widget.CenterOne --dce full -D lua-vanilla  --lua center-1.hx.lua 
+compiler/res/darwin/luac52  -o assets/test/widget/center-1.hx.hc center-1.hx.lua
