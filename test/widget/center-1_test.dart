4398554238557689d784a1ae5523841d3b695371
chgibb <chris.gibb@outlook.com>
2020-04-25 19:49:47 -0400

    inline test target strings

--- test/widget/center-1_test.dart
+++ test/widget/center-1_test.dart
@@ -3,25 +3,16 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:hydro_sdk/integrationTestHarness.dart' as harness;
 import 'package:hydro_sdk/testMode.dart';
 
-Future<void> testBody(
-    {@required String target, @required WidgetTester tester}) async {
-  harness.main(target);
-  await tester.pump();
-
-  expect(find.byKey(Key("non-null child")), findsOneWidget);
-}
-
 void main() {
   testWidgets('center widget test', (WidgetTester tester) async {
     var testMode = getTestMode();
     expect(testMode, isNotNull);
 
-    if (testMode == TestMode.typescript) {
-      await testBody(
-          target: "assets/test/widget/center-1.ts.hc", tester: tester);
-    } else if (testMode == TestMode.haxe) {
-      await testBody(
-          target: "assets/test/widget/center-1.hx.hc", tester: tester);
-    }
+    harness.main(testMode == TestMode.typescript
+        ? "assets/test/widget/center-1.ts.hc"
+        : testMode == TestMode.haxe ? "assets/test/widget/center-1.hx.hc" : "");
+    await tester.pump();
+
+    expect(find.byKey(Key("non-null child")), findsOneWidget);
   });
 }
