6d4263e41e58710f4cabd49ce2b04e14a6705719
chgibb <chris.gibb@outlook.com>
2020-04-26 14:06:26 -0400

    explicitly unbox column enums

--- lib/cfr/builtins/flutter/widgets/column.dart
+++ lib/cfr/builtins/flutter/widgets/column.dart
@@ -8,15 +8,14 @@ void loadColumn({@required HydroState luaState, @required HydroTable table}) {
   table["column"] = makeLuaDartFunc(func: (List<dynamic> args) {
     return [
       Column(
-          mainAxisAlignment: MainAxisAlignment.values.firstWhere(
-              (x) => x.index == args[0]["mainAxisAlignment"],
-              orElse: () => null),
-          mainAxisSize: MainAxisSize.values.firstWhere(
-              (x) => x.index == args[0]["mainAxisSize"],
-              orElse: () => null),
-          crossAxisAlignment: CrossAxisAlignment.values.firstWhere(
-              (x) => x.index == args[0]["crossAxisAlignment"],
-              orElse: () => null),
+          mainAxisAlignment: maybeUnwrapEnum<MainAxisAlignment>(
+              values: MainAxisAlignment.values,
+              boxedEnum: args[0]["mainAxisAlignment"]),
+          mainAxisSize: maybeUnwrapEnum<MainAxisSize>(
+              values: MainAxisSize.values, boxedEnum: args[0]["mainAxisSize"]),
+          crossAxisAlignment: maybeUnwrapEnum<CrossAxisAlignment>(
+              values: CrossAxisAlignment.values,
+              boxedEnum: args[0]["crossAxisAlignment"]),
           children: maybeUnwrapAndBuildArgument<Widget>(args[0]["children"],
               parentState: luaState))
     ];
