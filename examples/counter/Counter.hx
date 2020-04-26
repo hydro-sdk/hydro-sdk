3ff276ba398515d3d07feec459ca2e9c2582c1d1
chgibb <chris.gibb@outlook.com>
2020-04-26 18:13:48 -0400

    wip haxe counter example

---
+++ examples/counter/Counter.hx
@@ -0,0 +1,61 @@
+package examples.counter;
+
+import haxe.extern.Rest;
+import runtime.flutter.BuildContext;
+import runtime.flutter.Widget;
+import runtime.flutter.widgets.StatefulWidget;
+import runtime.flutter.widgets.State;
+import runtime.flutter.widgets.StatelessWidget;
+import runtime.flutter.widgets.SizedBox;
+import runtime.flutter.widgets.Binding;
+import runtime.flutter.material.MaterialApp;
+
+private class MyApp extends StatelessWidget {
+	public function new() {
+		super();
+	}
+
+	public override function build():Widget {
+		return new MaterialApp({
+			title: "Counter App",
+			initialRoute: "/",
+			home: new SizedBox({})
+		});
+	}
+}
+
+class MyHomePage extends StatefulWidget {
+	public var title:String;
+
+	public function new(title:String) {
+		super();
+		this.title = title;
+	}
+
+	public override function createState() {
+		return new MyHomePageState(this.title);
+	}
+}
+
+class MyHomePageState extends State<MyHomePage> {
+	private var counter = 0;
+
+	public var title:String;
+
+	public function new(title:String) {
+		super();
+		this.title = title;
+	}
+
+	public override function build(context:BuildContext) {
+		return new SizedBox({});
+	}
+}
+
+class Counter {
+	static function main() {
+		Binding.runApp((rest:Rest<Dynamic>) -> {
+			return new MyApp();
+		});
+	}
+}
--- scripts/buildAllHx.bash
+++ scripts/buildAllHx.bash
@@ -1,2 +1,4 @@
 node dist/compiler --class-path runtime --class-path test -t test/widget/CenterOne.hx --main-class test.widget.CenterOne -m center-1.hx -d assets/test/widget -p $1
 node dist/compiler --class-path runtime --class-path test -t test/widget/SizedBoxOne.hx --main-class test.widget.SizedBoxOne -m sizedBox-1.hx -d assets/test/widget -p $1
+
+node dist/compiler --class-path runtime --class-path examples -t examples/counter/Counter.hx --main-class examples.counter.Counter -m counter -d assets/examples -p $1
