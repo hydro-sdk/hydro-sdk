650f12805997cb38c5d5c54a6b11c922f3a10494
chgibb <chris.gibb@outlook.com>
2020-04-26 17:40:18 -0400

    haxe materialapp

---
+++ runtime/flutter/material/MaterialApp.hx
@@ -0,0 +1,42 @@
+package runtime.flutter.material;
+
+import runtime.dart.core.DartObject;
+import runtime.flutter.widgets.StatelessWidget;
+import runtime.flutter.BuildContext;
+import runtime.flutter.Widget;
+
+@:nullSafety(Strict)
+private typedef MaterialAppProps = {
+	var ?initialRoute:Null<String>;
+	var home:Widget;
+	var ?title:Null<String>;
+	var ?route:Null<Map<String, (context:BuildContext) -> Widget>>;
+}
+
+@:nullSafety(Strict)
+private extern class MaterialBinding {
+	@:luaDotMethod
+	public function materialApp(props:MaterialAppProps):MaterialApp;
+}
+
+@:nullSafety(Strict)
+@:native("flutter")
+private extern class FlutterBinding {
+	public static var material:MaterialBinding;
+}
+
+@:nullSafety(Strict)
+class MaterialApp extends StatelessWidget implements DartObject {
+	public var runtimeType = "MaterialApp";
+	public var props:MaterialAppProps;
+
+	public function new(props:MaterialAppProps) {
+		super();
+		this.props = props;
+		this.props.title = this.props.title != null ? this.props.title : "";
+	}
+
+	public override function build() {
+		return FlutterBinding.material.materialApp(this.props);
+	}
+}
