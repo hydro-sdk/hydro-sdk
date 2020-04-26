d4fb9f58d1e6254bee92a388315e0ca4261eda3d
chgibb <chris.gibb@outlook.com>
2020-04-26 13:35:24 -0400

    manually build sizedbox test

--- scripts/buildAllHx.bash
+++ scripts/buildAllHx.bash
@@ -1,2 +1,5 @@
 haxe-4.0.5/haxe -p test -p runtime -m test.widget.CenterOne --dce full -D lua-vanilla  --lua center-1.hx.lua 
 compiler/res/darwin/luac52  -o assets/test/widget/center-1.hx.hc center-1.hx.lua
+
+haxe-4.0.5/haxe -p test -p runtime -m test.widget.SizedBoxOne --dce full -D lua-vanilla  --lua sizedBox-1.hx.lua 
+compiler/res/darwin/luac52  -o assets/test/widget/sizedBox-1.hx.hc sizedBox-1.hx.lua
