2c0b84eb096b31e6f7becad5fabbfea40f88b5d5
chgibb <chris.gibb@outlook.com>
2020-04-29 13:55:47 -0400

    rollback vararg logic

--- lib/cfr/vm/instructions/vararg.dart
+++ lib/cfr/vm/instructions/vararg.dart
@@ -8,7 +8,7 @@ void vararg({@required Frame frame, @required int A, @required int B}) {
     var i = 0;
     for (int n = A; n <= A + B - 2; n++) frame.SR(n, frame.varargs[i++]);
   } else {
-    frame.setTop((A + frame.varargs.length) - 1);
+    frame.setTop((A + frame.varargs.length) - (frame.prototype.params + 1));
     var i = A;
     for (int n = frame.prototype.params; n < frame.varargs.length; n++)
       frame.SR(i++, frame.varargs[n]);
