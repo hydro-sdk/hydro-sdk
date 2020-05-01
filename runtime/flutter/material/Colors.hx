224536796a6951b69019555fc4db5f6280f1fdab
chgibb <chris.gibb@outlook.com>
2020-05-01 19:44:19 -0400

    haxe card and colors

---
+++ runtime/flutter/material/Colors.hx
@@ -0,0 +1,10 @@
+package runtime.flutter.material;
+
+import runtime.dart.ui.Color;
+
+class Colors {
+    public static var transparent = new Color(0x00000000);
+    public static var white = new Color(0xFFFFFFFF);
+    public static var white60 = new Color(0x99FFFFFF);
+    public static var white70 = new Color(0xB3FFFFFF);
+}
