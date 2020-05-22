c127cfa2409ce5feac7d11ff8764cf4c5be9b44e
chgibb <chris.gibb@outlook.com>
2020-05-21 22:51:46 -0400

    minimal over the air showcase example

--- example-project/lib/main.dart
+++ example-project/lib/main.dart
@@ -1,16 +1,70 @@
-import 'package:flutter/material.dart';
 import 'package:hydro_sdk/runFromNetwork.dart';
+import 'package:flutter/material.dart';
 
-//Hotel booking is a line for line port of https://github.com/cybdom/hotel_booking_ui
-void main() => runApp(RunFromNetwork(
-  args: [],
-    baseUrl:
-        "https://github.com/chgibb/hydro-sdk/raw/master/dist/hotel-booking.hc"));
+class ShowCase extends StatelessWidget {
+  ShowCase();
 
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        routes: {
+          "counter": (BuildContext context) => Builder(
+              builder: (BuildContext context) => RunFromNetwork(
+                  args: [],
+                  baseUrl:
+                      "https://github.com/chgibb/hydro-sdk/raw/master/dist/counter.hc")),
+          "app-bar": (BuildContext context) => Builder(
+              builder: (BuildContext context) => RunFromNetwork(
+                  args: [],
+                  baseUrl:
+                      "https://github.com/chgibb/hydro-sdk/raw/master/dist/basic-app-bar.hc")),
+          "animated-list": (BuildContext context) => Builder(
+              builder: (BuildContext context) => RunFromNetwork(
+                  args: [],
+                  baseUrl:
+                      "https://github.com/chgibb/hydro-sdk/raw/master/dist/animated-list.hc")),
+          "hotel": (BuildContext context) => Builder(
+              builder: (BuildContext context) => RunFromNetwork(
+                  args: [],
+                  baseUrl:
+                      "https://github.com/chgibb/hydro-sdk/raw/master/dist/hotel-booking.hc")),
+        },
+        home: Builder(
+            builder: (BuildContext context) => Scaffold(
+                  appBar: AppBar(
+                    title: Text("Over The Air Showcase"),
+                  ),
+                  body: Column(children: [
+                    FlatButton(
+                      child: Text("Counter Example"),
+                      onPressed: () {
+                        Navigator.pushNamed(context, "counter");
+                      },
+                    ),
+                    FlatButton(
+                      child: Text("App Bar Example"),
+                      onPressed: () {
+                        Navigator.pushNamed(context, "app-bar");
+                      },
+                    ),
+                    FlatButton(
+                      child: Text("Animated List Example"),
+                      onPressed: () {
+                        Navigator.pushNamed(context, "animated-list");
+                      },
+                    ),
+                    FlatButton(
+                      child: Text("Hotel Booking Example"),
+                      onPressed: () {
+                        Navigator.pushNamed(context, "hotel");
+                      },
+                    )
+                  ]),
+                )));
+  }
+}
 
-//Use the following to run the output from compiling hello-world.ts and packaging it into the root asset bundle
-//import 'package:hydro_sdk/runFromBundle.dart';
-// void main() => runApp(RunFromBundle(
-//     path:
-//         "assets/hello-world.hc"));
-
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(ShowCase());
+}
