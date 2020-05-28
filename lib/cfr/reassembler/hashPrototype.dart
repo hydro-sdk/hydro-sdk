4a70ea9a2f13141dfb9b970a48ee4ab7c1348396
chgibb <chris.gibb@outlook.com>
2020-05-28 19:01:39 -0400

    null guard in hash functions

--- lib/cfr/reassembler/hashPrototype.dart
+++ lib/cfr/reassembler/hashPrototype.dart
@@ -29,7 +29,7 @@ String hashPrototype(Prototype prototype,
     input.add([
       prototype.lineStart,
       prototype.lineEnd,
-      ...prototype.source.codeUnits
+      ...(prototype.source?.codeUnits ?? [])
     ]);
   }
 
