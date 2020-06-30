d8a96e1fd1a6b8a6959406d9f1137b4c2e8837c3
chgibb <chris.gibb@outlook.com>
2020-06-30 15:30:52 -0400

    report link status even when loading from asset bundle

--- lib/loadFileFromBundle.dart
+++ lib/loadFileFromBundle.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/services.dart';
 import 'package:hydro_sdk/cfr/lasm/nativeThunk.dart';
+import 'package:hydro_sdk/cfr/linkStatus.dart';
 import 'package:meta/meta.dart';
 import 'package:hydro_sdk/cfr/decode/decoder.dart';
 import 'package:hydro_sdk/cfr/vm/closure.dart';
@@ -10,12 +11,13 @@ import 'package:hydro_sdk/hydroState.dart';
 Future<HydroFunction> loadFileFromBundle({
   @required HydroState hydroState,
   @required String path,
+  @required LinkStatus linkStatus,
   @required Map<String, NativeThunk> thunks,
 }) async {
   var contents = await rootBundle.load(path);
   var decoder = Decoder(contents.buffer);
   var dump = decoder.readCodeDump(
-      name: path, linkStatus: null, dump: null, thunks: thunks);
+      name: path, linkStatus: linkStatus, dump: null, thunks: thunks);
 
   return HydroFunctionImpl(Closure(dump.main,
       context: hydroState.context,
