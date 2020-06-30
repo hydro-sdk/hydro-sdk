02567e20f85e9525d3fb3e74b97791cdab96f743
chgibb <chris.gibb@outlook.com>
2020-06-30 11:06:34 -0400

    load builtin for test

--- test/lua/spec/checktable_test.dart
+++ test/lua/spec/checktable_test.dart
@@ -1,9 +1,11 @@
+import 'package:hydro_sdk/cfr/builtins/loadBuiltins.dart';
 import 'package:hydro_sdk/hydroState.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
   test('', () async {
     var state = HydroState();
+    loadBuiltins(hydroState: state, builtins: [BuiltinLib.table]);
 
     var res = await state.doFile("lua/spec/checktable.hc");
     print(res.toString());
