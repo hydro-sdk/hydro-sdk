09da344f21f4477ad27649a205beed3a2228f9fa
chgibb <chris.gibb@outlook.com>
2020-06-30 11:19:59 -0400

    add more builtins to more tests

--- test/lua/spec/checktable_test.dart
+++ test/lua/spec/checktable_test.dart
@@ -5,7 +5,8 @@ import 'package:flutter_test/flutter_test.dart';
 void main() {
   test('', () async {
     var state = HydroState();
-    loadBuiltins(hydroState: state, builtins: [BuiltinLib.table]);
+    loadBuiltins(
+        hydroState: state, builtins: [BuiltinLib.table, BuiltinLib.base]);
 
     var res = await state.doFile("lua/spec/checktable.hc");
     print(res.toString());
