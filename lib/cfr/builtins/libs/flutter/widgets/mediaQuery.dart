e0f9429d7d5b838e7bb505ab3d9b1320acb71493
chgibb <chris.gibb@outlook.com>
2020-07-10 14:52:41 -0400

    refactor mediaquey and size boxes

--- lib/cfr/builtins/libs/flutter/widgets/mediaQuery.dart
+++ lib/cfr/builtins/libs/flutter/widgets/mediaQuery.dart
@@ -1,3 +1,4 @@
+import 'package:hydro_sdk/cfr/builtins/boxing/boxers.dart';
 import 'package:hydro_sdk/cfr/builtins/boxing/boxes.dart';
 import 'package:hydro_sdk/cfr/builtins/libs/dart/ui/size.dart';
 import 'package:hydro_sdk/cfr/vm/context.dart';
@@ -9,22 +10,25 @@ import 'package:hydro_sdk/hydroState.dart';
 class VMManagedMediaQueryData extends VMManagedBox<MediaQueryData> {
   final HydroTable table;
   final MediaQueryData vmObject;
-  VMManagedMediaQueryData({@required this.table, @required this.vmObject}) {
-    table["size"] =
-        VMManagedSize(table: HydroTable(), vmObject: vmObject.size).table;
+  VMManagedMediaQueryData({@required this.table, @required this.vmObject})
+      : super(table: table, vmObject: vmObject) {
+    table["size"] = maybeBoxObject(object: vmObject.size, hydroState: null);
   }
 }
 
 void loadMediaQuery(
     {@required HydroState luaState, @required HydroTable table}) {
+  registerBoxer(boxer: ({MediaQueryData vmObject, HydroState hydroState}) {
+    return VMManagedMediaQueryData(vmObject: vmObject, table: HydroTable());
+  });
+
   table["mediaQueryOf"] = makeLuaDartFunc(func: (List<dynamic> args) {
     return [
-      VMManagedMediaQueryData(
-              table: HydroTable(),
-              vmObject: MediaQuery.of(maybeUnBoxAndBuildArgument<BuildContext>(
-                  args[0],
-                  parentState: luaState)))
-          .table
+      maybeBoxObject(
+          object: MediaQuery.of(maybeUnBoxAndBuildArgument<BuildContext>(
+              args[0],
+              parentState: luaState)),
+          hydroState: null)
     ];
   });
 }
