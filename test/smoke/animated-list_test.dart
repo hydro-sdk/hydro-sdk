6c60d4280df9bf4429a778a4347448250451bac2
chgibb <chris.gibb@outlook.com>
2020-07-17 12:54:23 -0400

    remove test mode checks

--- test/smoke/animated-list_test.dart
+++ test/smoke/animated-list_test.dart
@@ -5,17 +5,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:hydro_sdk/integrationTestHarness.dart' ;
-import 'package:hydro_sdk/testMode.dart';
 
 void main() {
   testWidgets('animated_list sample app smoke test',
       (WidgetTester tester) async {
-    var testMode = getTestMode();
-    expect(testMode, isNotNull);
-
-    if (testMode != TestMode.typescript) {
-      return;
-    }
 
     await tester.pumpWidget(integrationTestHarness("../assets/examples/animated-list"));
     await tester.pumpAndSettle();
