67671ea681f405114c70e8b16cc6d89ca15b157d
chgibb <chris.gibb@outlook.com>
2020-07-17 15:31:20 -0400

    refactor to test harness

--- test/unit/async/futureValueChain-2_test.dart
+++ test/unit/async/futureValueChain-2_test.dart
@@ -1,23 +1,19 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:hydro_sdk/cfr/builtins/loadBuiltins.dart';
-import 'package:hydro_sdk/cfr/coroutine/coroutineresult.dart';
-import 'package:hydro_sdk/hydroState.dart';
+import 'package:hydro_sdk/unitTestHarness.dart';
 
 void main() async {
   LiveTestWidgetsFlutterBinding();
   testWidgets('', (WidgetTester tester) async {
-    var state = HydroState();
-    loadBuiltins(hydroState: state, builtins: [
-      BuiltinLib.dart,
-      BuiltinLib.base,
-      BuiltinLib.string,
-      BuiltinLib.table,
-      BuiltinLib.math
-    ]);
-    CoroutineResult res;
-
-    res = await state
-        .doFile("../assets/test/unit/async/futureValueChain-2.ts.hc");
+    var res = await unitTestHarness(
+        path: "../assets/test/unit/async/futureValueChain-2.ts.hc",
+        libs: [
+          BuiltinLib.dart,
+          BuiltinLib.base,
+          BuiltinLib.string,
+          BuiltinLib.table,
+          BuiltinLib.math
+        ]);
 
     if (!res.success) {
       print(res.values[0]);
