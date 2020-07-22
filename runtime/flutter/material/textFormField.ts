ce6386bffc056c568dc1691dd806cad3f5b428d0
chgibb <chris.gibb@outlook.com>
2020-07-22 17:57:46 -0400

    textformfield prop validations

--- runtime/flutter/material/textFormField.ts
+++ runtime/flutter/material/textFormField.ts
@@ -1,3 +1,4 @@
+import { Type } from "../../dart/core/type";
 import { TextDirection } from "./../../dart/ui/textDirection";
 import { TextAlign } from "./../../dart/ui/textAlign";
 import { Radius } from "./../../dart/ui/radius";
@@ -12,10 +13,11 @@ import { TextEditingController } from "./../widgets/textEditingController";
 import { ToolbarOptions } from "./../widgets/toolbarOptions";
 import { FormFieldValidator } from "./../widgets/formFieldValidator";
 import { FormFieldSetter } from "./../widgets/formFieldSetter";
+import { StatelessWidget } from "./../widgets/statelessWidget";
 import { TextStyle } from "./../painting/textStyle";
 import { TextAlignVertical } from "./../painting/textAlignVertical";
 import { EdgeInsets } from "./../painting/edgeInsets";
-import {StrutStyle } from "./../painting/strutStyle";
+import { StrutStyle } from "./../painting/strutStyle";
 import { TextInputAction } from "./../services/textInputAction";
 import { TextCapitalization } from "./../services/textCapitalization";
 import { SmartDashesType } from "./../services/smartDashesType";
@@ -23,6 +25,7 @@ import { SmartQuotesType } from "./../services/smartQuotesType";
 
 import { InputDecoration } from "./inputDecoration";
 import { InputCounterWidgetBuilder } from "./inputCounterWidgetBuilder";
+import { RuntimeBaseClass } from "../../runtimeBaseClass";
 
 interface TextFormFieldProps {
     key?: Key | undefined;
@@ -69,4 +72,84 @@ interface TextFormFieldProps {
     enableInteractiveSelection?: boolean | undefined;
     buildCounter?: InputCounterWidgetBuilder | undefined;
     //  scrollPhysics
+}
+
+declare const flutter: {
+    material: {
+        textFormField: (this: void, props: TextFormFieldProps) => TextFormField;
+    };
+};
+
+export class TextFormField extends StatelessWidget implements RuntimeBaseClass {
+    public readonly internalRuntimeType = new Type(TextFormField);
+    public props: TextFormFieldProps;
+    public constructor(props: TextFormFieldProps) {
+        super();
+
+        this.props = props;
+
+        if (this.props.decoration === undefined) {
+            this.props.decoration = new InputDecoration({});
+        }
+
+        if (this.props.textCapitalization === undefined) {
+            this.props.textCapitalization = TextCapitalization.none;
+        }
+
+        if (this.props.textAlign === undefined) {
+            this.props.textAlign = TextAlign.start;
+        }
+
+        if (this.props.autofocus === undefined) {
+            this.props.autofocus = false;
+        }
+
+        if (this.props.readOnly === undefined) {
+            this.props.readOnly = false;
+        }
+
+        if (this.props.obscureText === undefined) {
+            this.props.obscureText = false;
+        }
+
+        if (this.props.enableSuggestions === undefined) {
+            this.props.enableSuggestions = true;
+        }
+
+        if (this.props.autovalidate === undefined) {
+            this.props.autovalidate = false;
+        }
+
+        if (this.props.maxLengthEnforced === undefined) {
+            this.props.maxLengthEnforced = true;
+        }
+
+        if (this.props.maxLines === undefined) {
+            this.props.maxLines = 1;
+        }
+
+        if (this.props.expands === undefined) {
+            this.props.expands = false;
+        }
+
+        if (this.props.enabled === undefined) {
+            this.props.enabled = true;
+        }
+
+        if (this.props.cursorWidth === undefined) {
+            this.props.cursorWidth = 2.0;
+        }
+
+        if (this.props.scrollPadding === undefined) {
+            this.props.scrollPadding = EdgeInsets.all(20.0);
+        }
+
+        if (this.props.enableInteractiveSelection === undefined) {
+            this.props.enableInteractiveSelection = true;
+        }
+    }
+
+    public build() {
+        return flutter.material.textFormField(this.props);
+    }
 }
