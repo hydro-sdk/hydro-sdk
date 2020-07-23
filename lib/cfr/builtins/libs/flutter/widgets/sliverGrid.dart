e02c3dfd723ca32f8325c6159201e526d2252a4b
chgibb <chris.gibb@outlook.com>
2020-07-23 18:26:46 -0400

    slivergrid

---
+++ lib/cfr/builtins/libs/flutter/widgets/sliverGrid.dart
@@ -0,0 +1,38 @@
+import 'package:hydro_sdk/hydroState.dart';
+import 'package:hydro_sdk/cfr/vm/context.dart';
+import 'package:hydro_sdk/cfr/builtins/boxing/unboxers.dart';
+import 'package:hydro_sdk/cfr/vm/table.dart';
+import 'package:flutter/material.dart';
+
+void loadSliverGrid(
+    {@required HydroState luaState, @required HydroTable table}) {
+  table["sliverGridCount"] = makeLuaDartFunc(func: (List<dynamic> args) {
+    return [
+      SliverGrid.count(
+        key: maybeUnBoxAndBuildArgument<Key>(args[0]["key"],
+            parentState: luaState),
+        crossAxisCount: args[0]["crossAxisCount"],
+        mainAxisSpacing: args[0]["mainAxisSpacing"]?.toDouble(),
+        crossAxisSpacing: args[0]["crossAxisSpacing"]?.toDouble(),
+        childAspectRatio: args[0]["childAspectRatio"]?.toDouble(),
+        children: maybeUnBoxAndBuildArgument<Widget>(args[0]["children"],
+            parentState: luaState),
+      )
+    ];
+  });
+
+  table["sliverGridExtent"] = makeLuaDartFunc(func: (List<dynamic> args) {
+    return [
+      SliverGrid.extent(
+        key: maybeUnBoxAndBuildArgument<Key>(args[0]["key"],
+            parentState: luaState),
+        maxCrossAxisExtent: args[0]["maxCrossAxisExtent"],
+        mainAxisSpacing: args[0]["mainAxisSpacing"]?.toDouble(),
+        crossAxisSpacing: args[0]["crossAxisSpacing"]?.toDouble(),
+        childAspectRatio: args[0]["childAspectRatio"]?.toDouble(),
+        children: maybeUnBoxAndBuildArgument<Widget>(args[0]["children"],
+            parentState: luaState),
+      )
+    ];
+  });
+}
---
+++ runtime/flutter/widgets/sliverGrid.ts
@@ -0,0 +1,116 @@
+import { Widget } from "../widget";
+import { RuntimeBaseClass } from "../../runtimeBaseClass";
+import { Type } from "../../dart/core/type";
+import { List } from "../../dart/collection/list";
+import { StatelessWidget } from "./statelessWidget";
+import { Key } from "./../foundation/key";
+import { pauseInDebugger } from "../../dart/developer/debugger";
+
+interface SliverGridCountProps {
+    type: "count";
+    key?: Key | undefined;
+    crossAxisCount: number
+    mainAxisSpacing?: number | undefined;
+    crossAxisSpacing?: number | undefined;
+    childAspectRatio?: number | undefined;
+    children?: List<Widget> | undefined;
+}
+
+interface SliverGridExtentProps {
+
+    type: "extent";
+    key?: Key | undefined;
+    maxCrossAxisExtent: number
+    mainAxisSpacing?: number | undefined;
+    crossAxisSpacing?: number | undefined;
+    childAspectRatio?: number | undefined;
+    children?: List<Widget> | undefined;
+}
+
+
+type SliverGridProps = SliverGridCountProps | SliverGridExtentProps;
+
+declare const flutter: {
+    widgets: {
+        sliverGridCount: (this: void, props: SliverGridCountProps) => SliverGrid;
+        sliverGridExtent: (this: void, props: SliverGridExtentProps) => SliverGrid;
+    };
+};
+
+export class SliverGrid extends StatelessWidget implements RuntimeBaseClass {
+    public readonly internalRuntimeType = new Type(SliverGrid);
+    public props: SliverGridProps;
+    private constructor(props: SliverGridProps) {
+        super();
+
+        this.props = props;
+    }
+
+    public static count(props: Omit<SliverGridCountProps, "type">) {
+        if (props.mainAxisSpacing === undefined) {
+            props.mainAxisSpacing = 0.0;
+        }
+
+        if (props.crossAxisSpacing === undefined) {
+            props.crossAxisSpacing = 0.0;
+        }
+
+        if (props.childAspectRatio === undefined) {
+            props.childAspectRatio = 1.0;
+        }
+
+        if (props.children === undefined) {
+            props.children = List.fromArray([]);
+        }
+
+        return new SliverGrid({
+            type: "count",
+            key: props.key,
+            crossAxisCount: props.crossAxisCount,
+            mainAxisSpacing: props.crossAxisSpacing,
+            crossAxisSpacing: props.crossAxisSpacing,
+            childAspectRatio: props.childAspectRatio,
+            children: props.children
+        });
+    }
+
+    public static extent(props: SliverGridExtentProps) {
+        if (props.mainAxisSpacing === undefined) {
+            props.mainAxisSpacing = 0.0;
+        }
+
+        if (props.crossAxisSpacing === undefined) {
+            props.crossAxisSpacing = 0.0;
+        }
+
+        if (props.childAspectRatio === undefined) {
+            props.childAspectRatio = 1.0;
+        }
+
+        if (props.children === undefined) {
+            props.children = List.fromArray([]);
+        }
+
+        return new SliverGrid({
+            type: "extent",
+            key: props.key,
+            maxCrossAxisExtent: props.maxCrossAxisExtent,
+            mainAxisSpacing: props.crossAxisSpacing,
+            crossAxisSpacing: props.crossAxisSpacing,
+            childAspectRatio: props.childAspectRatio,
+            children: props.children
+        });
+    }
+
+    public build() {
+        // pauseInDebugger(this);
+        switch (this.props.type) {
+            case "count":
+                return flutter.widgets.sliverGridCount(this.props);
+                break;
+            case "extent":
+                return flutter.widgets.sliverGridExtent(this.props);
+                break;
+        }
+    }
+}
