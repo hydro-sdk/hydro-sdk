cbdbc81a4dcde3c91834222fdf366ee73ba55ab8
chgibb <chris.gibb@outlook.com>
2020-07-23 15:42:44 -0400

    expose regular list view ctor

--- runtime/flutter/widgets/listView.ts
+++ runtime/flutter/widgets/listView.ts
@@ -1,13 +1,34 @@
-import {Axis} from "../painting/axis";
-import {EdgeInsets} from "../painting/edgeInsets";
-import {DragStartBehavior} from "../gestures/dragStartBehavior";
-import {RuntimeBaseClass} from "../../runtimeBaseClass";
-import {Type} from "../../dart/core/type";
+import { List } from "../../dart/collection/list";
+import { Type } from "../../dart/core/type";
+import { Axis } from "../painting/axis";
+import { EdgeInsets } from "../painting/edgeInsets";
+import { DragStartBehavior } from "../gestures/dragStartBehavior";
+import { RuntimeBaseClass } from "../../runtimeBaseClass";
 
-import {Key} from "./../foundation/key";
-import {BuildContext} from "./../buildContext";
-import {Widget} from "./../widget";
-import {StatelessWidget} from "./../widgets/statelessWidget";
+import { Key } from "./../foundation/key";
+import { BuildContext } from "./../buildContext";
+import { Widget } from "./../widget";
+import { StatelessWidget } from "./statelessWidget";
+import { ScrollViewKeyboardDismissBehavior } from "./scrollViewKeyboardDismissBehavior";
+
+interface ListviewRegularProps {
+    key?: Key | undefined;
+    scrollDirection?: Axis | undefined;
+    reverse?: boolean | undefined;
+    primary?: boolean | undefined;
+    shrinkWrap?: boolean | undefined;
+    padding?: EdgeInsets | undefined;
+    itemExtent?: number | undefined;
+    itemCount?: number | undefined;
+    addAutomaticKeepAlives?: boolean | undefined;
+    addRepaintBoundaries?: boolean | undefined;
+    addSemanticIndexes?: boolean | undefined;
+    cacheExtent?: number | undefined;
+    children?: List<Widget> | undefined;
+    semanticChildCount?: number | undefined;
+    dragStartBehavior?: DragStartBehavior | undefined;
+    keyboardDismissBehavior?: ScrollViewKeyboardDismissBehavior | undefined;
+}
 
 interface ListViewBuilderProps {
     key?: Key | undefined;
@@ -33,70 +54,107 @@ declare const flutter: {
             this: void,
             props: ListViewBuilderProps
         ) => ListView;
+        listViewRegular: (
+            this: void,
+            props: ListviewRegularProps
+        ) => ListView;
     };
 };
 
-type ListViewType = "builder";
-type ListViewProps = {} & ListViewBuilderProps;
+type ListViewType = "regular" | "builder";
+type ListViewProps = {} & ListviewRegularProps & ListViewBuilderProps;
 
-export class ListView extends StatelessWidget implements RuntimeBaseClass 
-{
+export class ListView extends StatelessWidget implements RuntimeBaseClass {
     public readonly internalRuntimeType = new Type(ListView);
     private readonly listViewType: ListViewType;
     private readonly props: ListViewProps
-    private constructor(listViewType: ListViewType, props: ListViewProps) 
-    {
+    private constructor(listViewType: ListViewType, props: ListViewProps) {
         super();
 
         this.listViewType = listViewType;
         this.props = props;
     }
 
-    public build(): Widget 
-    {
-        switch (this.listViewType) 
-        {
-        case "builder":
-            return flutter.widgets.listViewBuilder(this.props);
-            break;
+    public build(): Widget {
+        switch (this.listViewType) {
+            case "builder":
+                return flutter.widgets.listViewBuilder(this.props);
+                break;
+
+            case "regular":
+                return flutter.widgets.listViewRegular(this.props);
+                break;
         }
     }
 
-    public static builder(props: ListViewBuilderProps): ListView 
-    {
+    public static create(props: ListviewRegularProps): ListView {
 
-        if (props.scrollDirection === undefined) 
-        {
+        if (props.scrollDirection === undefined) {
             props.scrollDirection = Axis.vertical;
         }
 
-        if (props.reverse === undefined) 
-        {
+        if (props.reverse === undefined) {
             props.reverse = false;
         }
 
-        if (props.shrinkWrap === undefined) 
-        {
+        if (props.shrinkWrap === undefined) {
             props.shrinkWrap = false;
         }
 
-        if (props.addAutomaticKeepAlives === undefined) 
-        {
+        if (props.addAutomaticKeepAlives === undefined) {
             props.addAutomaticKeepAlives = true;
         }
 
-        if (props.addRepaintBoundaries === undefined) 
-        {
+        if (props.addRepaintBoundaries === undefined) {
             props.addRepaintBoundaries = true;
         }
 
-        if (props.addSemanticIndexes === undefined) 
-        {
+        if (props.addSemanticIndexes === undefined) {
             props.addSemanticIndexes = true;
         }
 
-        if (props.dragStartBehavior === undefined) 
-        {
+        if (props.children === undefined) {
+            props.children = List.fromArray([]);
+        }
+
+        if (props.dragStartBehavior === undefined) {
+            props.dragStartBehavior = DragStartBehavior.start;
+        }
+
+        if (props.keyboardDismissBehavior === undefined) {
+            props.keyboardDismissBehavior = ScrollViewKeyboardDismissBehavior.manual;
+        }
+
+        return new ListView("regular", props);
+    }
+
+    public static builder(props: ListViewBuilderProps): ListView {
+
+        if (props.scrollDirection === undefined) {
+            props.scrollDirection = Axis.vertical;
+        }
+
+        if (props.reverse === undefined) {
+            props.reverse = false;
+        }
+
+        if (props.shrinkWrap === undefined) {
+            props.shrinkWrap = false;
+        }
+
+        if (props.addAutomaticKeepAlives === undefined) {
+            props.addAutomaticKeepAlives = true;
+        }
+
+        if (props.addRepaintBoundaries === undefined) {
+            props.addRepaintBoundaries = true;
+        }
+
+        if (props.addSemanticIndexes === undefined) {
+            props.addSemanticIndexes = true;
+        }
+
+        if (props.dragStartBehavior === undefined) {
             props.dragStartBehavior = DragStartBehavior.start;
         }
 
