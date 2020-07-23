3e3a37ffa59175c0223066dbdc501841b2f37b7e
chgibb <chris.gibb@outlook.com>
2020-07-23 15:43:03 -0400

    add search to example

--- examples/pokeApp/pokeApp.ts
+++ examples/pokeApp/pokeApp.ts
@@ -1,25 +1,26 @@
-import {StatelessWidget} from "../../runtime/flutter/widgets/statelessWidget";
-import {ScopedModel} from "../../runtime/scopedModel/scopedModel";
+import { StatelessWidget } from "../../runtime/flutter/widgets/statelessWidget";
+import { ScopedModel } from "../../runtime/scopedModel/scopedModel";
 
-import {PokeGridScreen} from "./screens/gridScreen";
-import {PokeHubService} from "./data/pokeHubService";
+import { PokeGridScreen } from "./screens/gridScreen";
+import { PokeHubService } from "./data/pokeHubService";
+import { TextSearchService } from "./data/textSearchService";
 
-export class PokeApp extends StatelessWidget 
-{
+export class PokeApp extends StatelessWidget {
     private pokeHubService: PokeHubService;
 
-    public constructor(pokeHubService: PokeHubService) 
-    {
+    public constructor(pokeHubService: PokeHubService) {
         super();
 
         this.pokeHubService = pokeHubService;
     }
 
-    public build() 
-    {
+    public build() {
         return new ScopedModel({
             model: this.pokeHubService,
-            child: new PokeGridScreen()
+            child: new ScopedModel({
+                model: new TextSearchService(),
+                child: new PokeGridScreen()
+            })
         });
     }
 }
--- examples/pokeApp/screens/gridScreen.ts
+++ examples/pokeApp/screens/gridScreen.ts
@@ -1,101 +1,73 @@
-import {Scaffold} from "../../../runtime/flutter/material/scaffold";
-import {AppBar} from "../../../runtime/flutter/material/appBar";
-import {Text} from "../../../runtime/flutter/widgets/text";
-import {CircularProgressIndicator} from "../../../runtime/flutter/material/circularProgressIndicator";
-import {Center} from "../../../runtime/flutter/widgets/center";
-import {GridView} from "../../../runtime/flutter/widgets/gridView";
-import {Padding} from "../../../runtime/flutter/widgets/padding";
-import {EdgeInsets} from "../../../runtime/flutter/painting/edgeInsets";
-import {InkWell} from "../../../runtime/flutter/material/inkWell";
-import {Card} from "../../../runtime/flutter/material/card";
-import {Column} from "../../../runtime/flutter/widgets/column";
-import {MainAxisSize} from "../../../runtime/flutter/rendering/mainAxisSize";
-import {MainAxisAlignment} from "../../../runtime/flutter/widgets/mainAxisAlignment";
-import {Container} from "../../../runtime/flutter/widgets/container";
-import {Image} from "../../../runtime/flutter/widgets/image";
-import {List} from "../../../runtime/dart/collection/list";
-import {TextStyle} from "../../../runtime/flutter/painting/textStyle";
-import {FontWeight} from "../../../runtime/dart/ui/fontWeight";
-import {Navigator} from "../../../runtime/flutter/widgets/navigator";
-import {MaterialPageRoute} from "../../../runtime/flutter/material/materialPageRoute";
-import {BuildContext} from "../../../runtime/flutter/buildContext";
-import {Colors} from "../../../runtime/flutter/material/colors";
-import {Hero} from "../../../runtime/flutter/widgets/hero";
-import {Key} from "../../../runtime/flutter/foundation/key";
-import {StatelessWidget} from "../../../runtime/flutter/widgets/statelessWidget";
-import {ScopedModel} from "../../../runtime/scopedModel/scopedModel";
-import {PokeHubService} from "../data/pokeHubService";
+import { Scaffold } from "../../../runtime/flutter/material/scaffold";
+import { AppBar } from "../../../runtime/flutter/material/appBar";
+import { Text } from "../../../runtime/flutter/widgets/text";
+import { CircularProgressIndicator } from "../../../runtime/flutter/material/circularProgressIndicator";
+import { Center } from "../../../runtime/flutter/widgets/center";
+import { GridView } from "../../../runtime/flutter/widgets/gridView";
+import { Padding } from "../../../runtime/flutter/widgets/padding";
+import { EdgeInsets } from "../../../runtime/flutter/painting/edgeInsets";
+import { List } from "../../../runtime/dart/collection/list";
+import { BuildContext } from "../../../runtime/flutter/buildContext";
+import { Colors } from "../../../runtime/flutter/material/colors";
+import { StatelessWidget } from "../../../runtime/flutter/widgets/statelessWidget";
+import { ScopedModel } from "../../../runtime/scopedModel/scopedModel";
+import { PokeHubService } from "../data/pokeHubService";
+import { PokeHub } from "./../data/pokeHub";
+import { ListView } from "../../../runtime/flutter/widgets/listView";
+import { TextFormField } from "../../../runtime/flutter/material/textFormField";
+import { PokeCard } from "../widgets/pokeCard";
+import { TextSearchService } from "../data/textSearchService";
 
-import {PokeHub} from "./../data/pokeHub";
-import {DetailScreen} from "./detailScreen";
-
-export class PokeGridScreen extends StatelessWidget 
-{
-    public constructor() 
-    {
+export class PokeGridScreen extends StatelessWidget {
+    public constructor() {
         super();
     }
 
-    public build(context: BuildContext) 
-    {
+    public build(context: BuildContext) {
         const pokeHubService = ScopedModel.of<PokeHubService>(context, PokeHubService.staticType);
         let pokeHub: PokeHub | undefined;
-        if (pokeHubService) 
-        {
+        if (pokeHubService) {
             pokeHub = pokeHubService.getPokeHub();
         }
+
+        const textSearchService = ScopedModel.of<TextSearchService>(context, TextSearchService.staticType);
+
         return new Scaffold({
             appBar: new AppBar({
                 title: new Text("Poke App"),
                 elevation: 0,
                 backgroundColor: Colors.cyan.swatch[500],
             }),
-            body: !pokeHub ? new Center({child: new CircularProgressIndicator()})
-                : GridView.count({
-                    crossAxisCount: 2,
-                    shrinkWrap: true,
-                    children: List.fromArray(pokeHub.pokemon).map(
-                        (poke) => new Padding({
-                            padding: EdgeInsets.all(2.0),
-                            key: new Key(poke.name),
-                            child: new InkWell({
-                                onTap: () => 
-                                {
-                                    Navigator.push(
-                                        context,
-                                        new MaterialPageRoute({
-                                            builder: () => new DetailScreen(poke)
-                                        })
-                                    );
-                                },
-                                child:
-                                    new Hero({
-                                        tag: poke.img,
-                                        child:
-                                            new Card({
-                                                child: new Column({
-                                                    mainAxisSize: MainAxisSize.min,
-                                                    mainAxisAlignment: MainAxisAlignment.start,
-                                                    children: [
-                                                        new Container({
-                                                            child: Image.network(poke.img, {
-                                                                width: 120,
-                                                                height: 120,
-                                                            })
-                                                        }),
-                                                        new Text(poke.name, {
-                                                            style: new TextStyle({
-                                                                fontSize: 20.0,
-                                                                fontWeight: FontWeight.bold
-                                                            })
-                                                        })
-                                                    ]
-                                                })
-                                            })
-                                    })
+            body: !pokeHub ? new Center({ child: new CircularProgressIndicator() })
+                :
+                ListView.create({
+                    children: List.fromArray([
+                        new Padding({
+                            padding: EdgeInsets.all(20),
+                            child: new TextFormField({
+                                onChanged: (val) => {
+                                    if (textSearchService) {
+                                        textSearchService.updateSearchText(val);
+                                    }
+                                }
                             })
+                        }),
+                        GridView.count({
+                            crossAxisCount: 2,
+                            shrinkWrap: true,
+                            children: List.fromArray(pokeHub.pokemon).where((poke) => {
+                                if(textSearchService){
+                                    const searchText = textSearchService.getSearchText();
+                                    if(searchText !== undefined && searchText != ""){
+                                        if(textSearchService.getSearchText() != poke.name.toLowerCase()){
+                                            return false;
+                                        }
+                                    }
+                                }
+                                return true;
+                            }).map((poke) => new PokeCard(poke))
                         })
-                    )
+                    ])
                 })
         });
     }
