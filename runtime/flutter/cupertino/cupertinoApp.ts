058b6c36d3a718cdfb9e2ad83fe84470476ce01e
chgibb <chris.gibb@outlook.com>
2020-07-29 19:16:54 -0400

    initial cupertinoapp

---
+++ runtime/flutter/cupertino/cupertinoApp.ts
@@ -0,0 +1,35 @@
+import { BuildContext } from "../buildContext";
+import { RuntimeBaseClass } from "../../runtimeBaseClass";
+import { Type } from "../../dart/core/type";
+
+import { Widget } from "./../widget";
+import { StatelessWidget } from "./../widgets/statelessWidget";
+
+interface CupertinoAppProps {
+    initialRoute?: string | undefined;
+    home: Widget;
+    title?: string | undefined;
+    routes?: {
+        [index: string]: (context: BuildContext) => Widget;
+    } | undefined;
+}
+
+declare const flutter: {
+    cupertino: {
+        cupertinoApp: (this: void, props: CupertinoAppProps) => CupertinoApp;
+    }
+}
+
+export class CupertinoApp extends StatelessWidget implements RuntimeBaseClass {
+    public readonly internalRuntimeType = new Type(CupertinoApp);
+    public props: CupertinoAppProps;
+    public constructor(props: CupertinoAppProps) {
+        super();
+        this.props = props;
+        this.props.title = this.props.title != undefined ? this.props.title : "";
+    }
+
+    public build() {
+        return flutter.cupertino.cupertinoApp(this.props);
+    }
+}
