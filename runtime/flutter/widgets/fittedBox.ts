4d8a95528aae718dcef67a2d7980498d9b2b4d47
chgibb <chris.gibb@outlook.com>
2020-08-02 18:55:11 -0400

    fittedbox

---
+++ runtime/flutter/widgets/fittedBox.ts
@@ -0,0 +1,43 @@
+import { Type } from "../../dart/core/type";
+import { Key } from "./../foundation/key"
+import { Clip } from "../painting/clip";
+import { BoxFit } from "../painting/boxFit";
+import { Alignment } from "../painting/alignment";
+import { RuntimeBaseClass } from "../../runtimeBaseClass";
+import { StatelessWidget } from "./statelessWidget";
+import { Widget } from "../widget";
+
+
+interface FittedBoxProps {
+    key?: Key | undefined;
+    fit?: BoxFit | undefined;
+    alignment?: Alignment | undefined;
+    child: Widget;
+}
+
+declare const flutter: {
+    widgets: {
+        fittedBox: (this: void, props: FittedBoxProps) => FittedBox;
+    };
+};
+
+export class FittedBox extends StatelessWidget implements RuntimeBaseClass {
+    public readonly internalRuntimeType = new Type(FittedBox);
+    public props: FittedBoxProps;
+    public constructor(props: FittedBoxProps) {
+        super();
+        this.props = props;
+
+        if (this.props.fit === undefined) {
+            this.props.fit = BoxFit.contain;
+        }
+
+        if (this.props.alignment === undefined) {
+            this.props.alignment = Alignment.center;
+        }
+    }
+
+    public build() {
+        return flutter.widgets.fittedBox(this.props);
+    }
+}
