22bbe916195c06fa669b15b2b69c066ea9b68403
Chris Gibb <chris.gibb@outlook.com>
2020-08-06 18:54:32 -0400

    remove global http override in test

--- test/widget/cupertinoContextMenu-2_test.dart
+++ test/widget/cupertinoContextMenu-2_test.dart
@@ -1,9 +1,12 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:hydro_sdk/integrationTestHarness.dart';
 
 void main() {
   testWidgets('center widget test', (WidgetTester tester) async {
+    HttpOverrides.global = null;
     await tester.pumpWidget(
         integrationTestHarness("assets/test/widget/cupertinoContextMenu-2.ts"));
     await tester.pumpAndSettle();
