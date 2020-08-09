ebb77e12e9c62186ce3ef70a9e1ac90c4bb97995
Chris Gibb <chris.gibb@outlook.com>
2020-08-08 23:14:12 -0400

    give default top level namespaces

--- lib/cfr/preloadCustomNamespaces.dart
+++ lib/cfr/preloadCustomNamespaces.dart
@@ -1,3 +1,4 @@
+import 'package:hydro_sdk/cfr/vm/table.dart';
 import 'package:hydro_sdk/hydroState.dart';
 import 'package:flutter/material.dart';
 import 'package:meta/meta.dart';
@@ -8,6 +9,13 @@ mixin PreloadableCustomNamespaces<T extends StatefulWidget> on State<T> {
   List<CustomNamespaceLoader> customNamespaceLoaders;
 
   void preloadCustomNamespaces({@required HydroState hydroState}) {
+    hydroState.context.env["com"] = HydroTable();
+    hydroState.context.env["edu"] = HydroTable();
+    hydroState.context.env["gov"] = HydroTable();
+    hydroState.context.env["mil"] = HydroTable();
+    hydroState.context.env["net"] = HydroTable();
+    hydroState.context.env["org"] = HydroTable();
+    
     customNamespaceLoaders?.forEach((x) => x(hydroState: hydroState));
   }
 }
