be6a4e9fa77e27cd72bfa46d794fdac1cceb48f5
Chris Gibb <chris.gibb@outlook.com>
2020-08-22 18:58:11 -0400

    stop using tstl's ts

--- compiler/src/ast/hashSourceFile.ts
+++ compiler/src/ast/hashSourceFile.ts
@@ -1,4 +1,4 @@
-import * as ts from "typescript-to-lua/node_modules/typescript";
+import * as ts from "typescript";
 import { hashText } from "./hashText";
 
 export function hashSourceFile(sourceFile: ts.SourceFile | undefined): string {
