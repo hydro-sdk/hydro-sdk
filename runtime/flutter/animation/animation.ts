3393a86803da5cdd694fff3066eb4cc619b91a1d
Chris Gibb <chris.gibb@outlook.com>
2020-08-29 17:14:40 -0400

    provide a vm box for animations

--- runtime/flutter/animation/animation.ts
+++ runtime/flutter/animation/animation.ts
@@ -5,5 +5,5 @@ import { double } from "../../dart/core/double";
 export class Animation<T extends double | Color>
 {
     public readonly runtimeType = new Type(Animation);
-    public readonly value: T extends double ? number : T = undefined as any;
+    public readonly value: () => T extends double ? number : T = undefined as any;
 }
