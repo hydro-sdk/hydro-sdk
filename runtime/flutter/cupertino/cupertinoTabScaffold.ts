31a1a586839af401e310ce4c80cd6092e4202913
Chris Gibb <chris.gibb@outlook.com>
2020-09-04 19:54:05 -0400

    cupertino tab scaffold

---
+++ runtime/flutter/cupertino/cupertinoTabScaffold.ts
@@ -0,0 +1,37 @@
+import { RuntimeBaseClass } from "../../runtimeBaseClass";
+import { Type } from "../../dart/core/type";
+import { Color } from "../../dart/ui/color";
+
+import { StatelessWidget } from "./../widgets/statelessWidget";
+import { IndexedWidgetBuilder } from "./../widgets/indexedWidgetBuilder";
+import { Key } from "../foundation/key";
+import { CupertinoTabBar } from "./cupertinoTabBar";
+import { CupertinoTabController } from "./cupertinoTabController";
+
+export interface CupertinoTabScaffoldProps {
+    key?: Key | undefined;
+    tabBar: CupertinoTabBar;
+    tabBuilder: IndexedWidgetBuilder;
+    controller?: CupertinoTabController | undefined;
+    backgroundColor?: Color | undefined;
+    resizeToAvoidBottomInset?: boolean | undefined;
+}
+
+declare const flutter: {
+    cupertino: {
+        cupertinoTabScaffold: (this: void, props: CupertinoTabScaffoldProps) => CupertinoTabScaffold;
+    }
+}
+
+export class CupertinoTabScaffold extends StatelessWidget implements RuntimeBaseClass {
+    public readonly internalRuntimeType = new Type(CupertinoTabScaffold);
+    public props: CupertinoTabScaffoldProps;
+    public constructor(props: CupertinoTabScaffoldProps) {
+        super();
+        this.props = props;
+    }
+
+    public build() {
+        return flutter.cupertino.cupertinoTabScaffold(this.props);
+    }
+}
