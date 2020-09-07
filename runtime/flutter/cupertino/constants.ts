269b6b5d86b941437e6aec4c5a367f25b8f66264
Chris Gibb <chris.gibb@outlook.com>
2020-09-07 14:51:29 -0400

    cupertino navigation bar

--- runtime/flutter/cupertino/constants.ts
+++ runtime/flutter/cupertino/constants.ts
@@ -1,10 +1,25 @@
 import { CupertinoColors } from "./cupertinoColors";
 import { CupertinoDynamicColor } from "./cupertinoDynamicColor";
 import { Color } from "../../dart/ui/color";
+import { Border } from "../painting/border";
+import { BorderSide } from "../painting/borderSide";
+import { BorderStyle } from "../painting/borderStyle";
 
 export const kMinInteractiveDimensionCupertino = 44.0;
+
 export const kDefaultTabBarBorderColor = CupertinoDynamicColor.withBrightness({
     color: new Color(0x4C000000),
     darkColor: new Color(0x29000000),
 });
+
 export const kDefaultTabBarInactiveColor = CupertinoColors.inactiveGray.color;
+
+export const kDefaultNavBarBorderColor = new Color(0x4D000000);
+
+export const kDefaultNavBarBorder = new Border({
+    bottom: new BorderSide({
+        color: kDefaultNavBarBorderColor,
+        width: 0.0,
+        style: BorderStyle.solid,
+    })
+});
