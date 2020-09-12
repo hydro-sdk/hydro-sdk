99538b291f4c532c31e8be785f54e9db5526a67e
Chris Gibb <chris.gibb@outlook.com>
2020-09-12 12:18:48 -0400

    flesh out textstyle

--- runtime/flutter/painting/textStyle.ts
+++ runtime/flutter/painting/textStyle.ts
@@ -1,13 +1,33 @@
-import {JITAllocatingRTManagedBox} from "../../syntheticBox";
-import {Color} from "../../dart/ui/color";
-import {FontWeight} from "../../dart/ui/fontWeight";
-import {RuntimeBaseClass} from "../../runtimeBaseClass";
-import {Type} from "../../dart/core/type";
+import { JITAllocatingRTManagedBox } from "../../syntheticBox";
+import { RuntimeBaseClass } from "../../runtimeBaseClass";
+import { Type } from "../../dart/core/type";
+import { Color } from "../../dart/ui/color";
+import { FontWeight } from "../../dart/ui/fontWeight";
+import { FontStyle } from "../../dart/ui/fontStyle";
+import { TextBaseline } from "../../dart/ui/textBaseline";
+import { TextDecorationStyle } from "../../dart/ui/textDecorationStyle";
 
 export interface TextStyleProps {
-    fontSize?: number | undefined;
+    inherit?: boolean | undefined;
     color?: Color | undefined;
+    backgroundColor?: Color | undefined;
+    fontSize?: number | undefined;
     fontWeight?: FontWeight | undefined;
+    fontStyle?: FontStyle | undefined;
+    letterSpacing?: number | undefined;
+    wordSpacing?: number | undefined;
+    textBaseLine?: TextBaseline | undefined;
+    height?: number | undefined;
+    //@todo TextStyleProps#locale : Locale
+    //@todo TextStyleProps#foreground : Paint
+    //@todo TextStyleProps#background : Paint
+    //@todo TextStyleProps#shadows : List<Shadow>
+    //@todo TextStyleProps#fontFeatures : List<FontFeature>
+    //@todo TextStyleProps#decoration : TextDecoration
+    decorationColor?: Color | undefined;
+    decorationStyle?: TextDecorationStyle | undefined;
+    decorationThickness?: number | undefined;
+    debugLabel?: string | undefined;
 }
 
 declare const flutter: {
@@ -16,32 +36,27 @@ declare const flutter: {
     };
 };
 
-export class TextStyle extends JITAllocatingRTManagedBox<TextStyleProps, TextStyle> implements RuntimeBaseClass 
-{
+export class TextStyle extends JITAllocatingRTManagedBox<TextStyleProps, TextStyle> implements RuntimeBaseClass {
     public readonly internalRuntimeType = new Type(TextStyle);
     public props: TextStyleProps;
     public color?: Color;
-    public constructor(props: TextStyleProps) 
-    {
+    public constructor(props: TextStyleProps) {
         super();
         this.props = props;
     }
 
-    public copyWith(props: { color: Color }): TextStyle 
-    {
+    public copyWith(props: { color: Color }): TextStyle {
         return {} as any;
     }
 
     public apply(props: {
         color?: Color;
         fontWeightDelta?: number;
-    }): TextStyle 
-    {
+    }): TextStyle {
         return {} as any;
     }
 
-    public unwrap(): TextStyle 
-    {
+    public unwrap(): TextStyle {
         return flutter.painting.textStyle(this.props);
     }
 }
