b1cb723bf6d58b2af6114f33936573ac1235eb4a
Chris Gibb <chris.gibb@outlook.com>
2020-09-18 18:45:42 -0400

    cupertino text field

--- runtime/flutter/cupertino/constants.ts
+++ runtime/flutter/cupertino/constants.ts
@@ -1,9 +1,12 @@
-import { CupertinoColors } from "./cupertinoColors";
-import { CupertinoDynamicColor } from "./cupertinoDynamicColor";
+import { Radius } from "../../dart/ui/radius";
 import { Color } from "../../dart/ui/color";
 import { Border } from "../painting/border";
 import { BorderSide } from "../painting/borderSide";
 import { BorderStyle } from "../painting/borderStyle";
+import { BoxDecoration } from "../painting/boxDecoration";
+import { BorderRadius } from "../painting/borderRadius";
+import { CupertinoColors } from "./cupertinoColors";
+import { CupertinoDynamicColor } from "./cupertinoDynamicColor";
 
 export const kMinInteractiveDimensionCupertino = 44.0;
 
@@ -22,4 +25,29 @@ export const kDefaultNavBarBorder = new Border({
         width: 0.0,
         style: BorderStyle.solid,
     })
+});
+
+export const kDefaultRoundedBorderSide = new BorderSide({
+    color: CupertinoDynamicColor.withBrightness({
+        color: new Color(0x33000000),
+        darkColor: new Color(0x33FFFFFF),
+    }).color,
+    style: BorderStyle.solid,
+    width: 0.0,
+});
+
+export const kDefaultRoundedBorder = new Border({
+    top: kDefaultRoundedBorderSide,
+    bottom: kDefaultRoundedBorderSide,
+    left: kDefaultRoundedBorderSide,
+    right: kDefaultRoundedBorderSide,
+});
+
+export const kDefaultRoundedBorderDecoration = new BoxDecoration({
+    color: CupertinoDynamicColor.withBrightness({
+        color: CupertinoColors.white,
+        darkColor: CupertinoColors.black,
+    }).color,
+    border: kDefaultRoundedBorder,
+    borderRadius: BorderRadius.all(Radius.circular(5.0)),
 });
