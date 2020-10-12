c079cb752de5b08aa9b5646acb8410efd1e7897c
Chris Gibb <chris.gibb@outlook.com>
2020-10-12 13:10:00 -0400

    static const function invocation transforms

--- lib/swid/transforms/ts/transformStaticConstFunctionInvocation.dart
+++ lib/swid/transforms/ts/transformStaticConstFunctionInvocation.dart
@@ -1,14 +1,35 @@
 import 'package:hydro_sdk/swid/ir/swidStaticConstFunctionInvocation.dart';
+import 'package:hydro_sdk/swid/transforms/ts/transformNamedParametersToTs.dart';
 import 'package:hydro_sdk/swid/transforms/ts/transformNormalParametersToTs.dart';
 import 'package:meta/meta.dart';
 
 String transformStaticConstFunctionInvocation(
-        {@required
-            SwidStaticConstFunctionInvocation
-                swidStaticConstFunctionInvocation}) =>
-    (swidStaticConstFunctionInvocation.isConstructorInvocation ? "new " : "") +
-    swidStaticConstFunctionInvocation.value +
-    "(" +
-    transformNormalParametersToTs(
-        swidLiterals: swidStaticConstFunctionInvocation.normalParameters) + 
-    ")";
+    {@required
+        SwidStaticConstFunctionInvocation swidStaticConstFunctionInvocation}) {
+  var normalParameters = transformNormalParametersToTs(
+      swidLiterals: swidStaticConstFunctionInvocation.normalParameters);
+
+  var res =
+      swidStaticConstFunctionInvocation.isConstructorInvocation ? "new " : "";
+  res += swidStaticConstFunctionInvocation.value + "(";
+
+  if (normalParameters?.isNotEmpty ?? false) {
+    res += normalParameters;
+  }
+
+  var namedParameters = transformNamedParametersToTs(
+      namedParameters: swidStaticConstFunctionInvocation.namedParameters);
+
+  if ((normalParameters?.isNotEmpty ?? false) &&
+      (namedParameters?.isNotEmpty ?? false)) {
+    res += ",";
+  }
+
+  if (namedParameters?.isNotEmpty ?? false) {
+    res += namedParameters;
+  }
+
+  res += ")";
+  
+  return res;
+}
---
+++ test/swid/transforms/staticConst/staticConstFunctionInvocation_test.dart
@@ -0,0 +1,62 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:hydro_sdk/swid/ir/swidIntegerLiteral.dart';
+import 'package:hydro_sdk/swid/ir/swidStaticConstFunctionInvocation.dart';
+import 'package:hydro_sdk/swid/ir/swidStringLiteral.dart';
+import 'package:hydro_sdk/swid/transforms/ts/transformStaticConstFunctionInvocation.dart';
+
+void main() {
+  LiveTestWidgetsFlutterBinding();
+  testWidgets('', (WidgetTester tester) async {
+    var normal = SwidStaticConstFunctionInvocation(
+        value: "IconData",
+        normalParameters: [SwidIntegerLiteral(value: "0xe52a")],
+        namedParameters: {},
+        isConstructorInvocation: false);
+    expect(
+        transformStaticConstFunctionInvocation(
+            swidStaticConstFunctionInvocation: normal),
+        "IconData(0xe52a)");
+
+    var manyNormal = SwidStaticConstFunctionInvocation(
+        value: "IconData",
+        normalParameters: [
+          SwidIntegerLiteral(value: "0xe52a"),
+          SwidStringLiteral(value: "foo"),
+          SwidStringLiteral(value: "bar"),
+          SwidIntegerLiteral(value: "123"),
+        ],
+        namedParameters: {},
+        isConstructorInvocation: false);
+    expect(
+        transformStaticConstFunctionInvocation(
+            swidStaticConstFunctionInvocation: manyNormal),
+        "IconData(0xe52a,\"foo\",\"bar\",123)");
+
+    var manyNamed = SwidStaticConstFunctionInvocation(
+        value: "IconData",
+        normalParameters: [],
+        namedParameters: {
+          "foo": SwidStringLiteral(value: "foo"),
+          "bar": SwidStringLiteral(value: "bar"),
+          "offset": SwidIntegerLiteral(value: "123"),
+        },
+        isConstructorInvocation: false);
+    expect(
+        transformStaticConstFunctionInvocation(
+            swidStaticConstFunctionInvocation: manyNamed),
+        "IconData({foo:\"foo\",bar:\"bar\",offset:123})");
+
+    var normalAndNamedCtor = SwidStaticConstFunctionInvocation(
+        value: "IconData",
+        normalParameters: [SwidIntegerLiteral(value: "0xe52a")],
+        namedParameters: {
+          "fontFamily": SwidStringLiteral(value: "MaterialIcons")
+        },
+        isConstructorInvocation: true);
+
+    expect(
+        transformStaticConstFunctionInvocation(
+            swidStaticConstFunctionInvocation: normalAndNamedCtor),
+        "new IconData(0xe52a,{fontFamily:\"MaterialIcons\"})");
+  });
+}
