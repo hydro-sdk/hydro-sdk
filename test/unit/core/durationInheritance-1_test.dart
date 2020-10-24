a1fbb34a3aaa07a96fa0863b547682156f91207e
Chris Gibb <chris.gibb@outlook.com>
2020-10-23 20:19:24 -0400

    initial binding inheritance test for duration

---
+++ test/unit/core/durationInheritance-1_test.dart
@@ -0,0 +1,23 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:hydro_sdk/cfr/builtins/loadBuiltins.dart';
+import 'package:hydro_sdk/unitTestHarness.dart';
+
+void main() {
+  testWidgets('', (WidgetTester tester) async {
+    var res = await unitTestHarness(
+        path: "assets/test/unit/core/durationInheritance-1.ts.hc",
+        libs: [
+          BuiltinLib.dart,
+          BuiltinLib.base,
+          BuiltinLib.string,
+          BuiltinLib.table,
+          BuiltinLib.math
+        ]);
+
+    if (!res.success) {
+      print(res.values[0]);
+    }
+
+    expect(res.success, true);
+  });
+}
