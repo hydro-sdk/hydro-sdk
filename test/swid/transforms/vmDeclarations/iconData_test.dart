9f73f49866d60ebe6696fd6096779104928019de
Chris Gibb <chris.gibb@outlook.com>
2020-10-31 15:18:39 -0400

    wip test for vm declaration emit

---
+++ test/swid/transforms/vmDeclarations/iconData_test.dart
@@ -0,0 +1,24 @@
+import 'dart:io';
+import 'dart:convert';
+
+import 'package:flutter_test/flutter_test.dart';
+import 'package:hydro_sdk/swid/ir/backend/ts/tsClassVmDeclaration.dart';
+import 'package:hydro_sdk/swid/ir/frontend/dart/swidClass.dart';
+
+void main() {
+  LiveTestWidgetsFlutterBinding();
+  testWidgets('', (WidgetTester tester) async {
+    var iconDataClass = SwidClass.fromJson(
+        json.decode(File("test/swid/res/IconData.json").readAsStringSync()));
+
+    expect(iconDataClass.instanceFieldDeclarations.length, 4);
+    expect(
+        TsClassVmDeclaration(swidClass: iconDataClass).toTsSource(),
+        """
+    public readonly codePoint: number = undefined as any;
+    public readonly fontFamily: string = undefined as any;
+    public readonly fontPackage: string = undefined as any;
+    public readonly matchTextDirection: boolean = undefined as any;
+""");
+  });
+}
