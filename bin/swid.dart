a1731417e1d5a7c7e2e5ecf08c20b4326f572d67
Chris Gibb <chris.gibb@outlook.com>
2020-11-07 12:24:52 -0500

    fix awaits when writing translation units. Manually patch nullability for known types before emit

--- bin/swid.dart
+++ bin/swid.dart
@@ -1,5 +1,6 @@
 import 'package:hydro_sdk/swid/ir/backend/translationUnitProducer.dart';
 import 'package:hydro_sdk/swid/ir/backend/writeTranslationUnit.dart';
+import 'package:hydro_sdk/swid/ir/frontend/dart/fixupNullability.dart';
 import 'package:hydro_sdk/swid/swid.dart';
 import 'package:hydro_sdk/swid/transforms/transformPackageUri.dart';
 import 'package:hydro_sdk/swid/transforms/transformToCamelCase.dart';
@@ -9,32 +10,35 @@ void main() async {
       await swid(["/Users/chgibb/Downloads/flutter/packages/flutter"]);
 
   for (var i = 0; i != visitor.enums.length; ++i) {
-    TranslationUnitProducer(
-      path: transformPackageUri(
-        packageUri: visitor.enums[i].originalPackagePath,
-      ),
-      baseFileName: "${transformToCamelCase(str: visitor.enums[i].identifier)}",
-      tsPrefixPaths: ["runtime"],
-      dartPrefixPaths: [],
-    )
-        .produceFromSwidEnum(swidEnum: visitor.enums[i])
-        ?.forEach((x) => writeTranslationUnit(translationUnit: x));
+    await Future.forEach(
+        TranslationUnitProducer(
+          path: transformPackageUri(
+            packageUri: visitor.enums[i].originalPackagePath,
+          ),
+          baseFileName:
+              "${transformToCamelCase(str: visitor.enums[i].identifier)}",
+          tsPrefixPaths: ["runtime"],
+          dartPrefixPaths: [],
+        ).produceFromSwidEnum(swidEnum: visitor.enums[i]),
+        (x) => writeTranslationUnit(translationUnit: x));
   }
 
   for (var i = 0; i != visitor.classes.length; ++i) {
     if (visitor.classes[i].name == "Icons" ||
         visitor.classes[i].name == "CupertinoIcons" ||
         visitor.classes[i].name == "IconData") {
-      TranslationUnitProducer(
-        path: transformPackageUri(
-          packageUri: visitor.classes[i].originalPackagePath,
-        ),
-        baseFileName: "${transformToCamelCase(str: visitor.classes[i].name)}",
-        tsPrefixPaths: ["runtime"],
-        dartPrefixPaths: ["lib", "cfr", "builtins", "libs"],
-      )
-          .produceFromSwidClass(swidClass: visitor.classes[i])
-          ?.forEach((x) => writeTranslationUnit(translationUnit: x));
+      await Future.forEach(
+          TranslationUnitProducer(
+            path: transformPackageUri(
+              packageUri: visitor.classes[i].originalPackagePath,
+            ),
+            baseFileName:
+                "${transformToCamelCase(str: visitor.classes[i].name)}",
+            tsPrefixPaths: ["runtime"],
+            dartPrefixPaths: ["lib", "cfr", "builtins", "libs"],
+          ).produceFromSwidClass(
+              swidClass: fixupNullability(swidClass: visitor.classes[i])),
+          (x) => writeTranslationUnit(translationUnit: x));
     }
   }
 }
