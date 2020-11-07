6389a466be42b417f87069fd3859770ed449945d
Chris Gibb <chris.gibb@outlook.com>
2020-11-07 12:07:41 -0500

    generate bindings for icondata

--- runtime/flutter/widgets/iconData.ts
+++ runtime/flutter/widgets/iconData.ts
@@ -1,35 +1,28 @@
-import {Widget} from "../widget";
-import {RuntimeBaseClass} from "../../runtimeBaseClass";
-import {Type} from "../../dart/core/type";
-
-import {JITAllocatingRTManagedBox} from "./../../syntheticBox";
-
-export interface IconDataProps {
-    fontFamily: string;
-    matchTextDirection?: boolean | undefined;
-    fontPackage?: string | undefined;
-}
-
 declare const flutter: {
     widgets: {
-        iconData: (this: void, codePoint: number, props: IconDataProps) => { tag: string };
-    };
-};
-
-export class IconData extends JITAllocatingRTManagedBox<IconDataProps, Widget> implements RuntimeBaseClass 
-{
-    public readonly internalRuntimeType = new Type(IconData);
-    public codePoint: number;
-    public props: IconDataProps;
-    public constructor(codePoint: number, props: IconDataProps) 
-    {
-        super();
-        this.codePoint = codePoint;
-        this.props = props;
+        iconData: (this: void, iconData: IconData, codePoint: number, props: { fontFamily: string, fontPackage: string, matchTextDirection: boolean, }) => IconData
     }
-
-    public unwrap(): Widget 
-    {
-        return flutter.widgets.iconData(this.codePoint, this.props);
+};
+const iconDataDefaultProps = {
+    matchTextDirection: false,
+};
+export class IconData {
+    public readonly codePoint: number = undefined as any;
+    public readonly fontFamily: string = undefined as any;
+    public readonly fontPackage: string = undefined as any;
+    public readonly matchTextDirection: boolean = undefined as any;
+    public constructor(codePoint: number, props: { fontFamily: string, fontPackage: string, matchTextDirection?: boolean, }) {
+        flutter.widgets.iconData(this, codePoint, {
+            ...iconDataDefaultProps,
+            ...props
+        });
+    }
+    private readonly _dart_getHashCode: () => number = undefined as any;
+    private readonly _dart_toString: () => string = undefined as any;
+    public getHashCode() {
+        return this._dart_getHashCode();
+    }
+    public toString() {
+        return this._dart_toString();
     }
 }
