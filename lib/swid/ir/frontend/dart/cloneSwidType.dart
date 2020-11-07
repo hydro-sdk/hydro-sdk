11ed35fc0e11065b49054b1c463a17bb749c04d1
Chris Gibb <chris.gibb@outlook.com>
2020-11-07 12:24:06 -0500

    allow changing nullability when cloning generic types

--- lib/swid/ir/frontend/dart/cloneSwidType.dart
+++ lib/swid/ir/frontend/dart/cloneSwidType.dart
@@ -2,19 +2,30 @@ import 'package:hydro_sdk/swid/ir/frontend/dart/swidClass.dart';
 import 'package:hydro_sdk/swid/ir/frontend/dart/swidDefaultFormalParameter.dart';
 import 'package:hydro_sdk/swid/ir/frontend/dart/swidFunctionType.dart';
 import 'package:hydro_sdk/swid/ir/frontend/dart/swidInterface.dart';
+import 'package:hydro_sdk/swid/ir/frontend/dart/swidNullabilitySuffix.dart';
 import 'package:hydro_sdk/swid/ir/frontend/dart/swidType.dart';
 import 'package:meta/meta.dart';
 
-SwidType cloneSwidType({@required SwidType swidType}) => swidType.map(
+SwidType cloneSwidType({
+  @required SwidType swidType,
+  SwidNullabilitySuffix swidNullabilitySuffix,
+}) =>
+    swidType.map(
       fromSwidInterface: (val) => SwidType.fromSwidInterface(
-          swidInterface: SwidInterface.clone(swidType: val.swidInterface)),
+          swidInterface: SwidInterface.clone(
+              swidType: val.swidInterface,
+              nullabilitySuffix: swidNullabilitySuffix)),
       fromSwidClass: (val) => SwidType.fromSwidClass(
-          swidClass: SwidClass.clone(swidClass: val.swidClass)),
+          swidClass: SwidClass.clone(
+              swidClass: val.swidClass,
+              nullabilitySuffix: swidNullabilitySuffix)),
       fromSwidDefaultFormalParameter: (val) =>
           SwidType.fromSwidDefaultFormalParameter(
               swidDefaultFormalParameter: SwidDefaultFormalParameter.clone(
-                  swidType: val.swidDefaultFormalParameter)),
+        swidType: val.swidDefaultFormalParameter,
+      )),
       fromSwidFunctionType: (val) => SwidType.fromSwidFunctionType(
-          swidFunctionType:
-              SwidFunctionType.clone(swidFunctionType: val.swidFunctionType)),
+          swidFunctionType: SwidFunctionType.clone(
+              swidFunctionType: val.swidFunctionType,
+              nullabilitySuffix: swidNullabilitySuffix)),
     );
