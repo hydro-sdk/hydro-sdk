09438a15cfb5aba8e04a987dbc30d51b4b4fe895
Chris Gibb <chris.gibb@outlook.com>
2020-11-08 22:09:33 -0500

    utility to determine if a frontend type represents a dart primitive

---
+++ lib/swid/ir/frontend/dart/isPrimitive.dart
@@ -0,0 +1,16 @@
+import 'package:hydro_sdk/swid/ir/frontend/dart/swidType.dart';
+import 'package:meta/meta.dart';
+
+bool isPrimitive({@required SwidType swidType}) => swidType.when(
+      fromSwidInterface: (val) =>
+          val.originalPackagePath == "dart:core" &&
+          (val.name == "String" ||
+              val.name == "String*" ||
+              val.name == "bool" ||
+              val.name == "bool*" ||
+              val.name == "int" ||
+              val.name == "int*"),
+      fromSwidClass: (_) => false,
+      fromSwidDefaultFormalParameter: (_) => false,
+      fromSwidFunctionType: (_) => false,
+    );
