9a0407d7b15fd216b307f92b96a97e16472f5582
Chris Gibb <chris.gibb@outlook.com>
2020-11-15 14:26:35 -0500

    test for unboxing positional parameters in self binding invocation

---
+++ test/swid/transforms/dart/methodInjectionImplementation/diagnosticsNodeIsFiltered_test.dart
@@ -0,0 +1,26 @@
+import 'dart:io';
+import 'dart:convert';
+
+import 'package:flutter_test/flutter_test.dart';
+import 'package:hydro_sdk/swid/ir/backend/dart/methodInjectionImplementation.dart';
+import 'package:hydro_sdk/swid/ir/frontend/dart/swidClass.dart';
+
+void main() {
+  LiveTestWidgetsFlutterBinding();
+  testWidgets('', (WidgetTester tester) async {
+    var diagnosticsNodeClass = SwidClass.fromJson(json
+        .decode(File("../test/swid/res/DiagnosticsNode.json").readAsStringSync()));
+
+    expect(
+        MethodInjectionImplementation(
+            swidFunctionType: diagnosticsNodeClass.methods
+                .firstWhere((x) => x.name == "isFiltered")).toDartSource(),
+"""
+table['_dart_isFiltered'] = makeLuaDartFunc(func: (List<dynamic> args) {
+  return [
+    super.isFiltered(
+        maybeUnBoxEnum(values: DiagnosticLevel.values, boxedEnum: args[1]))
+  ];
+});""");
+  }, tags: "swid");
+}
