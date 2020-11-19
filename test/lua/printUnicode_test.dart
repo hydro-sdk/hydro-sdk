218a74f5472c5a471cb40d7876b0c1edc4dd1f7d
Chris Gibb <chris.gibb@outlook.com>
2020-11-19 17:11:00 -0500

    test for unicode printing

---
+++ test/lua/printUnicode_test.dart
@@ -0,0 +1,30 @@
+import 'package:hydro_sdk/cfr/vm/context.dart';
+import 'package:hydro_sdk/hydroState.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  test('', () async {
+    var state = HydroState();
+
+    List<String> outLines = [];
+
+    state.context.env["print"] = (List<dynamic> args) {
+      outLines.add(args
+          .map((a) => Context.luaToString(a, hydroState: state).toString())
+          .join("\t"));
+    };
+
+    List<String> expectedOutLines = [
+      "你好，世界",
+    ];
+
+    var res = await state.doFile("lua/printUnicode.hc");
+
+    expect(res.success, true);
+    expect(outLines.length, expectedOutLines.length);
+
+    for (var i = 0; i != outLines.length; ++i) {
+      expect(outLines[i], expectedOutLines[i]);
+    }
+  }, tags: "lua");
+}
