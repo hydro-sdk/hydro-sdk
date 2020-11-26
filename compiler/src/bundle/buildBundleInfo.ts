22d085df661b355c8fb22d5ce1824f58a62d5b7a
Chris Gibb <chris.gibb@outlook.com>
2020-11-25 21:50:08 -0500

    format

--- compiler/src/bundle/buildBundleInfo.ts
+++ compiler/src/bundle/buildBundleInfo.ts
@@ -76,10 +76,10 @@ export async function buildBundleInfo(
 
     const sourceFilesToTranspile = oldEntries
         ? sourceFiles.filter(
-            (x) =>
-                hashSourceFile(x) !=
-                (oldEntries[x.fileName]?.originalFileHash ?? "")
-        )
+              (x) =>
+                  hashSourceFile(x) !=
+                  (oldEntries[x.fileName]?.originalFileHash ?? "")
+          )
         : sourceFiles;
 
     let currentStep = Math.abs(
@@ -93,17 +93,17 @@ export async function buildBundleInfo(
     ) =>
         newDiagnostics && newDiagnostics.length
             ? (res.diagnostics = [
-                ...res.diagnostics,
-                ...newDiagnostics.map((x) => x),
-            ])
+                  ...res.diagnostics,
+                  ...newDiagnostics.map((x) => x),
+              ])
             : undefined;
 
     const getFullDiagnostics = () => {
         let diagnostics:
             | Readonly<Array<ts.Diagnostic>>
             | Readonly<
-                Array<ts.DiagnosticWithLocation>
-            > = program.getSemanticDiagnostics();
+                  Array<ts.DiagnosticWithLocation>
+              > = program.getSemanticDiagnostics();
         concatDiagnostics(diagnostics);
 
         diagnostics = program.getDeclarationDiagnostics();
@@ -114,8 +114,8 @@ export async function buildBundleInfo(
         let diagnostics:
             | Readonly<Array<ts.Diagnostic>>
             | Readonly<
-                Array<ts.DiagnosticWithLocation>
-            > = program.getSyntacticDiagnostics(sourceFile);
+                  Array<ts.DiagnosticWithLocation>
+              > = program.getSyntacticDiagnostics(sourceFile);
         concatDiagnostics(diagnostics);
 
         diagnostics = program.getSemanticDiagnostics(sourceFile);
@@ -136,7 +136,7 @@ export async function buildBundleInfo(
             debugInfo.forEach((k) => {
                 if (
                     x.symbolFullyQualifiedMangleName ==
-                    k.symbolFullyQualifiedMangleName &&
+                        k.symbolFullyQualifiedMangleName &&
                     x.originalLineStart != k.originalLineStart &&
                     x.originalColumnStart != k.originalColumnStart
                 ) {
@@ -217,11 +217,12 @@ export async function buildBundleInfo(
             res.entries[transpiledFile.fileName] = {
                 debugSymbols: debugInfo,
                 moduleText: transpiledFile.lua!,
-                moduleName: `${makeRelativePath(transpiledFile.fileName)
+                moduleName: `${
+                    makeRelativePath(transpiledFile.fileName)
                         .split(path.sep)
                         .join(".")
                         .split(".ts")[0]
-                    }`,
+                }`,
                 originalFileName: transpiledFile.fileName,
                 originalFileHash: hashSourceFile(
                     sourceFilesToTranspile.find(
