8f9bc404e617ff52fbd4d25ed49f4352b69ed4e1
Chris Gibb <chris.gibb@outlook.com>
2020-12-12 15:47:55 -0500

    boxer registrant

---
+++ lib/swid/ir/backend/dart/dartVmManagedClassBoxerRegistrant.dart
@@ -0,0 +1,47 @@
+import 'package:code_builder/code_builder.dart'
+    show refer, Block, Method, Parameter, TypeReference, DartEmitter;
+import 'package:hydro_sdk/swid/ir/frontend/dart/swidClass.dart';
+import 'package:meta/meta.dart';
+
+class DartVMManagedClassBoxerRegistrant {
+  final SwidClass swidClass;
+
+  DartVMManagedClassBoxerRegistrant({@required this.swidClass});
+
+  String toDartSource() => refer("registerBoxer")
+      .call([], {
+        "boxer": Method((m) => m
+          ..optionalParameters.addAll([
+            Parameter((p) => p
+              ..named = true
+              ..annotations.add(refer("required"))
+              ..name = "vmObject"
+              ..type = TypeReference((t) => t..symbol = swidClass.name)),
+            Parameter((p) => p
+              ..named = true
+              ..annotations.add(refer("required"))
+              ..name = "hydroState"
+              ..type = TypeReference((t) => t..symbol = "HydroState")),
+            Parameter((p) => p
+              ..named = true
+              ..annotations.add(refer("required"))
+              ..name = "table"
+              ..type = TypeReference((t) => t..symbol = "HydroTable")),
+          ])
+          ..body = Block.of([
+            refer("VMManaged${swidClass.name}")
+                .call([], {
+                  "vmObject": refer("vmObject"),
+                  "hydroState": refer("hydroState"),
+                  "table": refer("table"),
+                })
+                .returned
+                .statement
+          ])).closure
+      }, [
+        TypeReference((t) => t..symbol = swidClass.name),
+      ])
+      .statement
+      .accept(DartEmitter())
+      .toString();
+}
---
+++ test/swid/transforms/dart/vmManagedClassBoxerRegistrant_test.dart
@@ -0,0 +1,49 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:hydro_sdk/swid/ir/backend/dart/dartVmManagedClassBoxerRegistrant.dart';
+import 'package:hydro_sdk/swid/ir/frontend/dart/swidClass.dart';
+import 'package:hydro_sdk/swid/ir/frontend/dart/swidDeclarationModifiers.dart';
+import 'package:hydro_sdk/swid/ir/frontend/dart/swidFunctionType.dart';
+import 'package:hydro_sdk/swid/ir/frontend/dart/swidInterface.dart';
+import 'package:hydro_sdk/swid/ir/frontend/dart/swidNullabilitySuffix.dart';
+import 'package:hydro_sdk/swid/ir/frontend/dart/swidReferenceDeclarationKind.dart';
+import 'package:hydro_sdk/swid/ir/frontend/dart/swidType.dart';
+
+void main() {
+  LiveTestWidgetsFlutterBinding();
+  testWidgets('', (WidgetTester tester) async {
+    var ir = DartVMManagedClassBoxerRegistrant(
+        swidClass: SwidClass(
+      name: "fooClass",
+      originalPackagePath: "",
+      nullabilitySuffix: SwidNullabilitySuffix.none,
+      staticMethods: [],
+      factoryConstructors: [],
+      instanceFieldDeclarations: {},
+      constructorType: SwidFunctionType(
+        name: "",
+        namedDefaults: {},
+        namedParameterTypes: {},
+        optionalParameterNames: [],
+        normalParameterNames: [],
+        normalParameterTypes: [],
+        nullabilitySuffix: SwidNullabilitySuffix.none,
+        optionalParameterTypes: [],
+        originalPackagePath: "",
+        returnType: SwidType.fromSwidInterface(
+            swidInterface: SwidInterface(
+          name: "",
+          nullabilitySuffix: SwidNullabilitySuffix.none,
+          originalPackagePath: "",
+          referenceDeclarationKind: SwidReferenceDeclarationKind.voidType,
+          typeArguments: [],
+        )),
+        swidDeclarationModifiers: SwidDeclarationModifiers.empty(),
+      ),
+      staticConstFieldDeclarations: [],
+      methods: [],
+    ));
+
+    expect(ir.toDartSource(), 
+  """registerBoxer<fooClass>(boxer: ({@required fooClass vmObject, @required HydroState hydroState, @required HydroTable table}) { return  VMManagedfooClass(vmObject: vmObject, hydroState: hydroState, table: table); } );""");
+  });
+}
