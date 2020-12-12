bba0e22297a338ae2477c03f5567ad346c2bc4d7
Chris Gibb <chris.gibb@outlook.com>
2020-12-12 17:39:46 -0500

    test for vm managed class for empty class

---
+++ test/swid/transforms/dart/vmManagedClass/emptyClass_test.dart
@@ -0,0 +1,66 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:hydro_sdk/swid/ir/backend/dart/vmManagedClassDeclaration.dart';
+import 'package:hydro_sdk/swid/ir/frontend/dart/swidClass.dart';
+import 'package:hydro_sdk/swid/ir/frontend/dart/swidDeclarationModifiers.dart';
+import 'package:hydro_sdk/swid/ir/frontend/dart/swidFunctionType.dart';
+import 'package:hydro_sdk/swid/ir/frontend/dart/swidInterface.dart';
+import 'package:hydro_sdk/swid/ir/frontend/dart/swidNullabilitySuffix.dart';
+import 'package:hydro_sdk/swid/ir/frontend/dart/swidReferenceDeclarationKind.dart';
+import 'package:hydro_sdk/swid/ir/frontend/dart/swidType.dart';
+
+void main() {
+  LiveTestWidgetsFlutterBinding();
+  testWidgets('', (WidgetTester tester) async {
+    var ir = VMManagedClassDeclaration(
+        swidClass: SwidClass(
+      name: "fooClass",
+      originalPackagePath: "",
+      nullabilitySuffix: SwidNullabilitySuffix.none,
+      staticMethods: [],
+      factoryConstructors: [],
+      instanceFieldDeclarations: {},
+      constructorType: SwidFunctionType(
+        name: "",
+        namedDefaults: {},
+        namedParameterTypes: {},
+        optionalParameterNames: [],
+        normalParameterNames: [],
+        normalParameterTypes: [],
+        nullabilitySuffix: SwidNullabilitySuffix.none,
+        optionalParameterTypes: [],
+        originalPackagePath: "",
+        returnType: SwidType.fromSwidInterface(
+            swidInterface: SwidInterface(
+          name: "",
+          nullabilitySuffix: SwidNullabilitySuffix.none,
+          originalPackagePath: "",
+          referenceDeclarationKind: SwidReferenceDeclarationKind.voidType,
+          typeArguments: [],
+        )),
+        swidDeclarationModifiers: SwidDeclarationModifiers.empty(),
+      ),
+      staticConstFieldDeclarations: [],
+      methods: [],
+    ));
+
+    expect(ir.toDartSource(), """
+class VMManagedfooClass extends VMManagedBox<fooClass> {
+  VMManagedfooClass(
+      {@required this.table,
+      @required this.vmObject,
+      @required this.hydroState})
+      : super(
+          table: table,
+          vmObject: vmObject,
+          hydroState: hydroState,
+        ) {}
+
+  final HydroTable table;
+
+  final HydroState hydroState;
+
+  final fooClass vmObject;
+}
+""");
+  });
+}
