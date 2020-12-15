6d0a40154a9c88457e9165b73d65935791842ff5
Chris Gibb <chris.gibb@outlook.com>
2020-12-14 19:51:22 -0500

    start test for map transformation to object with index signature

---
+++ test/swid/transforms/ts/functionSignatures/returnPrimitiveMap_test.dart
@@ -0,0 +1,35 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:hydro_sdk/swid/ir/frontend/dart/swidDeclarationModifiers.dart';
+import 'package:hydro_sdk/swid/ir/frontend/dart/swidFunctionType.dart';
+import 'package:hydro_sdk/swid/ir/frontend/dart/swidInterface.dart';
+import 'package:hydro_sdk/swid/ir/frontend/dart/swidNullabilitySuffix.dart';
+import 'package:hydro_sdk/swid/ir/frontend/dart/swidReferenceDeclarationKind.dart';
+import 'package:hydro_sdk/swid/ir/frontend/dart/swidType.dart';
+import 'package:hydro_sdk/swid/transforms/ts/trailingReturnTypeKind.dart';
+import 'package:hydro_sdk/swid/transforms/ts/transformFunctionTypeToTs.dart';
+import 'package:hydro_sdk/swid/transforms/ts/transformTypeDeclarationToTs.dart';
+
+void main() {
+  LiveTestWidgetsFlutterBinding();
+  testWidgets('', (WidgetTester tester) async {
+    var functionType = SwidFunctionType(
+      name: "foo",
+      nullabilitySuffix: null,
+      originalPackagePath: null,
+      swidDeclarationModifiers: null,
+      namedParameterTypes: null,
+      namedDefaults: null,
+      normalParameterNames: null,
+      normalParameterTypes: null,
+      optionalParameterNames: null,
+      optionalParameterTypes: null,
+      returnType: null,
+    );
+
+    expect(
+        transformTypeDeclarationToTs(
+            swidType:
+                SwidType.fromSwidFunctionType(swidFunctionType: functionType)),
+        "foo : () => {[index : string]: string}");
+  });
+}
