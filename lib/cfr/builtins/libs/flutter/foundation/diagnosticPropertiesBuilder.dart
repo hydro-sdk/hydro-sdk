cebb8ce87ee28da4b8468e6d14a129fd866dedc7
Chris Gibb <chris.gibb@outlook.com>
2020-12-18 18:37:23 -0500

    initial emit of DiagnosticPropertiesBuilder

---
+++ lib/cfr/builtins/libs/flutter/foundation/diagnosticPropertiesBuilder.dart
@@ -0,0 +1,96 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:hydro_sdk/cfr/builtins/boxing/boxers.dart';
+import 'package:hydro_sdk/cfr/builtins/boxing/boxes.dart';
+import 'package:hydro_sdk/cfr/builtins/boxing/unboxers.dart';
+import 'package:hydro_sdk/cfr/vm/closure.dart';
+import 'package:hydro_sdk/cfr/vm/context.dart';
+import 'package:hydro_sdk/cfr/vm/table.dart';
+import 'package:hydro_sdk/hydroState.dart';
+
+class VMManagedDiagnosticPropertiesBuilder
+    extends VMManagedBox<DiagnosticPropertiesBuilder> {
+  VMManagedDiagnosticPropertiesBuilder(
+      {@required this.table,
+      @required this.vmObject,
+      @required this.hydroState})
+      : super(
+          table: table,
+          vmObject: vmObject,
+          hydroState: hydroState,
+        ) {
+    maybeBoxObject<List<DiagnosticsNode>>(
+        object: vmObject.properties,
+        hydroState: hydroState,
+        table: HydroTable());
+    table['defaultDiagnosticsTreeStyle'] =
+        DiagnosticsTreeStyle.values.indexWhere((x) {
+      return x == vmObject.defaultDiagnosticsTreeStyle;
+    });
+    table['emptyBodyDescription'] = vmObject.emptyBodyDescription;
+    table['add'] = makeLuaDartFunc(func: (List<dynamic> args) {
+      return [
+        vmObject.add(maybeUnBoxAndBuildArgument<DiagnosticsNode>(args[1],
+            parentState: hydroState))
+      ];
+    });
+  }
+
+  final HydroTable table;
+
+  final HydroState hydroState;
+
+  final DiagnosticPropertiesBuilder vmObject;
+}
+
+class RTManagedDiagnosticPropertiesBuilder extends DiagnosticPropertiesBuilder
+    implements Box<DiagnosticPropertiesBuilder> {
+  RTManagedDiagnosticPropertiesBuilder(
+      {@required this.table, @required this.hydroState})
+      : super() {
+    table['vmObject'] = vmObject;
+    table['unwrap'] = makeLuaDartFunc(func: (List<dynamic> args) {
+      return [unwrap()];
+    });
+    maybeBoxObject<List<DiagnosticsNode>>(
+        object: properties, hydroState: hydroState, table: HydroTable());
+    table['defaultDiagnosticsTreeStyle'] =
+        DiagnosticsTreeStyle.values.indexWhere((x) {
+      return x == defaultDiagnosticsTreeStyle;
+    });
+    table['emptyBodyDescription'] = emptyBodyDescription;
+    table['_dart_add'] = makeLuaDartFunc(func: (List<dynamic> args) {
+      return [
+        super.add(maybeUnBoxAndBuildArgument<DiagnosticsNode>(args[1],
+            parentState: hydroState))
+      ];
+    });
+  }
+
+  final HydroTable table;
+
+  final HydroState hydroState;
+
+  DiagnosticPropertiesBuilder unwrap() => this;
+  DiagnosticPropertiesBuilder get vmObject => this;
+  @override
+  void add(DiagnosticsNode property) {
+    Closure closure = table["add"];
+    return closure.dispatch([table], parentState: hydroState)[0];
+  }
+}
+
+void loadDiagnosticPropertiesBuilder(
+    {@required HydroState hydroState, @required HydroTable table}) {
+  table['diagnosticPropertiesBuilder'] =
+      makeLuaDartFunc(func: (List<dynamic> args) {
+    return [RTManagedDiagnosticPropertiesBuilder()];
+  });
+  registerBoxer<DiagnosticPropertiesBuilder>(boxer: (
+      {@required DiagnosticPropertiesBuilder vmObject,
+      @required HydroState hydroState,
+      @required HydroTable table}) {
+    return VMManagedDiagnosticPropertiesBuilder(
+        vmObject: vmObject, hydroState: hydroState, table: table);
+  });
+}
---
+++ runtime/flutter/foundation/diagnosticPropertiesBuilder.ts
@@ -0,0 +1,26 @@
+import { List } from "./../../dart/collection/list";
+import { DiagnosticsNode } from "./diagnosticsNode";
+import { DiagnosticsTreeStyle } from "./diagnosticsTreeStyle";
+
+declare const flutter: {
+    foundation: {
+        diagnosticPropertiesBuilder: (
+            this: void,
+            diagnosticPropertiesBuilder: DiagnosticPropertiesBuilder
+        ) => DiagnosticPropertiesBuilder;
+    };
+};
+export class DiagnosticPropertiesBuilder {
+    public readonly properties: List<DiagnosticsNode> = undefined as any;
+    public readonly defaultDiagnosticsTreeStyle: DiagnosticsTreeStyle = undefined as any;
+    public readonly emptyBodyDescription: string | undefined = undefined as any;
+    public constructor() {
+        flutter.foundation.diagnosticPropertiesBuilder(this);
+    }
+    private readonly _dart_add: (
+        property: DiagnosticsNode
+    ) => void = undefined as any;
+    public add(property: DiagnosticsNode) {
+        return this._dart_add(property);
+    }
+}
