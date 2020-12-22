f301039541cf54a41ace34fd0ab2d2af56bbea7e
Chris Gibb <chris.gibb@outlook.com>
2020-12-21 19:30:03 -0500

    handle lists of enums or objects

--- lib/swid/ir/backend/dart/dartBoxList.dart
+++ lib/swid/ir/backend/dart/dartBoxList.dart
@@ -1,6 +1,9 @@
 import 'package:code_builder/code_builder.dart'
     show DartEmitter, Expression, refer, Method, Parameter, Block, Code;
 import 'package:hydro_sdk/swid/ir/backend/dart/codeKind.dart';
+import 'package:hydro_sdk/swid/ir/backend/dart/dartBoxEnumReference.dart';
+import 'package:hydro_sdk/swid/ir/backend/dart/dartBoxObjectReference.dart';
+import 'package:hydro_sdk/swid/ir/frontend/dart/narrowSwidInterfaceByReferenceDeclaration.dart';
 import 'package:hydro_sdk/swid/ir/frontend/dart/swidInterface.dart';
 import 'package:hydro_sdk/swid/ir/frontend/dart/swidType.dart';
 import 'package:meta/meta.dart';
@@ -21,6 +24,41 @@ class DartBoxList {
           ? expression.statement
           : codeKind == CodeKind.expression
               ? expression.expression
-              : null)(refer(referenceName).property("map").call(
-          [Method().closure.expression], {})).accept(DartEmitter()).toString();
+              : null)(refer(referenceName)
+          .property("map")
+          .call([
+            Method((m) => m
+              ..requiredParameters.addAll([
+                Parameter((p) => p..name = "x"),
+              ])
+              ..lambda = true
+              ..body = Block.of([
+                Code(
+                  narrowSwidInterfaceByReferenceDeclaration(
+                    swidInterface: type.typeArguments.first.when(
+                      fromSwidInterface: (val) => val,
+                      fromSwidClass: (_) => null,
+                      fromSwidDefaultFormalParameter: (_) => null,
+                      fromSwidFunctionType: (_) => null,
+                    ),
+                    onPrimitive: (_) => null,
+                    onClass: (val) => DartBoxObjectReference(
+                      type: SwidType.fromSwidInterface(swidInterface: val),
+                      objectReference: refer("x"),
+                      codeKind: CodeKind.expression,
+                    ).toDartSource(),
+                    onEnum: (val) => DartBoxEnumReference(
+                            type:
+                                SwidType.fromSwidInterface(swidInterface: val),
+                            codeKind: CodeKind.expression,
+                            referenceName:
+                                refer("x").accept(DartEmitter()).toString())
+                        .toDartSource(),
+                    onVoid: (_) => null,
+                  ),
+                )
+              ])).closure.expression
+          ], {})
+          .property("toList")
+          .call([])).accept(DartEmitter()).toString();
 }
--- test/swid/transforms/dart/boxList_test.dart
+++ test/swid/transforms/dart/boxList_test.dart
@@ -1,13 +1,9 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:hydro_sdk/swid/ir/backend/dart/dartBoxList.dart';
-import 'package:hydro_sdk/swid/ir/frontend/dart/swidDeclarationModifiers.dart';
-import 'package:hydro_sdk/swid/ir/frontend/dart/swidFunctionType.dart';
 import 'package:hydro_sdk/swid/ir/frontend/dart/swidInterface.dart';
 import 'package:hydro_sdk/swid/ir/frontend/dart/swidNullabilitySuffix.dart';
 import 'package:hydro_sdk/swid/ir/frontend/dart/swidReferenceDeclarationKind.dart';
 import 'package:hydro_sdk/swid/ir/frontend/dart/swidType.dart';
-import 'package:hydro_sdk/swid/transforms/ts/trailingReturnTypeKind.dart';
-import 'package:hydro_sdk/swid/transforms/ts/transformFunctionTypeToTs.dart';
 
 void main() {
   LiveTestWidgetsFlutterBinding();
@@ -31,12 +27,6 @@ void main() {
     );
 
     expect(DartBoxList(type: field, referenceName: "properties").toDartSource(),
-"""
-properties
-.map((x) => maybeBoxObject<DiagnosticsNode>(
-  object: x, hydroState: hydroState, table: HydroTable()))
-.toList()
-"""    
-);
+        "properties.map((x) => maybeBoxObject<DiagnosticsNode>(object: x, hydroState: hydroState, table: HydroTable())).toList();");
   }, tags: "swid");
 }
