037b22632f26c4a76649653bfaf823e775001ad0
Chris Gibb <chris.gibb@outlook.com>
2020-12-26 19:03:36 -0500

    transform operator names when building injection candidates

--- lib/swid/ir/backend/ts/tsClassMethodInjectionCandidates.dart
+++ lib/swid/ir/backend/ts/tsClassMethodInjectionCandidates.dart
@@ -2,13 +2,15 @@ import 'package:meta/meta.dart';
 
 import 'package:hydro_sdk/swid/ir/frontend/dart/swidFunctionType.dart';
 import 'package:hydro_sdk/swid/transforms/transformAccessorName.dart';
+import 'package:hydro_sdk/swid/transforms/transformOperatorName.dart';
 import 'package:hydro_sdk/swid/transforms/ts/transformPrimitiveFunctionTypeNamesToTs.dart';
 
 List<SwidFunctionType> tsClassMethodInjectionCandidates(
         {@required List<SwidFunctionType> swidFunctionTypes}) =>
     swidFunctionTypes
-        .map((x) => transformPrimitiveFunctionTypeNamesToTs(
-            swidFunctionType: transformAccessorName(swidFunctionType: x)))
+        .map((x) => transformOperatorName(
+            swidFunctionType: transformPrimitiveFunctionTypeNamesToTs(
+                swidFunctionType: transformAccessorName(swidFunctionType: x))))
         .where((x) => x.name != "==")
         .where((x) => !x.swidDeclarationModifiers.hasProtected)
         .toList();
