2caadbdf60c849a7c14390f978eeaa1bf1342903
Chris Gibb <chris.gibb@outlook.com>
2020-12-27 14:18:56 -0500

    ignore more operators

--- lib/swid/ir/frontend/dart/isOperator.dart
+++ lib/swid/ir/frontend/dart/isOperator.dart
@@ -4,6 +4,7 @@ import 'package:hydro_sdk/swid/ir/frontend/dart/swidFunctionType.dart';
 
 bool isOperator({@required SwidFunctionType swidFunctionType}) =>
     swidFunctionType.name == "-" ||
+    swidFunctionType.name == "unary-" ||
     swidFunctionType.name == "+" ||
     swidFunctionType.name == "*" ||
     swidFunctionType.name == "/" ||
@@ -13,4 +14,5 @@ bool isOperator({@required SwidFunctionType swidFunctionType}) =>
     swidFunctionType.name == ">" ||
     swidFunctionType.name == "<=" ||
     swidFunctionType.name == ">=" ||
-    swidFunctionType.name == "%";
+    swidFunctionType.name == "%" ||
+    swidFunctionType.name == "&";
