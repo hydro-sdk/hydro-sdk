e6b704396c3ff5d5f73bcd9c32475e3b29409afd
Chris Gibb <chris.gibb@outlook.com>
2020-12-29 12:11:53 -0500

    flesh out test

--- test/unit/dart/ui/offset-1.ts
+++ test/unit/dart/ui/offset-1.ts
@@ -7,10 +7,25 @@ declare const assert: (this: void, arg: boolean, message?: string) => void;
 function first() {
     const offset = new Offset(1, 1);
 
+    assert(offset !== undefined);
+
     assert(offset.getDx() == 1);
     assert(offset.getDy() == 1);
 }
 
+function second() {
+    const offset = new Offset(1, 1);
+
+    assert(offset !== undefined);
+
+    const offSet2 = offset.scale(2, 2);
+
+    assert(offSet2 !== undefined);
+    assert(offSet2.getDx() == 2);
+    assert(offSet2.getDy() == 2);
+}
+
 function main() {
     first();
+    second();
 }
