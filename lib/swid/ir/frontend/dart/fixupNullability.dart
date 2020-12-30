7cb3731d4e77772fdfcebaea9dbcd2af1c5525a2
Chris Gibb <chris.gibb@outlook.com>
2020-12-29 21:39:07 -0500

    remove unresolvable static field from rrect

--- lib/swid/ir/frontend/dart/fixupNullability.dart
+++ lib/swid/ir/frontend/dart/fixupNullability.dart
@@ -5,20 +5,27 @@ import 'package:hydro_sdk/swid/ir/frontend/dart/swidClass.dart';
 import 'package:hydro_sdk/swid/ir/frontend/dart/swidFunctionType.dart';
 import 'package:hydro_sdk/swid/ir/frontend/dart/swidNullabilitySuffix.dart';
 
-SwidClass fixupNullability({@required SwidClass swidClass}) =>
-    swidClass.name == "IconData"
+SwidClass fixupNullability({@required SwidClass swidClass}) => swidClass.name ==
+        "IconData"
+    ? SwidClass.clone(
+        swidClass: swidClass,
+        constructorType: SwidFunctionType.clone(
+            swidFunctionType: swidClass.constructorType,
+            namedParameterTypes: Map.fromEntries(swidClass
+                .constructorType.namedParameterTypes.entries
+                .map((x) => x.key == "fontPackage"
+                    ? MapEntry(
+                        x.key,
+                        cloneSwidType(
+                            swidType: x.value,
+                            swidNullabilitySuffix:
+                                SwidNullabilitySuffix.question))
+                    : MapEntry(x.key, x.value)))))
+    : swidClass.name == "RRect" && swidClass.originalPackagePath == "dart:ui"
         ? SwidClass.clone(
             swidClass: swidClass,
-            constructorType: SwidFunctionType.clone(
-                swidFunctionType: swidClass.constructorType,
-                namedParameterTypes: Map.fromEntries(swidClass
-                    .constructorType.namedParameterTypes.entries
-                    .map((x) => x.key == "fontPackage"
-                        ? MapEntry(
-                            x.key,
-                            cloneSwidType(
-                                swidType: x.value,
-                                swidNullabilitySuffix:
-                                    SwidNullabilitySuffix.question))
-                        : MapEntry(x.key, x.value)))))
+            staticConstFieldDeclarations: swidClass.staticConstFieldDeclarations
+                .where((x) => x.name != "zero")
+                .toList(),
+          )
         : swidClass;
