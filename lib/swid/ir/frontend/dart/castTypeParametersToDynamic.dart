7d5c0c39f0f7ab523ee79cccdff4317d74ec203f
Chris Gibb <chris.gibb@outlook.com>
2021-01-01 22:51:59 -0500

    cast type parameters referencing type formals out of lists even when otherwise preserving list type parmeters

--- lib/swid/ir/frontend/dart/castTypeParametersToDynamic.dart
+++ lib/swid/ir/frontend/dart/castTypeParametersToDynamic.dart
@@ -15,176 +15,191 @@ SwidType castTypeParametersToDynamic({
   @required SwidType swidType,
   @required bool preserveTypeParametersInLists,
 }) =>
-    preserveTypeParametersInLists && isList(swidType: swidType)
-        ? swidType
-        : swidType.when(
-            fromSwidInterface: (val) =>
-                narrowSwidInterfaceByReferenceDeclaration(
-                  swidInterface: val,
-                  onPrimitive: (val) =>
-                      SwidType.fromSwidInterface(swidInterface: val),
-                  onClass: (val) => SwidType.fromSwidInterface(
-                    swidInterface: SwidInterface.clone(
-                        swidType: val,
-                        name: removeTypeArguments(str: val.name),
-                        typeArguments: val.typeArguments
-                            .map(
-                              (x) => SwidType.fromSwidInterface(
-                                swidInterface: SwidInterface(
-                                  name: "dynamic",
-                                  nullabilitySuffix: SwidNullabilitySuffix.none,
-                                  originalPackagePath: "",
-                                  referenceDeclarationKind:
-                                      SwidReferenceDeclarationKind.dynamicType,
-                                  typeArguments: [],
-                                ),
-                              ),
-                            )
-                            .toList()),
-                  ),
-                  onEnum: (val) =>
-                      SwidType.fromSwidInterface(swidInterface: val),
-                  onVoid: (val) =>
-                      SwidType.fromSwidInterface(swidInterface: val),
-                  onTypeParameter: (val) => SwidType.fromSwidInterface(
-                    swidInterface: SwidInterface(
-                      name: "dynamic",
-                      nullabilitySuffix: SwidNullabilitySuffix.none,
-                      originalPackagePath: "",
-                      referenceDeclarationKind:
-                          SwidReferenceDeclarationKind.dynamicType,
-                      typeArguments: [],
-                    ),
-                  ),
-                  onDynamic: (val) =>
-                      SwidType.fromSwidInterface(swidInterface: val),
-                ),
-            fromSwidClass: (val) => SwidType.fromSwidClass(
-                  swidClass: SwidClass.clone(
-                    swidClass: val,
-                    constructorType: val.constructorType != null
-                        ? castTypeParametersToDynamic(
-                            swidType: SwidType.fromSwidFunctionType(
-                                swidFunctionType: val.constructorType),
-                            preserveTypeParametersInLists:
-                                preserveTypeParametersInLists,
-                          ).when(
-                            fromSwidInterface: (_) => null,
-                            fromSwidClass: (_) => null,
-                            fromSwidDefaultFormalParameter: (_) => null,
-                            fromSwidFunctionType: (val) => val,
-                          )
-                        : null,
-                    factoryConstructors: val.factoryConstructors
-                        .map(
-                          (x) => castTypeParametersToDynamic(
-                            swidType: SwidType.fromSwidFunctionType(
-                                swidFunctionType: x),
-                            preserveTypeParametersInLists:
-                                preserveTypeParametersInLists,
-                          ).when(
-                            fromSwidInterface: (_) => null,
-                            fromSwidClass: (_) => null,
-                            fromSwidDefaultFormalParameter: (_) => null,
-                            fromSwidFunctionType: (val) => val,
-                          ),
-                        )
-                        .toList(),
-                    staticMethods: val.staticMethods
-                        .map(
-                          (x) => castTypeParametersToDynamic(
-                            swidType: SwidType.fromSwidFunctionType(
-                                swidFunctionType: x),
-                            preserveTypeParametersInLists:
-                                preserveTypeParametersInLists,
-                          ).when(
-                            fromSwidInterface: (_) => null,
-                            fromSwidClass: (_) => null,
-                            fromSwidDefaultFormalParameter: (_) => null,
-                            fromSwidFunctionType: (val) => val,
-                          ),
-                        )
-                        .toList(),
-                    methods: val.methods
-                        .map(
-                          (x) => castTypeParametersToDynamic(
-                            swidType: SwidType.fromSwidFunctionType(
-                                swidFunctionType: x),
-                            preserveTypeParametersInLists:
-                                preserveTypeParametersInLists,
-                          ).when(
-                            fromSwidInterface: (_) => null,
-                            fromSwidClass: (_) => null,
-                            fromSwidDefaultFormalParameter: (_) => null,
-                            fromSwidFunctionType: (val) => val,
-                          ),
-                        )
-                        .toList(),
-                    instanceFieldDeclarations:
-                        Map.fromEntries(val.instanceFieldDeclarations.entries
-                            .map(
-                              (x) => MapEntry(
-                                x.key,
-                                castTypeParametersToDynamic(
-                                  swidType: x.value,
-                                  preserveTypeParametersInLists:
-                                      preserveTypeParametersInLists,
-                                ),
-                              ),
-                            )
-                            .toList()),
-                    typeFormals: val.typeFormals
-                        .map((_) => SwidTypeFormal(
-                              name: "dynamic",
-                              swidReferenceDeclarationKind:
-                                  SwidReferenceDeclarationKind.dynamicType,
-                            ))
-                        .toList(),
-                  ),
-                ),
-            fromSwidDefaultFormalParameter: (_) => null,
-            fromSwidFunctionType: (val) => SwidType.fromSwidFunctionType(
-                  swidFunctionType: SwidFunctionType.clone(
-                    swidFunctionType: val,
-                    typeFormals: val.typeFormals
-                        .map((_) => SwidTypeFormal(
-                              name: "dynamic",
-                              swidReferenceDeclarationKind:
-                                  SwidReferenceDeclarationKind.dynamicType,
-                            ))
-                        .toList(),
-                    namedParameterTypes: Map.fromEntries(
-                      val.namedParameterTypes.entries
-                          .map(
-                            (x) => MapEntry(
-                              x.key,
-                              castTypeParametersToDynamic(
-                                swidType: x.value,
-                                preserveTypeParametersInLists:
-                                    preserveTypeParametersInLists,
-                              ),
+    swidType.when(
+        fromSwidInterface: (val) => narrowSwidInterfaceByReferenceDeclaration(
+              swidInterface: val,
+              onPrimitive: (val) =>
+                  SwidType.fromSwidInterface(swidInterface: val),
+              onClass: (val) => preserveTypeParametersInLists &&
+                      isList(swidType: swidType) &&
+                      (val.typeArguments.first?.when(
+                            fromSwidInterface: (val) =>
+                                narrowSwidInterfaceByReferenceDeclaration(
+                              swidInterface: val,
+                              onPrimitive: (_) => true,
+                              onClass: (_) => true,
+                              onEnum: (_) => true,
+                              onVoid: (_) => true,
+                              onTypeParameter: (_) => false,
+                              onDynamic: (_) => true,
                             ),
-                          )
-                          .toList(),
+                            fromSwidClass: (_) => true,
+                            fromSwidDefaultFormalParameter: (_) => true,
+                            fromSwidFunctionType: (_) => true,
+                          ) ??
+                          true)
+                  ? swidType
+                  : SwidType.fromSwidInterface(
+                      swidInterface: SwidInterface.clone(
+                          swidType: val,
+                          name: removeTypeArguments(str: val.name),
+                          typeArguments: val.typeArguments
+                              .map(
+                                (x) => SwidType.fromSwidInterface(
+                                  swidInterface: SwidInterface(
+                                    name: "dynamic",
+                                    nullabilitySuffix:
+                                        SwidNullabilitySuffix.none,
+                                    originalPackagePath: "",
+                                    referenceDeclarationKind:
+                                        SwidReferenceDeclarationKind
+                                            .dynamicType,
+                                    typeArguments: [],
+                                  ),
+                                ),
+                              )
+                              .toList()),
                     ),
-                    normalParameterTypes: val.normalParameterTypes
-                        .map((x) => castTypeParametersToDynamic(
-                              swidType: x,
+              onEnum: (val) => SwidType.fromSwidInterface(swidInterface: val),
+              onVoid: (val) => SwidType.fromSwidInterface(swidInterface: val),
+              onTypeParameter: (val) => SwidType.fromSwidInterface(
+                swidInterface: SwidInterface(
+                  name: "dynamic",
+                  nullabilitySuffix: SwidNullabilitySuffix.none,
+                  originalPackagePath: "",
+                  referenceDeclarationKind:
+                      SwidReferenceDeclarationKind.dynamicType,
+                  typeArguments: [],
+                ),
+              ),
+              onDynamic: (val) =>
+                  SwidType.fromSwidInterface(swidInterface: val),
+            ),
+        fromSwidClass: (val) => SwidType.fromSwidClass(
+              swidClass: SwidClass.clone(
+                swidClass: val,
+                constructorType: val.constructorType != null
+                    ? castTypeParametersToDynamic(
+                        swidType: SwidType.fromSwidFunctionType(
+                            swidFunctionType: val.constructorType),
+                        preserveTypeParametersInLists:
+                            preserveTypeParametersInLists,
+                      ).when(
+                        fromSwidInterface: (_) => null,
+                        fromSwidClass: (_) => null,
+                        fromSwidDefaultFormalParameter: (_) => null,
+                        fromSwidFunctionType: (val) => val,
+                      )
+                    : null,
+                factoryConstructors: val.factoryConstructors
+                    .map(
+                      (x) => castTypeParametersToDynamic(
+                        swidType:
+                            SwidType.fromSwidFunctionType(swidFunctionType: x),
+                        preserveTypeParametersInLists:
+                            preserveTypeParametersInLists,
+                      ).when(
+                        fromSwidInterface: (_) => null,
+                        fromSwidClass: (_) => null,
+                        fromSwidDefaultFormalParameter: (_) => null,
+                        fromSwidFunctionType: (val) => val,
+                      ),
+                    )
+                    .toList(),
+                staticMethods: val.staticMethods
+                    .map(
+                      (x) => castTypeParametersToDynamic(
+                        swidType:
+                            SwidType.fromSwidFunctionType(swidFunctionType: x),
+                        preserveTypeParametersInLists:
+                            preserveTypeParametersInLists,
+                      ).when(
+                        fromSwidInterface: (_) => null,
+                        fromSwidClass: (_) => null,
+                        fromSwidDefaultFormalParameter: (_) => null,
+                        fromSwidFunctionType: (val) => val,
+                      ),
+                    )
+                    .toList(),
+                methods: val.methods
+                    .map(
+                      (x) => castTypeParametersToDynamic(
+                        swidType:
+                            SwidType.fromSwidFunctionType(swidFunctionType: x),
+                        preserveTypeParametersInLists:
+                            preserveTypeParametersInLists,
+                      ).when(
+                        fromSwidInterface: (_) => null,
+                        fromSwidClass: (_) => null,
+                        fromSwidDefaultFormalParameter: (_) => null,
+                        fromSwidFunctionType: (val) => val,
+                      ),
+                    )
+                    .toList(),
+                instanceFieldDeclarations:
+                    Map.fromEntries(val.instanceFieldDeclarations.entries
+                        .map(
+                          (x) => MapEntry(
+                            x.key,
+                            castTypeParametersToDynamic(
+                              swidType: x.value,
                               preserveTypeParametersInLists:
                                   preserveTypeParametersInLists,
-                            ))
-                        .toList(),
-                    optionalParameterTypes: val.optionalParameterTypes
-                        .map((x) => castTypeParametersToDynamic(
-                              swidType: x,
-                              preserveTypeParametersInLists:
-                                  preserveTypeParametersInLists,
-                            ))
-                        .toList(),
-                    returnType: castTypeParametersToDynamic(
-                      swidType: val.returnType,
-                      preserveTypeParametersInLists:
-                          preserveTypeParametersInLists,
-                    ),
-                  ),
-                ));
+                            ),
+                          ),
+                        )
+                        .toList()),
+                typeFormals: val.typeFormals
+                    .map((_) => SwidTypeFormal(
+                          name: "dynamic",
+                          swidReferenceDeclarationKind:
+                              SwidReferenceDeclarationKind.dynamicType,
+                        ))
+                    .toList(),
+              ),
+            ),
+        fromSwidDefaultFormalParameter: (_) => null,
+        fromSwidFunctionType: (val) => SwidType.fromSwidFunctionType(
+              swidFunctionType: SwidFunctionType.clone(
+                swidFunctionType: val,
+                typeFormals: val.typeFormals
+                    .map((_) => SwidTypeFormal(
+                          name: "dynamic",
+                          swidReferenceDeclarationKind:
+                              SwidReferenceDeclarationKind.dynamicType,
+                        ))
+                    .toList(),
+                namedParameterTypes: Map.fromEntries(
+                  val.namedParameterTypes.entries
+                      .map(
+                        (x) => MapEntry(
+                          x.key,
+                          castTypeParametersToDynamic(
+                            swidType: x.value,
+                            preserveTypeParametersInLists:
+                                preserveTypeParametersInLists,
+                          ),
+                        ),
+                      )
+                      .toList(),
+                ),
+                normalParameterTypes: val.normalParameterTypes
+                    .map((x) => castTypeParametersToDynamic(
+                          swidType: x,
+                          preserveTypeParametersInLists:
+                              preserveTypeParametersInLists,
+                        ))
+                    .toList(),
+                optionalParameterTypes: val.optionalParameterTypes
+                    .map((x) => castTypeParametersToDynamic(
+                          swidType: x,
+                          preserveTypeParametersInLists:
+                              preserveTypeParametersInLists,
+                        ))
+                    .toList(),
+                returnType: castTypeParametersToDynamic(
+                  swidType: val.returnType,
+                  preserveTypeParametersInLists: preserveTypeParametersInLists,
+                ),
+              ),
+            ));
