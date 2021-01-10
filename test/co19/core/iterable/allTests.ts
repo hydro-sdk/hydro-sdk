6329ade74ba639842c82bdf9c26cbf3cd179c4e4
Chris Gibb <chris.gibb@outlook.com>
2021-01-10 10:49:42 -0500

    port iterable contains test

--- test/co19/core/iterable/allTests.ts
+++ test/co19/core/iterable/allTests.ts
@@ -5,6 +5,7 @@ import { any_A01_t03 } from "./any_A01_t03";
 import { any_A01_t04 } from "./any_A01_t04";
 import { any_A01_t05 } from "./any_A01_t05";
 import { any_A02_t01 } from "./any_A02_t01";
+import { contains_A01_t01 } from "./contains_A01_t01";
 
 export function allIterableTests(
     create: (
@@ -18,4 +19,5 @@ export function allIterableTests(
     any_A01_t04(create);
     any_A01_t05(create);
     any_A02_t01(create);
+    contains_A01_t01(create);
 }
