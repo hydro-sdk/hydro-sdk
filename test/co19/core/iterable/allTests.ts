2ff39558495516d0a2116929c408d5c2808b6dfa
Chris Gibb <chris.gibb@outlook.com>
2021-01-09 21:29:14 -0500

    port any test

--- test/co19/core/iterable/allTests.ts
+++ test/co19/core/iterable/allTests.ts
@@ -4,6 +4,7 @@ import { any_A01_t02 } from "./any_A01_t02";
 import { any_A01_t03 } from "./any_A01_t03";
 import { any_A01_t04 } from "./any_A01_t04";
 import { any_A01_t05 } from "./any_A01_t05";
+import { any_A02_t01 } from "./any_A02_t01";
 
 export function allIterableTests(
     create: (
@@ -16,4 +17,5 @@ export function allIterableTests(
     any_A01_t03(create);
     any_A01_t04(create);
     any_A01_t05(create);
+    any_A02_t01(create);
 }
