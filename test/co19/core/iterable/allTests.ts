aa7a1b5bfebc7ee5f18d434ece4d97e2626943cb
Chris Gibb <chris.gibb@outlook.com>
2021-01-14 22:16:02 -0500

    port a map test

--- test/co19/core/iterable/allTests.ts
+++ test/co19/core/iterable/allTests.ts
@@ -43,6 +43,7 @@ import { lastWhere_A02_t01 } from "./lastWhere_A02_t01";
 import { lastWhere_A03_t01 } from "./lastWhere_A03_t01";
 import { lastWhere_A04_t01 } from "./lastWhere_A04_t01";
 import { length_A01_t01 } from "./length_A01_t01";
+import { map_A01_t01 } from "./map_A01_t01";
 
 export function allIterableTests(
     create: (content: IIterable<any> | undefined) => IIterable<any>,
@@ -92,4 +93,5 @@ export function allIterableTests(
     lastWhere_A03_t01(create);
     lastWhere_A04_t01(create, { isSet: props.isSet });
     length_A01_t01(create, { isSet: props.isSet });
+    map_A01_t01(create);
 }
---
+++ test/co19/core/iterable/map_A01_t01.ts
@@ -0,0 +1,43 @@
+/**
+ * @assertion Iterable map(dynamic f(E e))
+ * Returns a new lazy Iterable with elements that are created by calling f on e
+ * ach element of this Iterable in iteration order.
+ * @description Checks that the resulting Iterable contains all the results of
+ * calling f(e).
+ */
+
+import { IIterable } from "./../../../../runtime/dart/core/iterable";
+import { List } from "../../../../runtime/dart/core/list";
+declare const assert: (this: void, arg: boolean, message?: string) => void;
+
+export function map_A01_t01(
+    create: (content: IIterable<any> | undefined) => IIterable<any>
+) {
+    const f = (element: any) => List.from([element] as any, {});
+
+    const checkResult = (a: IIterable<any>) => {
+        let count = 0;
+        const mapIter = a.map(f);
+        mapIter.forEach((x) => {
+            const l = f(a.elementAt(count++));
+            for (let i = 0; i != l.getLength(); ++i) {
+                assert(l.elementAt(i) == x.elementAt(i));
+            }
+        });
+        assert(a.getLength() == count);
+    };
+
+    let a = create(undefined);
+    checkResult(a);
+    a = create(List.from([22] as any, {}));
+    checkResult(a);
+    a = create(List.from([22, 11] as any, {}));
+    checkResult(a);
+    a = create(List.from([22, 11, , 11] as any, {}));
+    checkResult(a);
+    let l = List.empty({ growable: true });
+    for (let k = -100; k < 200; k++) {
+        l.add(k);
+    }
+    checkResult(create(l));
+}
