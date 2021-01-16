150c4e0227d8f2c637952be515f65a24f3ec939f
Chris Gibb <chris.gibb@outlook.com>
2021-01-15 22:03:04 -0500

    format

--- test/co19/core/iterable/singleWhere_A01_t01.ts
+++ test/co19/core/iterable/singleWhere_A01_t01.ts
@@ -12,11 +12,19 @@ declare const assert: (this: void, arg: boolean, message?: string) => void;
 export function singleWhere_A01_t01(
     create: (content: IIterable<any> | undefined) => IIterable<any>
 ) {
-    const check = (a: IIterable<any>, f: (value: any) => boolean, expected: number) => {
+    const check = (
+        a: IIterable<any>,
+        f: (value: any) => boolean,
+        expected: number
+    ) => {
         const actual = a.singleWhere(f, {});
         assert(expected == actual);
     };
 
     check(create(List.from([1, 2, -3, 0] as any, {})), (value) => value > 1, 2);
-    check(create(List.from([1, 2, -5, -6] as any, {})), (value) => value == 1, 1);
+    check(
+        create(List.from([1, 2, -5, -6] as any, {})),
+        (value) => value == 1,
+        1
+    );
 }
