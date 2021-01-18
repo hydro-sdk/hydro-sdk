0cd486e9d4d88177887fae7dd9c5fe4c76b4828d
Chris Gibb <chris.gibb@outlook.com>
2021-01-17 21:33:31 -0500

    port a take test

--- test/co19/core/iterable/allTests.ts
+++ test/co19/core/iterable/allTests.ts
@@ -61,6 +61,7 @@ import { skipwhile_A03_t01 } from "./skipWhile_A03_t01";
 import { skipwhile_A04_t01 } from "./skipWhile_A04_t01";
 import { skipwhile_A05_t01 } from "./skipWhile_A05_t01";
 import { take_A01_t01 } from "./take_A01_t01";
+import { take_A02_t01 } from "./take_A02_t01";
 
 export function allIterableTests(
     create: (content: IIterable<any> | undefined) => IIterable<any>,
@@ -128,4 +129,5 @@ export function allIterableTests(
     skipwhile_A04_t01(create);
     skipwhile_A05_t01(create);
     take_A01_t01(create);
+    take_A02_t01(create);
 }
---
+++ test/co19/core/iterable/take_A02_t01.ts
@@ -0,0 +1,28 @@
+/**
+ * @assertion Iterable<E> take(int count)
+ * The count must not be negative.
+ * @description Checks that an error is thrown if n is negative.
+ */
+
+import { IIterable } from "./../../../../runtime/dart/core/iterable";
+import { List } from "../../../../runtime/dart/core/list";
+declare const assert: (this: void, arg: boolean, message?: string) => void;
+
+export function take_A02_t01(
+    create: (content: IIterable<any> | undefined) => IIterable<any>
+) {
+    const check = (a: IIterable<any>, n: number) => {
+        let threw = false;
+        try {
+            a.take(n);
+        }
+        catch (err) {
+            threw = true;
+        }
+        assert(threw == true);
+    };
+
+    check(create(List.from([1, 2, -3, 4] as any, {})), -1);
+    check(create(List.from([1, 2, -5, -6, 100] as any, {})), -1);
+    check(create(List.from([undefined, 2, -5, -6, 100] as any, {})), -1000);
+}
