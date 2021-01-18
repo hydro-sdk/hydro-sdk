a9d3db6705b4c889f309e3d44c14ef2388d9e914
Chris Gibb <chris.gibb@outlook.com>
2021-01-17 21:46:31 -0500

    port a takeWhile test

--- test/co19/core/iterable/allTests.ts
+++ test/co19/core/iterable/allTests.ts
@@ -63,6 +63,7 @@ import { skipwhile_A05_t01 } from "./skipWhile_A05_t01";
 import { take_A01_t01 } from "./take_A01_t01";
 import { take_A02_t01 } from "./take_A02_t01";
 import { take_A03_t01 } from "./take_A03_t01";
+import { takeWhile_A01_t01 } from "./takeWhile_A01_t01";
 
 export function allIterableTests(
     create: (content: IIterable<any> | undefined) => IIterable<any>,
@@ -132,4 +133,5 @@ export function allIterableTests(
     take_A01_t01(create);
     take_A02_t01(create);
     take_A03_t01(create);
+    takeWhile_A01_t01(create);
 }
---
+++ test/co19/core/iterable/takeWhile_A01_t01.ts
@@ -0,0 +1,49 @@
+/**
+ * @assertion Iterable<E> takeWhile(bool test(E value))
+ * Returns a lazy iterable of the leading elements satisfying test
+ * @description Checks that all first elements that satisfy test are retained,
+ * and elements after that, are skipped.
+ */
+
+import { IIterable } from "./../../../../runtime/dart/core/iterable";
+import { List } from "../../../../runtime/dart/core/list";
+declare const assert: (this: void, arg: boolean, message?: string) => void;
+
+export function takeWhile_A01_t01(
+    create: (content: IIterable<any> | undefined) => IIterable<any>
+) {
+    const check = (a0: IIterable<any>, tst: (element: any) => boolean) => {
+        const it0 = a0.getIterator();
+        const a = a0.takeWhile(tst);
+        const it = a.getIterator();
+
+        let hasNext0 = it0.moveNext();
+        let len = 0;
+        for (; ;) {
+            let hasNext = it.moveNext();
+            if (!hasNext) {
+                break;
+            }
+
+            assert(tst(it0.getCurrent()) == true);
+            assert(it0.getCurrent() == it.getCurrent());
+            len++;
+            hasNext0 = it0.moveNext();
+        }
+        assert(a.getLength() == len);
+
+        let skipcount = 0;
+        while (hasNext0) {
+            skipcount++;
+            hasNext0 = it0.moveNext();
+        }
+        assert(a0.getLength() == len + skipcount);
+    };
+    const a0 = create(List.from([1, 3, 7, 4, 5, 6] as any, {}));
+    check(a0, (element) => element == 1);
+    check(a0, (element) => true);
+    check(a0, (element) => false);
+    check(a0, (element) => element > 4);
+    check(a0, (element) => element < 4);
+    check(a0, (element) => element == 4);
+}
