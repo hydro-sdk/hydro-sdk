bc5d4a36157422322473d6a401fc6cf6c6e3a1be
Chris Gibb <chris.gibb@outlook.com>
2021-01-20 23:21:18 -0500

    stop resolving imports within class

--- lib/swid/ir/backend/ts/tsResolvedImport.dart
+++ lib/swid/ir/backend/ts/tsResolvedImport.dart
@@ -1,45 +1,21 @@
 import 'package:meta/meta.dart';
-import 'package:path/path.dart' as path;
-
-import 'package:hydro_sdk/swid/ir/frontend/dart/swidType.dart';
-import 'package:hydro_sdk/swid/transforms/removeTypeArguments.dart';
-import 'package:hydro_sdk/swid/transforms/transformPackageUri.dart';
-import 'package:hydro_sdk/swid/transforms/transformToCamelCase.dart';
 
 class TsResolvedImport {
-  final SwidType importee;
-  final SwidType importer;
-  final List<String> prefixPaths;
+  final List<String> symbols;
+  final String path;
 
   TsResolvedImport({
-    @required this.importee,
-    @required this.importer,
-    @required this.prefixPaths,
+    @required this.symbols,
+    @required this.path,
   });
 
-  String toTsSource() => (({String symbolName, String resolvedPath}) => [
-            "import { ",
-            removeTypeArguments(str: symbolName),
-            " }",
-            " from ",
-            "\"",
-            resolvedPath,
-            path.separator,
-            transformToCamelCase(str: removeTypeArguments(str: symbolName)),
-            "\"",
-          ].join())(
-        symbolName: importee.name,
-        resolvedPath: path.relative(
-          prefixPaths.join(path.separator) +
-              path.separator +
-              transformPackageUri(
-                packageUri: importee.originalPackagePath,
-              ),
-          from: prefixPaths.join(path.separator) +
-              path.separator +
-              transformPackageUri(
-                packageUri: importer.originalPackagePath,
-              ),
-        ),
-      );
+  String toTsSource() => [
+        "import { ",
+        symbols.join(","),
+        " }",
+        " from ",
+        "\"",
+        path,
+        "\";",
+      ].join();
 }
