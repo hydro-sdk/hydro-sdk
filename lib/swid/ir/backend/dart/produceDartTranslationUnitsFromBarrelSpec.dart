ab8a96a2b41d31e96d503c47ceaf6ef9ce8965e7
Chris Gibb <chris.gibb@outlook.com>
2021-01-29 23:14:19 -0500

    fix paths in barrel member imports

--- lib/swid/ir/backend/dart/produceDartTranslationUnitsFromBarrelSpec.dart
+++ lib/swid/ir/backend/dart/produceDartTranslationUnitsFromBarrelSpec.dart
@@ -5,7 +5,9 @@ import 'package:hydro_sdk/swid/ir/backend/dart/dartImportStatement.dart';
 import 'package:hydro_sdk/swid/ir/backend/dart/dartLinebreak.dart';
 import 'package:hydro_sdk/swid/ir/backend/dart/dartTranslationUnit.dart';
 import 'package:hydro_sdk/swid/ir/backend/dart/dartir.dart';
+import 'package:hydro_sdk/swid/ir/backend/util/barrelMember.dart';
 import 'package:hydro_sdk/swid/ir/backend/util/barrelSpec.dart';
+import 'package:hydro_sdk/swid/transforms/transformToCamelCase.dart';
 
 List<DartTranslationUnit> produceDartTranslationUnitsFromBarrelSpec({
   @required String packageName,
@@ -14,8 +16,8 @@ List<DartTranslationUnit> produceDartTranslationUnitsFromBarrelSpec({
 }) =>
     [
       DartTranslationUnit(
-          path: prefixPaths.join(p.separator),
-          fileName: barrelSpec.path + ".dart",
+          path: prefixPaths.join(p.separator) + p.separator + barrelSpec.path,
+          fileName: barrelSpec.name + ".dart",
           ir: [
             DartIr.fromDartImportStatement(
                 dartImportStatement:
@@ -29,22 +31,47 @@ List<DartTranslationUnit> produceDartTranslationUnitsFromBarrelSpec({
                 dartImportStatement: DartImportStatement(
                     path: "package:hydro_sdk/hydroState.dart")),
             DartIr.fromDartLinebreak(dartLinebreak: DartLinebreak()),
+            ...barrelSpec.members
+                .map((x) => [
+                      DartIr.fromDartImportStatement(
+                          dartImportStatement: DartImportStatement(
+                              path: [
+                        "package:",
+                        packageName,
+                        p.separator,
+                        prefixPaths.first == "lib"
+                            ? prefixPaths.skip(1).join(p.separator)
+                            : prefixPaths.join(p.separator),
+                        p.separator,
+                        x.originalPackagePath,
+                        p.separator,
+                        transformToCamelCase(
+                          str: x.when(
+                            fromSwidClass: (val) => val.name,
+                            fromSwidEnum: (val) => val.identifier,
+                            fromBarrelSpec: (val) => val.name,
+                          ),
+                        ),
+                        ".dart",
+                      ].join()))
+                    ])
+                .reduce((value, element) => [...value, ...element])
           ]),
-      ...((List<List<DartTranslationUnit>> translationUnits) => translationUnits
-              .isNotEmpty
-          ? translationUnits.reduce((value, element) => [...value, ...element])
-          : [])(
-        barrelSpec.members
-            .where((x) =>
-                x.maybeWhen(fromBarrelSpec: (val) => val, orElse: () => null) !=
-                null)
-            .cast<BarrelSpec>()
-            .map(
-              (x) => produceDartTranslationUnitsFromBarrelSpec(
-                packageName: packageName,
-                prefixPaths: prefixPaths,
-                barrelSpec: x,
-              ),
-            ),
-      )
+      // ...((List<List<DartTranslationUnit>> translationUnits) => translationUnits
+      //         .isNotEmpty
+      //     ? translationUnits.reduce((value, element) => [...value, ...element])
+      //     : [])(
+      //   barrelSpec.members
+      //       .where((x) =>
+      //           x.maybeWhen(fromBarrelSpec: (val) => val, orElse: () => null) !=
+      //           null)
+      //       .cast<BarrelSpec>()
+      //       .map(
+      //         (x) => produceDartTranslationUnitsFromBarrelSpec(
+      //           packageName: packageName,
+      //           prefixPaths: prefixPaths,
+      //           barrelSpec: x,
+      //         ),
+      //       ),
+      // )
     ];
