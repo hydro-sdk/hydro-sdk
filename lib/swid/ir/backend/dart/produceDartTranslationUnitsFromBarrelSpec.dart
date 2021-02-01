f9abc9eca7ac7ae14fff37e5a6d1ce7fd5487c03
Chris Gibb <chris.gibb@outlook.com>
2021-01-31 22:31:53 -0500

    recursively produce barrel translation units

--- lib/swid/ir/backend/dart/produceDartTranslationUnitsFromBarrelSpec.dart
+++ lib/swid/ir/backend/dart/produceDartTranslationUnitsFromBarrelSpec.dart
@@ -1,6 +1,7 @@
 import 'package:meta/meta.dart';
 import 'package:path/path.dart' as p;
 
+import 'package:hydro_sdk/swid/ir/backend/dart/dartBarrelLoadNamespaceSymbolDeclaration.dart';
 import 'package:hydro_sdk/swid/ir/backend/dart/dartImportStatement.dart';
 import 'package:hydro_sdk/swid/ir/backend/dart/dartLinebreak.dart';
 import 'package:hydro_sdk/swid/ir/backend/dart/dartTranslationUnit.dart';
@@ -8,6 +9,7 @@ import 'package:hydro_sdk/swid/ir/backend/dart/dartir.dart';
 import 'package:hydro_sdk/swid/ir/backend/util/barrelMember.dart';
 import 'package:hydro_sdk/swid/ir/backend/util/barrelSpec.dart';
 import 'package:hydro_sdk/swid/ir/backend/util/requiresDartClassTranslationUnit.dart';
+import 'package:hydro_sdk/swid/transforms/transformPackageUri.dart';
 import 'package:hydro_sdk/swid/transforms/transformToCamelCase.dart';
 
 List<DartTranslationUnit> produceDartTranslationUnitsFromBarrelSpec({
@@ -55,7 +57,8 @@ List<DartTranslationUnit> produceDartTranslationUnitsFromBarrelSpec({
                               ? prefixPaths.skip(1).join(p.separator)
                               : prefixPaths.join(p.separator),
                           p.separator,
-                          x.originalPackagePath,
+                          transformPackageUri(
+                              packageUri: x.originalPackagePath),
                           p.separator,
                           transformToCamelCase(
                             str: x.when(
@@ -68,23 +71,28 @@ List<DartTranslationUnit> produceDartTranslationUnitsFromBarrelSpec({
                         ].join()))
                       ]
                     : [])
-                .reduce((value, element) => [...value, ...element])
+                .reduce((value, element) => [...value, ...element]),
+            DartIr.fromDartBarrelLoadNamespaceSymbolDeclaration(
+                dartBarrelLoadNamespaceSymbolDeclaration:
+                    DartBarrelLoadNamespaceSymbolDeclaration(
+                        barrelSpec: barrelSpec))
           ]),
-      // ...((List<List<DartTranslationUnit>> translationUnits) => translationUnits
-      //         .isNotEmpty
-      //     ? translationUnits.reduce((value, element) => [...value, ...element])
-      //     : [])(
-      //   barrelSpec.members
-      //       .where((x) =>
-      //           x.maybeWhen(fromBarrelSpec: (val) => val, orElse: () => null) !=
-      //           null)
-      //       .cast<BarrelSpec>()
-      //       .map(
-      //         (x) => produceDartTranslationUnitsFromBarrelSpec(
-      //           packageName: packageName,
-      //           prefixPaths: prefixPaths,
-      //           barrelSpec: x,
-      //         ),
-      //       ),
-      // )
+      ...((List<List<DartTranslationUnit>> translationUnits) => translationUnits
+              .isNotEmpty
+          ? translationUnits.reduce((value, element) => [...value, ...element])
+          : [])(
+        barrelSpec.members
+            .map((x) =>
+                x.maybeWhen(fromBarrelSpec: (val) => val, orElse: () => null))
+            .where((x) => x != null)
+            .where((x) => x.name != "_internal")
+            .map(
+              (x) => produceDartTranslationUnitsFromBarrelSpec(
+                packageName: packageName,
+                prefixPaths: prefixPaths,
+                barrelSpec: x,
+              ),
+            )
+            .toList(),
+      )
     ];
