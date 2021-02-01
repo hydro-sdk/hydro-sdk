949f7b6077fe3a43b193e6e5679b54ee783e3497
Chris Gibb <chris.gibb@outlook.com>
2021-01-31 22:32:04 -0500

    reemit

--- lib/cfr/builtins/libs/dart/math/math.dart
+++ lib/cfr/builtins/libs/dart/math/math.dart
@@ -4,9 +4,8 @@ import 'package:hydro_sdk/cfr/builtins/libs/dart/math/random.dart';
 import 'package:hydro_sdk/cfr/vm/table.dart';
 import 'package:hydro_sdk/hydroState.dart';
 
-void loadMath({@required HydroState luaState, @required HydroTable table}) {
-  var math = HydroTable();
-
-  table["math"] = math;
-  loadRandom(hydroState: luaState, table: math);
+void loadMath({@required HydroState hydroState, @required HydroTable table}) {
+  final math = HydroTable();
+  table['math'] = math;
+  loadRandom(table: math, hydroState: hydroState);
 }
