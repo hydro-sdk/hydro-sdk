137c2a7e6516cf34910e4ceca7dc04241224286c
Chris Gibb <chris.gibb@outlook.com>
2021-02-01 22:39:11 -0500

    factor out primitive renaming

--- lib/swid/transforms/ts/transformPrimitiveSwidTypeNamesToTs.dart
+++ lib/swid/transforms/ts/transformPrimitiveSwidTypeNamesToTs.dart
@@ -1,3 +1,4 @@
+import 'package:hydro_sdk/swid/transforms/ts/mapPrimitiveSwidTypeNameToPrimitiveTsTypeName.dart';
 import 'package:meta/meta.dart';
 
 import 'package:hydro_sdk/swid/ir/frontend/dart/swidInterface.dart';
@@ -5,22 +6,9 @@ import 'package:hydro_sdk/swid/ir/frontend/dart/swidInterface.dart';
 SwidInterface transformPrimitiveSwidTypeNamesToTs(
     {@required SwidInterface swidInterface}) {
   if (swidInterface.originalPackagePath == "dart:core") {
-    if (swidInterface.name == "String" ||
-        swidInterface.name == "String*" ||
-        swidInterface.name == "String?") {
-      return swidInterface.copyWith(name: "string");
-    } else if (swidInterface.name == "bool" ||
-        swidInterface.name == "bool*" ||
-        swidInterface.name == "bool?") {
-      return swidInterface.copyWith(name: "boolean");
-    } else if (swidInterface.name == "int" ||
-        swidInterface.name == "int*" ||
-        swidInterface.name == "int?" ||
-        swidInterface.name == "double" ||
-        swidInterface.name == "double*" ||
-        swidInterface.name == "double?") {
-      return swidInterface.copyWith(name: "number");
-    }
+    return swidInterface.copyWith(
+        name: mapPrimitiveSwidTypeNameToPrimitiveTsTypeName(
+            str: swidInterface.name));
   }
 
   return swidInterface;
