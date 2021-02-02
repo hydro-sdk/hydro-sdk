b77efa2e27b39d98bd8237c722262723994bb18d
Chris Gibb <chris.gibb@outlook.com>
2021-02-01 22:49:09 -0500

    fix broken fall through mapping primitive type names

--- lib/swid/transforms/ts/mapPrimitiveSwidTypeNameToPrimitiveTsTypeName.dart
+++ lib/swid/transforms/ts/mapPrimitiveSwidTypeNameToPrimitiveTsTypeName.dart
@@ -12,4 +12,4 @@ String mapPrimitiveSwidTypeNameToPrimitiveTsTypeName({@required String str}) =>
                     str == "double*" ||
                     str == "double?"
                 ? "number"
-                : "";
+                : str;
