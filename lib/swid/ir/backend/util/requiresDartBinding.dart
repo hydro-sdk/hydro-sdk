1fd7dc7b60a872524c99c4b394a8bd6f2887a0b1
Chris Gibb <chris.gibb@outlook.com>
2021-02-15 12:59:44 -0500

    tests for inexpressible static const class fields in typescript vm delcarations

--- lib/swid/ir/backend/util/requiresDartBinding.dart
+++ lib/swid/ir/backend/util/requiresDartBinding.dart
@@ -1,7 +1,14 @@
 import 'package:meta/meta.dart';
 
 import 'package:hydro_sdk/swid/ir/frontend/dart/swidClass.dart';
+import 'package:hydro_sdk/swid/ir/frontend/dart/util/isInexpressibleStaticConst.dart';
 
 bool requiresDartBinding({@required SwidClass swidClass}) =>
     swidClass.instanceFieldDeclarations.entries.isNotEmpty ||
-    swidClass.methods.isNotEmpty;
+    swidClass.methods.isNotEmpty ||
+    swidClass.staticConstFieldDeclarations.firstWhere(
+          (x) => isInexpressibleStaticConst(
+              parentClass: swidClass, staticConst: x.value),
+          orElse: () => null,
+        ) !=
+        null;
--- lib/swid/ir/backend/util/requiresDartClassTranslationUnit.dart
+++ lib/swid/ir/backend/util/requiresDartClassTranslationUnit.dart
@@ -2,8 +2,16 @@ import 'package:meta/meta.dart';
 
 import 'package:hydro_sdk/swid/ir/backend/util/requiresDartBinding.dart';
 import 'package:hydro_sdk/swid/ir/frontend/dart/swidClass.dart';
+import 'package:hydro_sdk/swid/ir/frontend/dart/util/isInexpressibleStaticConst.dart';
 
 bool requiresDartClassTranslationUnit({@required SwidClass swidClass}) =>
     swidClass.name[0] != "_" &&
     swidClass.originalPackagePath != "dart:_internal" &&
-    (requiresDartBinding(swidClass: swidClass) || swidClass.isConstructible());
+    (requiresDartBinding(swidClass: swidClass) ||
+        swidClass.isConstructible() ||
+        swidClass.staticConstFieldDeclarations.firstWhere(
+              (x) => isInexpressibleStaticConst(
+                  parentClass: swidClass, staticConst: x.value),
+              orElse: () => null,
+            ) !=
+            null);
