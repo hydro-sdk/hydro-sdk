a2bf8f4ff29b494c1cac5cf6db519e96a4a1d019
Chris Gibb <chris.gibb@outlook.com>
2021-02-15 13:55:43 -0500

    reemit

--- runtime/dart/typed_data/endian.ts
+++ runtime/dart/typed_data/endian.ts
@@ -1,5 +1,11 @@
+declare const dart: {
+    typed_data: {
+        endianBig: (this: void) => Endian;
+        endianLittle: (this: void) => Endian;
+    };
+};
 export interface IEndian {}
 export class Endian {
-    public static big = Endian._(false);
-    public static little = Endian._(true);
+    public static big = dart.typed_data.endianBig();
+    public static little = dart.typed_data.endianLittle();
 }
