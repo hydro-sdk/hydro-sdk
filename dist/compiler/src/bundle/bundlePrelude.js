417f3b64308d55c807e4557bbfb2a8417e280949
Chris Gibb <chris.gibb@outlook.com>
2021-02-16 23:37:50 -0500

    use suggested bundle prelude

--- dist/compiler/src/bundle/bundlePrelude.js
+++ dist/compiler/src/bundle/bundlePrelude.js
@@ -1,3 +1,3 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.bundlePrelude = "\nlocal package = {preload={}}\nlocal function require(file)\n    return package.preload[file]()\nend\n";
+exports.bundlePrelude = "\nlocal package = {preload={}, loaded={}}\nlocal function require(file)\n    local loadedModule = package.loaded[file]\n    if loadedModule == nil\n    then\n        loadedModule = package.preload[file]()\n        package.loaded[file] = loadedModule\n        return loadedModule\n    end\n    return loadedModule\nend\n";
