0ccc582eace8d0dc1aa1655406b09f08580bf44a
Chris Gibb <chris.gibb@outlook.com>
2021-02-18 23:58:51 -0500

    test for instantiating only select generics in class methods

---
+++ test/swid/generics/instantiateGenericsInClassMethodsOneLevel_test.dart
@@ -0,0 +1,192 @@
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:hydro_sdk/swid/ir/frontend/dart/swidClass.dart';
+import 'package:hydro_sdk/swid/ir/frontend/dart/swidDeclarationModifiers.dart';
+import 'package:hydro_sdk/swid/ir/frontend/dart/swidFunctionType.dart';
+import 'package:hydro_sdk/swid/ir/frontend/dart/swidInterface.dart';
+import 'package:hydro_sdk/swid/ir/frontend/dart/swidNullabilitySuffix.dart';
+import 'package:hydro_sdk/swid/ir/frontend/dart/swidReferenceDeclarationKind.dart';
+import 'package:hydro_sdk/swid/ir/frontend/dart/swidType.dart';
+import 'package:hydro_sdk/swid/ir/frontend/dart/swidTypeFormal.dart';
+import 'package:hydro_sdk/swid/ir/frontend/dart/util/instantiateGeneric.dart';
+
+void main() {
+  LiveTestWidgetsFlutterBinding();
+  testWidgets('', (WidgetTester tester) async {
+    var iterable = SwidClass(
+      name: "Iterable",
+      nullabilitySuffix: SwidNullabilitySuffix.none,
+      originalPackagePath: "dart:core",
+      constructorType: null,
+      factoryConstructors: [],
+      staticMethods: [],
+      methods: [
+        SwidFunctionType(
+          name: "expand",
+          nullabilitySuffix: SwidNullabilitySuffix.none,
+          originalPackagePath: "dart:core",
+          swidDeclarationModifiers: SwidDeclarationModifiers.empty(),
+          namedParameterTypes: {},
+          namedDefaults: {},
+          normalParameterNames: ["f"],
+          normalParameterTypes: [
+            SwidType.fromSwidFunctionType(
+              swidFunctionType: SwidFunctionType(
+                name: "",
+                nullabilitySuffix: SwidNullabilitySuffix.none,
+                originalPackagePath: "",
+                swidDeclarationModifiers: SwidDeclarationModifiers.empty(),
+                namedParameterTypes: {},
+                namedDefaults: {},
+                normalParameterNames: ["element"],
+                normalParameterTypes: [
+                  SwidType.fromSwidInterface(
+                    swidInterface: SwidInterface(
+                      name: "E",
+                      nullabilitySuffix: SwidNullabilitySuffix.none,
+                      originalPackagePath: "dart:core",
+                      typeArguments: [],
+                      referenceDeclarationKind:
+                          SwidReferenceDeclarationKind.typeParameterType,
+                    ),
+                  ),
+                ],
+                optionalParameterNames: [],
+                optionalParameterTypes: [],
+                returnType: SwidType.fromSwidInterface(
+                  swidInterface: SwidInterface(
+                    name: "Iterable<T>",
+                    nullabilitySuffix: SwidNullabilitySuffix.none,
+                    originalPackagePath: "dart:core",
+                    typeArguments: [
+                      SwidType.fromSwidInterface(
+                        swidInterface: SwidInterface(
+                          name: "T",
+                          nullabilitySuffix: SwidNullabilitySuffix.none,
+                          originalPackagePath: "dart:core",
+                          typeArguments: [],
+                          referenceDeclarationKind:
+                              SwidReferenceDeclarationKind.typeParameterType,
+                        ),
+                      ),
+                    ],
+                    referenceDeclarationKind:
+                        SwidReferenceDeclarationKind.classElement,
+                  ),
+                ),
+                isFactory: false,
+                typeFormals: [],
+              ),
+            )
+          ],
+          optionalParameterNames: [],
+          optionalParameterTypes: [],
+          returnType: SwidType.fromSwidInterface(
+            swidInterface: SwidInterface(
+              name: "Iterable<T>",
+              nullabilitySuffix: SwidNullabilitySuffix.none,
+              originalPackagePath: "dart:core",
+              typeArguments: [
+                SwidType.fromSwidInterface(
+                  swidInterface: SwidInterface(
+                    name: "T",
+                    nullabilitySuffix: SwidNullabilitySuffix.none,
+                    originalPackagePath: "dart:core",
+                    typeArguments: [],
+                    referenceDeclarationKind:
+                        SwidReferenceDeclarationKind.typeParameterType,
+                  ),
+                )
+              ],
+              referenceDeclarationKind:
+                  SwidReferenceDeclarationKind.classElement,
+            ),
+          ),
+          isFactory: false,
+          typeFormals: [
+            SwidTypeFormal(
+              value: SwidTypeFormalValue.fromString(string: "T"),
+              swidReferenceDeclarationKind:
+                  SwidReferenceDeclarationKind.typeParameterType,
+            )
+          ],
+        ),
+      ],
+      staticConstFieldDeclarations: [],
+      instanceFieldDeclarations: {},
+      swidDeclarationModifiers: SwidDeclarationModifiers.empty(),
+      mixedInClasses: [],
+      implementedClasses: [],
+      extendedClass: null,
+      isMixin: false,
+      typeFormals: [
+        SwidTypeFormal(
+          value: SwidTypeFormalValue.fromString(string: "E"),
+          swidReferenceDeclarationKind:
+              SwidReferenceDeclarationKind.typeParameterType,
+        )
+      ],
+    );
+
+    var replacedIterable = instantiateGeneric(
+      genericInstantiator: SwidGenericInstantiator(
+        name: "E",
+        type: SwidType.fromSwidInterface(
+          swidInterface: SwidInterface(
+            name: "double",
+            nullabilitySuffix: SwidNullabilitySuffix.none,
+            originalPackagePath: "dart:core",
+            typeArguments: [],
+            referenceDeclarationKind: SwidReferenceDeclarationKind.classElement,
+          ),
+        ),
+      ),
+      swidType: SwidType.fromSwidClass(
+        swidClass: iterable,
+      ),
+    ).when(
+      fromSwidInterface: (_) => null,
+      fromSwidClass: (val) => val,
+      fromSwidDefaultFormalParameter: (_) => null,
+      fromSwidFunctionType: (_) => null,
+    );
+
+    //All instances of T should be left alone
+    expect(replacedIterable.methods.first.typeFormals.isNotEmpty, true);
+    expect(replacedIterable.methods.first.typeFormals.first.value.displayName,
+        "T");
+    expect(replacedIterable.methods.first.typeFormals,
+        iterable.methods.first.typeFormals);
+    expect(
+        replacedIterable.methods.first.returnType.displayName, "Iterable<T>");
+    expect(replacedIterable.methods.first.returnType,
+        iterable.methods.first.returnType);
+    expect(
+        replacedIterable.methods.first.normalParameterTypes.first
+            .maybeWhen(fromSwidFunctionType: (val) => val, orElse: () => null)
+            .returnType
+            .displayName,
+        "Iterable<T>");
+    expect(
+        replacedIterable.methods.first.normalParameterTypes.first
+            .maybeWhen(fromSwidFunctionType: (val) => val, orElse: () => null)
+            .returnType,
+        iterable.methods.first.normalParameterTypes.first
+            .maybeWhen(fromSwidFunctionType: (val) => val, orElse: () => null)
+            .returnType);
+    //E should have been replaced with double
+    expect(
+        replacedIterable.methods.first.normalParameterTypes.first
+            .maybeWhen(fromSwidFunctionType: (val) => val, orElse: () => null)
+            .normalParameterTypes
+            .first
+            .maybeWhen(fromSwidInterface: (val) => val, orElse: () => null),
+        SwidInterface(
+          name: "double",
+          nullabilitySuffix: SwidNullabilitySuffix.none,
+          originalPackagePath: "dart:core",
+          typeArguments: [],
+          referenceDeclarationKind: SwidReferenceDeclarationKind.classElement,
+        ));
+  }, tags: "swid");
+}
