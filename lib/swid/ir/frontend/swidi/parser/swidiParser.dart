9a402b7589155542d2297d5d5bdd43613d5d248a
Chris Gibb <chris.gibb@outlook.com>
2021-02-23 23:34:07 -0500

    mixin function declaration parser

--- lib/swid/ir/frontend/swidi/parser/swidiParser.dart
+++ lib/swid/ir/frontend/swidi/parser/swidiParser.dart
@@ -1,6 +1,8 @@
 import 'package:hydro_sdk/swid/ir/frontend/swidi/grammar/swidiGrammarDefinition.dart';
 import 'package:hydro_sdk/swid/ir/frontend/swidi/parser/swidiClassParser.dart';
+import 'package:hydro_sdk/swid/ir/frontend/swidi/parser/swidiFunctionDeclarationParser.dart';
 
-class SwidiParser extends SwidiGrammarDefinition with SwidiClassParser {
+class SwidiParser extends SwidiGrammarDefinition
+    with SwidiClassParser, SwidiFunctionDeclarationParser {
   const SwidiParser();
 }
