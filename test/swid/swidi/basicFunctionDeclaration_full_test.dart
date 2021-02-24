26ed937f536edf6e65fab188380fe11edf5d5793
Chris Gibb <chris.gibb@outlook.com>
2021-02-24 00:01:20 -0500

    start test for basic function declaration with full parser

---
+++ test/swid/swidi/basicFunctionDeclaration_full_test.dart
@@ -0,0 +1,21 @@
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:hydro_sdk/swid/ir/frontend/swidi/ast/swidiFunctionDeclaration.dart';
+import 'package:hydro_sdk/swid/ir/frontend/swidi/parser/swidiParser.dart';
+
+import 'lib/parserTestHarness.dart';
+
+void main() {
+  LiveTestWidgetsFlutterBinding();
+  testWidgets('', (WidgetTester tester) async {
+    parserTestHarness(
+      input: """
+  class IconData {
+    void foo();
+  }
+    """,
+      parser: const SwidiParser().build(),
+      result: const SwidiFunctionDeclaration(returnType: "void", name: "foo"),
+    );
+  }, tags: "swid");
+}
