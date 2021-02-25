98175aa6caaa785687072f45135d82d8a0d04505
Chris Gibb <chris.gibb@outlook.com>
2021-02-24 23:57:13 -0500

    mixin more parsers

--- lib/swid/ir/frontend/swidi/parser/swidiParser.dart
+++ lib/swid/ir/frontend/swidi/parser/swidiParser.dart
@@ -1,8 +1,14 @@
 import 'package:hydro_sdk/swid/ir/frontend/swidi/grammar/swidiGrammarDefinition.dart';
 import 'package:hydro_sdk/swid/ir/frontend/swidi/parser/swidiClassParser.dart';
 import 'package:hydro_sdk/swid/ir/frontend/swidi/parser/swidiFunctionDeclarationParser.dart';
+import 'package:hydro_sdk/swid/ir/frontend/swidi/parser/swidiSimpleDeclarationParser.dart';
+import 'package:hydro_sdk/swid/ir/frontend/swidi/parser/swidiTypeParser.dart';
 
 class SwidiParser extends SwidiGrammarDefinition
-    with SwidiClassParser, SwidiFunctionDeclarationParser {
+    with
+        SwidiClassParser,
+        SwidiFunctionDeclarationParser,
+        SwidiTypeParser,
+        SwidiSimpleDeclarationParser {
   const SwidiParser();
 }
