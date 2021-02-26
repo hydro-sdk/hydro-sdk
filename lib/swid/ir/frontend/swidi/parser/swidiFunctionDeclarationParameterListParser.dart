d9b4ffa385da4082cb564d4140af3255c6b51c4d
Chris Gibb <chris.gibb@outlook.com>
2021-02-25 22:55:39 -0500

    wip test for multiple positional parameters

--- lib/swid/ir/frontend/swidi/parser/swidiFunctionDeclarationParameterListParser.dart
+++ lib/swid/ir/frontend/swidi/parser/swidiFunctionDeclarationParameterListParser.dart
@@ -6,13 +6,22 @@ import 'package:hydro_sdk/swid/ir/frontend/swidi/parser/swidiSimpleDeclarationPa
 
 mixin SwidiFunctionDeclarationParameterListParser
     on SwidiGrammarDefinition, SwidiSimpleDeclarationParser {
-  Parser<SwidiDeclaration> functionDeclarationParameterList() =>
+  Parser<List<SwidiDeclaration>> functionDeclarationParameterList() =>
       super.functionDeclarationParameterList().map((x) {
-        var res = List.from(x)
-            .where((e) => e != null)
-            .whereType<SwidiDeclaration>()
-            .toList();
-
-        return res.isNotEmpty ? res.first : null;
+        var res = [
+          ...List.from(x)
+              .where((e) => e != null)
+              .whereType<SwidiDeclaration>()
+              .toList(),
+          ...List.from(x)
+              .where((e) => e != null)
+              .whereType<List>()
+              .where((e) => e != null)
+              .reduce((value, element) => [...value, ...element])
+              .where((e) => e != null)
+              .whereType<SwidiDeclaration>()
+              .toList()
+        ];
+        return res;
       });
 }
---
+++ test/swid/swidi/functionDeclarationMultiplePositionalParameters_test.dart
@@ -0,0 +1,33 @@
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:hydro_sdk/swid/ir/frontend/swidi/ast/swidiDeclaration.dart';
+import 'package:hydro_sdk/swid/ir/frontend/swidi/ast/swidiInterface.dart';
+import 'package:hydro_sdk/swid/ir/frontend/swidi/grammar/swidiGrammarDefinition.dart';
+import 'package:hydro_sdk/swid/ir/frontend/swidi/parser/swidiFunctionDeclarationParameterListParser.dart';
+import 'package:hydro_sdk/swid/ir/frontend/swidi/parser/swidiSimpleDeclarationParser.dart';
+import 'package:hydro_sdk/swid/ir/frontend/swidi/parser/swidiTypeParser.dart';
+import 'lib/parserTestHarness.dart';
+
+class BasicFunctionParameterListParser extends SwidiGrammarDefinition
+    with
+        SwidiTypeParser,
+        SwidiSimpleDeclarationParser,
+        SwidiFunctionDeclarationParameterListParser {
+  const BasicFunctionParameterListParser();
+}
+
+void main() {
+  LiveTestWidgetsFlutterBinding();
+  testWidgets('', (WidgetTester tester) async {
+    parserTestHarness(
+        input: "(void foo,int bar)",
+        parser: const BasicFunctionParameterListParser().build(
+            start: const BasicFunctionParameterListParser()
+                .functionDeclarationParameterList),
+        result: [
+          const SwidiDeclaration(
+              name: "foo", type: SwidiInterface(name: "void")),
+          const SwidiDeclaration(name: "bar", type: SwidiInterface(name: "int"))
+        ]);
+  }, tags: "swid");
+}
