00853b8c192eaa7f9f62142e6c8c925779ec140a
Chris Gibb <chris.gibb@outlook.com>
2021-02-28 13:12:56 -0500

    optional parameter form3 tests

---
+++ test/swid/swidi/functionDeclarationOptionalParameterListForm3_test.dart
@@ -0,0 +1,49 @@
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:hydro_sdk/swid/ir/frontend/swidi/ast/swidiDeclaration.dart';
+import 'package:hydro_sdk/swid/ir/frontend/swidi/ast/swidiInterface.dart';
+import 'package:hydro_sdk/swid/ir/frontend/swidi/ast/swidiOptionalParameter.dart';
+import 'package:hydro_sdk/swid/ir/frontend/swidi/ast/swidiPositionalOrOptionalParameter.dart';
+import 'package:hydro_sdk/swid/ir/frontend/swidi/grammar/swidiGrammarDefinition.dart';
+import 'package:hydro_sdk/swid/ir/frontend/swidi/parser/swidiFunctionDeclarationOptionalParameterParser.dart';
+import 'package:hydro_sdk/swid/ir/frontend/swidi/parser/swidiFunctionDeclarationParameterListParser.dart';
+import 'package:hydro_sdk/swid/ir/frontend/swidi/parser/swidiFunctionDeclarationPositionalParameterParser.dart';
+import 'package:hydro_sdk/swid/ir/frontend/swidi/parser/swidiSimpleDeclarationParser.dart';
+import 'package:hydro_sdk/swid/ir/frontend/swidi/parser/swidiTypeParser.dart';
+import 'lib/parserTestHarness.dart';
+
+class BasicFunctionParameterListParser extends SwidiGrammarDefinition
+    with
+        SwidiTypeParser,
+        SwidiSimpleDeclarationParser,
+        SwidiFunctionDeclarationOptionalParameterParser,
+        SwidiFunctionDeclarationPositionalParameterParser,
+        SwidiFunctionDeclarationParameterListParser {
+  const BasicFunctionParameterListParser();
+}
+
+void main() {
+  LiveTestWidgetsFlutterBinding();
+  testWidgets('', (WidgetTester tester) async {
+    parserTestHarness(
+        input: const ParserTestHarnessInput.fromList(
+            inputs: ["([void foo,int bar,int baz,])"]),
+        parser: const BasicFunctionParameterListParser().build(
+            start: const BasicFunctionParameterListParser()
+                .functionDeclarationParameterList),
+        result: const [
+          SwidiPositionalOrOptionalParameter.fromSwidiOptionalParameter(
+              optionalParameter: SwidiOptionalParameter(
+                  declaration: SwidiDeclaration(
+                      name: "foo", type: SwidiInterface(name: "void")))),
+          SwidiPositionalOrOptionalParameter.fromSwidiOptionalParameter(
+              optionalParameter: SwidiOptionalParameter(
+                  declaration: SwidiDeclaration(
+                      name: "bar", type: SwidiInterface(name: "int")))),
+          SwidiPositionalOrOptionalParameter.fromSwidiOptionalParameter(
+              optionalParameter: SwidiOptionalParameter(
+                  declaration: SwidiDeclaration(
+                      name: "baz", type: SwidiInterface(name: "int"))))
+        ]);
+  }, tags: "swid");
+}
