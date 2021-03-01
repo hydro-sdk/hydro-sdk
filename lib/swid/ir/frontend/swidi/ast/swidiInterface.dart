23f97f7ff7362a98716bad1d438278e0f43d250f
Chris Gibb <chris.gibb@outlook.com>
2021-02-28 21:45:11 -0500

    format

--- lib/swid/ir/frontend/swidi/ast/swidiInterface.dart
+++ lib/swid/ir/frontend/swidi/ast/swidiInterface.dart
@@ -1,7 +1,7 @@
 import 'package:freezed_annotation/freezed_annotation.dart';
-import 'package:hydro_sdk/swid/ir/frontend/swidi/ast/swidiLibraryScopePrefix.dart';
 import 'package:meta/meta.dart';
 
+import 'package:hydro_sdk/swid/ir/frontend/swidi/ast/swidiLibraryScopePrefix.dart';
 import 'package:hydro_sdk/swid/ir/frontend/swidi/ast/swidiNullabilitySuffix.dart';
 
 part 'swidiInterface.freezed.dart';
--- lib/swid/ir/frontend/swidi/parser/swidiLibraryScopePrefixParser.dart
+++ lib/swid/ir/frontend/swidi/parser/swidiLibraryScopePrefixParser.dart
@@ -1,7 +1,8 @@
+import 'package:petitparser/petitparser.dart';
+
 import 'package:hydro_sdk/swid/ir/frontend/swidi/ast/swidiLibraryScopePrefix.dart';
 import 'package:hydro_sdk/swid/ir/frontend/swidi/grammar/swidiGrammarDefinition.dart';
 import 'package:hydro_sdk/swid/ir/frontend/swidi/parser/util/collectTokens.dart';
-import 'package:petitparser/petitparser.dart';
 
 mixin SwidiLibraryScopePrefixParser on SwidiGrammarDefinition {
   Parser<SwidiLibraryScopePrefix> libraryScopePrefix() =>
