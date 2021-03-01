7996d7e23014bc964d80dd71a25d5162748c4789
Chris Gibb <chris.gibb@outlook.com>
2021-02-28 20:02:17 -0500

    test and full test for named parameters form 2

---
+++ test/swid/swidi/functionDeclarationNamedParameterListForm2_full_test.dart
@@ -0,0 +1,46 @@
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:hydro_sdk/swid/ir/frontend/swidi/ast/swidiClass.dart';
+import 'package:hydro_sdk/swid/ir/frontend/swidi/ast/swidiDeclaration.dart';
+import 'package:hydro_sdk/swid/ir/frontend/swidi/ast/swidiFunctionDeclaration.dart';
+import 'package:hydro_sdk/swid/ir/frontend/swidi/ast/swidiInterface.dart';
+import 'package:hydro_sdk/swid/ir/frontend/swidi/ast/swidiNamedParameter.dart';
+import 'package:hydro_sdk/swid/ir/frontend/swidi/parser/swidiParser.dart';
+import 'lib/parserTestHarness.dart';
+
+void main() {
+  LiveTestWidgetsFlutterBinding();
+  testWidgets('', (WidgetTester tester) async {
+    parserTestHarness(
+      input: const ParserTestHarnessInput.fromList(inputs: [
+        """
+  class IconData {
+    void foo({int bar,int baz,int qux});
+  }
+    """,
+      ]),
+      parser: const SwidiParser().build(),
+      result: [
+        const SwidiClass(name: "IconData", methods: [
+          SwidiFunctionDeclaration(
+            name: "foo",
+            returnType: "void",
+            optionalParameters: [],
+            positionalParameters: [],
+            namedParameters: [
+              SwidiNamedParameter(
+                  declaration: SwidiDeclaration(
+                      name: "bar", type: SwidiInterface(name: "int"))),
+              SwidiNamedParameter(
+                  declaration: SwidiDeclaration(
+                      name: "baz", type: SwidiInterface(name: "int"))),
+              SwidiNamedParameter(
+                  declaration: SwidiDeclaration(
+                      name: "qux", type: SwidiInterface(name: "int")))
+            ],
+          )
+        ]),
+      ],
+    );
+  }, tags: "swid");
+}
---
+++ test/swid/swidi/functionDeclarationNamedParameterListForm2_test.dart
@@ -0,0 +1,51 @@
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:hydro_sdk/swid/ir/frontend/swidi/ast/swidiDeclaration.dart';
+import 'package:hydro_sdk/swid/ir/frontend/swidi/ast/swidiInterface.dart';
+import 'package:hydro_sdk/swid/ir/frontend/swidi/ast/swidiNamedParameter.dart';
+import 'package:hydro_sdk/swid/ir/frontend/swidi/ast/swidiPositionalOrOptionalOrNamedParameter.dart';
+import 'package:hydro_sdk/swid/ir/frontend/swidi/grammar/swidiGrammarDefinition.dart';
+import 'package:hydro_sdk/swid/ir/frontend/swidi/parser/swidiFunctionDeclarationNamedParameterParser.dart';
+import 'package:hydro_sdk/swid/ir/frontend/swidi/parser/swidiFunctionDeclarationOptionalParameterParser.dart';
+import 'package:hydro_sdk/swid/ir/frontend/swidi/parser/swidiFunctionDeclarationParameterListParser.dart';
+import 'package:hydro_sdk/swid/ir/frontend/swidi/parser/swidiFunctionDeclarationPositionalParameterParser.dart';
+import 'package:hydro_sdk/swid/ir/frontend/swidi/parser/swidiSimpleDeclarationParser.dart';
+import 'package:hydro_sdk/swid/ir/frontend/swidi/parser/swidiTypeParser.dart';
+import 'lib/parserTestHarness.dart';
+
+class BasicFunctionParameterListParser extends SwidiGrammarDefinition
+    with
+        SwidiTypeParser,
+        SwidiSimpleDeclarationParser,
+        SwidiFunctionDeclarationOptionalParameterParser,
+        SwidiFunctionDeclarationPositionalParameterParser,
+        SwidiFunctionDeclarationNamedParameterParser,
+        SwidiFunctionDeclarationParameterListParser {
+  const BasicFunctionParameterListParser();
+}
+
+void main() {
+  LiveTestWidgetsFlutterBinding();
+  testWidgets('', (WidgetTester tester) async {
+    parserTestHarness(
+        input: const ParserTestHarnessInput.fromList(
+            inputs: ["({void foo,int bar,int baz})"]),
+        parser: const BasicFunctionParameterListParser().build(
+            start: const BasicFunctionParameterListParser()
+                .functionDeclarationParameterList),
+        result: const [
+          SwidiPositionalOrOptionalOrNamedParameter.fromSwidiNamedParameter(
+              namedParameter: SwidiNamedParameter(
+                  declaration: SwidiDeclaration(
+                      name: "foo", type: SwidiInterface(name: "void")))),
+          SwidiPositionalOrOptionalOrNamedParameter.fromSwidiNamedParameter(
+              namedParameter: SwidiNamedParameter(
+                  declaration: SwidiDeclaration(
+                      name: "bar", type: SwidiInterface(name: "int")))),
+          SwidiPositionalOrOptionalOrNamedParameter.fromSwidiNamedParameter(
+              namedParameter: SwidiNamedParameter(
+                  declaration: SwidiDeclaration(
+                      name: "baz", type: SwidiInterface(name: "int"))))
+        ]);
+  }, tags: "swid");
+}
