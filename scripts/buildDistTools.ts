821c19a6baabcb80d06082f91d16538dec3050b4
Chris Gibb <chris.gibb@outlook.com>
2021-03-16 14:35:30 -0400

    try to play nicer on windows

--- scripts/buildDistTools.ts
+++ scripts/buildDistTools.ts
@@ -20,17 +20,18 @@ import * as path from "path";
     for (let dartEntryPoint of dartEntryPoints) {
         const startTime = +new Date();
         const outputPath = `${outputFolder}${path.sep}${path.parse(dartEntryPoint).name}-${process.platform}-${process.arch}`;
+
         console.log(`Building ${dartEntryPoint} -> ${outputPath}`);
 
-        const dart2Native = cp.spawnSync("dart2native", [`bin/${dartEntryPoint}`, '-v', '-o',
+        const dart2Native = cp.spawnSync("dart2native", [`bin${path.sep}${dartEntryPoint}`, '-v', '-o',
             outputPath
         ]);
 
-        console.log(dart2Native.stdout.toString());
-        console.log(dart2Native.stderr.toString());
+        console.log(dart2Native?.stdout?.toString());
+        console.log(dart2Native?.stderr?.toString());
 
-        if (dart2Native.error) {
-            throw dart2Native.error;
+        if (dart2Native?.error) {
+            throw dart2Native?.error;
         }
 
         console.log(`Finished in ${+new Date() - startTime}`);
