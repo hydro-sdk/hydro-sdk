c12ef10de28138f63e70e77f055926e865f27d4b
Chris Gibb <chris.gibb@outlook.com>
2021-03-17 15:48:35 -0400

    bail out of sdk tool building if any single one fails

--- scripts/buildSdkTools.bash
+++ scripts/buildSdkTools.bash
@@ -1,6 +1,6 @@
-./node_modules/.bin/tsc
-./node_modules/.bin/tsc -p sdk-tools/ts2hc/tsconfig.json
+./node_modules/.bin/tsc || exit 1
+./node_modules/.bin/tsc -p sdk-tools/ts2hc/tsconfig.json || exit 1
 
-node scripts/buildLua52SdkTool
-node scripts/buildDartSdkTools
-node scripts/buildTsSdkTools
+node scripts/buildLua52SdkTool || exit 1
+node scripts/buildDartSdkTools || exit 1
+node scripts/buildTsSdkTools || exit 1
