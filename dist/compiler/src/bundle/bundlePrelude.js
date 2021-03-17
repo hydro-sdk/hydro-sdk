3971c20a5a8338e021cd4522bef4fe44a37dfb0c
Chris Gibb <chris.gibb@outlook.com>
2021-03-17 15:36:22 -0400

    delete dist directory

--- dist/compiler/src/bundle/bundlePrelude.js
+++
@@ -1,3 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.bundlePrelude = "\nlocal package = {preload={}, loaded={}}\nlocal function require(file)\n    local loadedModule = package.loaded[file]\n    if loadedModule == nil\n    then\n        loadedModule = package.preload[file]()\n        package.loaded[file] = loadedModule\n        return loadedModule\n    end\n    return loadedModule\nend\n";
