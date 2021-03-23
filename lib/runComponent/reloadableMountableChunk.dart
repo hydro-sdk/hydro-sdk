72c9c66f56d4c75a9369b048599d7fff6ab66ae1
Chris Gibb <chris.gibb@outlook.com>
2021-03-23 08:47:55 -0400

    refactor run component

---
+++ lib/runComponent/reloadableMountableChunk.dart
@@ -0,0 +1,56 @@
+part of hydro_sdk.run_component;
+
+mixin ReloadableMountableChunk<T extends StatefulWidget> on State<T>
+    implements HotReloadable<T> {
+  Future<void> maybeReloadMountableChunk({
+    @required Uint8List rawPackage,
+    @required String component,
+    @required
+        Map<String, Prototype Function({CodeDump codeDump, Prototype parent})>
+            thunks,
+  }) async {
+    final decodedBzip2 = BZip2Decoder().decodeBytes(rawPackage);
+    final decodedTar = TarDecoder().decodeBytes(decodedBzip2);
+
+    final manifest = PackageManifest.fromJson(jsonDecode(
+        _getFileContentFromArchiveAsString(
+            fileName: "${component}/manifest.json",
+            archive: decodedTar)));
+
+    final debugInfo = jsonDecode(_getFileContentFromArchiveAsString(
+            fileName:
+                "${component}/${manifest.mountableChunk}.hc.symbols",
+            archive: decodedTar))
+        .map((x) => ModuleDebugInfo.fromJson(x))
+        .toList()
+        .cast<ModuleDebugInfo>();
+
+    final mountableChunk = _getFileContentFromArchive(
+        fileName: "${component}/${manifest.mountableChunk}.hc",
+        archive: decodedTar);
+
+    if (res == null) {
+      await fullRestart(
+        bytecodeImage: Uint8List.fromList(mountableChunk),
+        baseUrl: manifest.mountableChunk,
+        symbols: debugInfo,
+        thunks: thunks,
+      );
+    } else {
+      var status = await hotReload(
+        bytecodeImage: Uint8List.fromList(mountableChunk),
+        baseUrl: manifest.mountableChunk,
+        symbols: debugInfo,
+        thunks: thunks,
+      );
+      if (!status) {
+        await fullRestart(
+          bytecodeImage: Uint8List.fromList(mountableChunk),
+          baseUrl: manifest.mountableChunk,
+          symbols: debugInfo,
+          thunks: thunks,
+        );
+      }
+    }
+  }
+}
---
+++ lib/runComponent/runComponent.dart
@@ -0,0 +1,66 @@
+library hydro_sdk.run_component;
+
+import 'dart:async';
+import 'dart:convert';
+import 'dart:io';
+import 'dart:typed_data';
+
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+
+import 'package:archive/archive_io.dart';
+import 'package:http/http.dart';
+
+import 'package:hydro_sdk/build-project/packageManifest.dart';
+import 'package:hydro_sdk/cfr/builtins/boxing/unboxers.dart';
+import 'package:hydro_sdk/cfr/decode/codedump.dart';
+import 'package:hydro_sdk/cfr/hotReloadable.dart';
+import 'package:hydro_sdk/cfr/moduleDebugInfo.dart';
+import 'package:hydro_sdk/cfr/preloadCustomNamespaces.dart';
+import 'package:hydro_sdk/cfr/vm/prototype.dart';
+
+part 'runDebugComponent.dart';
+part 'debugUrls.dart';
+part 'debugDownload.dart';
+part 'reloadableMountableChunk.dart';
+
+class RunComponent extends StatelessWidget {
+  final String project;
+  final String component;
+  final Map<String, Prototype Function({CodeDump codeDump, Prototype parent})>
+      thunks;
+
+  const RunComponent({
+    @required this.project,
+    @required this.component,
+    this.thunks = const {},
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    if (kDebugMode) {
+      return _RunDebugComponent(
+        project: project,
+        component: component,
+        thunks: thunks,
+      );
+    } else {
+      return const SizedBox();
+    }
+  }
+}
+
+List<int> _getFileContentFromArchive({
+  @required String fileName,
+  @required Archive archive,
+}) =>
+    archive.fileData(archive.files.indexWhere((x) => x.name == fileName));
+
+String _getFileContentFromArchiveAsString({
+  @required String fileName,
+  @required Archive archive,
+}) =>
+    String.fromCharCodes(_getFileContentFromArchive(
+      fileName: fileName,
+      archive: archive,
+    ));
