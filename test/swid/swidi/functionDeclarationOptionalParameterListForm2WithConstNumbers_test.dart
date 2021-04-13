9e2dca35a89fc2eb5058e17c89253dadc966885f
Chris Gibb <chris.gibb@outlook.com>
2021-04-12 22:42:33 -0400

    optional positional parameter form 2 with const numbers

---
+++ test/swid/swidi/functionDeclarationOptionalParameterListForm2WithConstNumbers_test.dart
@@ -0,0 +1,175 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:hydro_sdk/swid/frontend/swidi/ast/swidiConst.dart';
+import 'package:hydro_sdk/swid/frontend/swidi/ast/swidiConstNumber.dart';
+
+import 'package:hydro_sdk/swid/frontend/swidi/ast/swidiDeclaration.dart';
+import 'package:hydro_sdk/swid/frontend/swidi/ast/swidiInterface.dart';
+import 'package:hydro_sdk/swid/frontend/swidi/ast/swidiLibraryScopePrefix.dart';
+import 'package:hydro_sdk/swid/frontend/swidi/ast/swidiNullabilitySuffix.dart';
+import 'package:hydro_sdk/swid/frontend/swidi/ast/swidiOptionalParameter.dart';
+import 'package:hydro_sdk/swid/frontend/swidi/ast/swidiPositionalOrOptionalOrNamedParameter.dart';
+import 'package:hydro_sdk/swid/frontend/swidi/ast/swidiReferenceDeclarationPrefix.dart';
+import 'package:hydro_sdk/swid/frontend/swidi/grammar/swidiGrammarDefinition.dart';
+import 'package:hydro_sdk/swid/frontend/swidi/parser/swidiConstNumberParser.dart';
+import 'package:hydro_sdk/swid/frontend/swidi/parser/swidiConstParser.dart';
+import 'package:hydro_sdk/swid/frontend/swidi/parser/swidiFunctionDeclarationNamedParameterParser.dart';
+import 'package:hydro_sdk/swid/frontend/swidi/parser/swidiFunctionDeclarationOptionalParameterParser.dart';
+import 'package:hydro_sdk/swid/frontend/swidi/parser/swidiFunctionDeclarationParameterListParser.dart';
+import 'package:hydro_sdk/swid/frontend/swidi/parser/swidiFunctionDeclarationPositionalParameterParser.dart';
+import 'package:hydro_sdk/swid/frontend/swidi/parser/swidiLibraryScopePrefixParser.dart';
+import 'package:hydro_sdk/swid/frontend/swidi/parser/swidiReferenceDeclarationPrefixParser.dart';
+import 'package:hydro_sdk/swid/frontend/swidi/parser/swidiSimpleDeclarationParser.dart';
+import 'package:hydro_sdk/swid/frontend/swidi/parser/swidiTypeParser.dart';
+import 'lib/parserTestHarness.dart';
+
+class BasicFunctionParameterListParser extends SwidiGrammarDefinition
+    with
+        SwidiLibraryScopePrefixParser,
+        SwidiReferenceDeclarationPrefixParser,
+        SwidiTypeParser,
+        SwidiConstNumberParser,
+        SwidiConstParser,
+        SwidiSimpleDeclarationParser,
+        SwidiFunctionDeclarationOptionalParameterParser,
+        SwidiFunctionDeclarationPositionalParameterParser,
+        SwidiFunctionDeclarationNamedParameterParser,
+        SwidiFunctionDeclarationParameterListParser {
+  const BasicFunctionParameterListParser();
+}
+
+void main() {
+  LiveTestWidgetsFlutterBinding();
+  testWidgets('', (WidgetTester tester) async {
+    parserTestHarness(
+      input: const ParserTestHarnessInput.fromList(
+          inputs: ["([void foo = 100,int bar = 100,int baz = 100])"]),
+      parser: const BasicFunctionParameterListParser().build(
+          start: const BasicFunctionParameterListParser()
+              .functionDeclarationParameterList),
+      result: const [
+        SwidiPositionalOrOptionalOrNamedParameter.fromSwidiOptionalParameter(
+          optionalParameter: SwidiOptionalParameter(
+            declaration: SwidiDeclaration(
+              name: "foo",
+              type: SwidiInterface(
+                name: "void",
+                libraryScopePrefix: SwidiLibraryScopePrefix.empty,
+                referenceDeclarationPrefix:
+                    SwidiReferenceDeclarationPrefix.empty,
+                nullabilitySuffix: SwidiNullabilitySuffix.none,
+              ),
+              defaultConstValue: SwidiConst.fromSwidiConstNumber(
+                swidiConstNumber: SwidiConstNumber(
+                  value: "100",
+                ),
+              ),
+            ),
+          ),
+        ),
+        SwidiPositionalOrOptionalOrNamedParameter.fromSwidiOptionalParameter(
+          optionalParameter: SwidiOptionalParameter(
+            declaration: SwidiDeclaration(
+              name: "bar",
+              type: SwidiInterface(
+                name: "int",
+                libraryScopePrefix: SwidiLibraryScopePrefix.empty,
+                referenceDeclarationPrefix:
+                    SwidiReferenceDeclarationPrefix.empty,
+                nullabilitySuffix: SwidiNullabilitySuffix.none,
+              ),
+              defaultConstValue: SwidiConst.fromSwidiConstNumber(
+                swidiConstNumber: SwidiConstNumber(
+                  value: "100",
+                ),
+              ),
+            ),
+          ),
+        ),
+        SwidiPositionalOrOptionalOrNamedParameter.fromSwidiOptionalParameter(
+          optionalParameter: SwidiOptionalParameter(
+            declaration: SwidiDeclaration(
+              name: "baz",
+              type: SwidiInterface(
+                name: "int",
+                libraryScopePrefix: SwidiLibraryScopePrefix.empty,
+                referenceDeclarationPrefix:
+                    SwidiReferenceDeclarationPrefix.empty,
+                nullabilitySuffix: SwidiNullabilitySuffix.none,
+              ),
+              defaultConstValue: SwidiConst.fromSwidiConstNumber(
+                swidiConstNumber: SwidiConstNumber(
+                  value: "100",
+                ),
+              ),
+            ),
+          ),
+        )
+      ],
+    );
+
+    parserTestHarness(
+      input: const ParserTestHarnessInput.fromList(
+          inputs: ["([void foo = 100,int bar = 100,int? baz = 100])"]),
+      parser: const BasicFunctionParameterListParser().build(
+          start: const BasicFunctionParameterListParser()
+              .functionDeclarationParameterList),
+      result: const [
+        SwidiPositionalOrOptionalOrNamedParameter.fromSwidiOptionalParameter(
+            optionalParameter: SwidiOptionalParameter(
+          declaration: SwidiDeclaration(
+            name: "foo",
+            type: SwidiInterface(
+              name: "void",
+              libraryScopePrefix: SwidiLibraryScopePrefix.empty,
+              referenceDeclarationPrefix: SwidiReferenceDeclarationPrefix.empty,
+              nullabilitySuffix: SwidiNullabilitySuffix.none,
+            ),
+            defaultConstValue: SwidiConst.fromSwidiConstNumber(
+              swidiConstNumber: SwidiConstNumber(
+                value: "100",
+              ),
+            ),
+          ),
+        )),
+        SwidiPositionalOrOptionalOrNamedParameter.fromSwidiOptionalParameter(
+          optionalParameter: SwidiOptionalParameter(
+            declaration: SwidiDeclaration(
+              name: "bar",
+              type: SwidiInterface(
+                name: "int",
+                libraryScopePrefix: SwidiLibraryScopePrefix.empty,
+                referenceDeclarationPrefix:
+                    SwidiReferenceDeclarationPrefix.empty,
+                nullabilitySuffix: SwidiNullabilitySuffix.none,
+              ),
+              defaultConstValue: SwidiConst.fromSwidiConstNumber(
+                swidiConstNumber: SwidiConstNumber(
+                  value: "100",
+                ),
+              ),
+            ),
+          ),
+        ),
+        SwidiPositionalOrOptionalOrNamedParameter.fromSwidiOptionalParameter(
+          optionalParameter: SwidiOptionalParameter(
+            declaration: SwidiDeclaration(
+              name: "baz",
+              type: SwidiInterface(
+                name: "int?",
+                libraryScopePrefix: SwidiLibraryScopePrefix.empty,
+                referenceDeclarationPrefix:
+                    SwidiReferenceDeclarationPrefix.empty,
+                nullabilitySuffix: SwidiNullabilitySuffix.question,
+              ),
+              defaultConstValue: SwidiConst.fromSwidiConstNumber(
+                swidiConstNumber: SwidiConstNumber(
+                  value: "100",
+                ),
+              ),
+            ),
+          ),
+        )
+      ],
+    );
+  }, tags: "swid");
+}
