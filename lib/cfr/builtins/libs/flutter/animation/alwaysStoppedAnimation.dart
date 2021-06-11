2c3e200d717604fbec594cb5a31808e3f11ec61a
Chris Gibb <chris.gibb@outlook.com>
2021-06-10 22:40:20 -0400

    fixup nullability

--- lib/cfr/builtins/libs/flutter/animation/alwaysStoppedAnimation.dart
+++ lib/cfr/builtins/libs/flutter/animation/alwaysStoppedAnimation.dart
@@ -9,10 +9,10 @@ import 'package:hydro_sdk/cfr/vm/table.dart';
 import 'package:hydro_sdk/hydroState.dart';
 
 class VMManagedAlwaysStoppedAnimation<T>
-    extends VMManagedBox<AlwaysStoppedAnimation<T>?> {
-  final HydroTable? table;
-  final HydroState? hydroState;
-  final AlwaysStoppedAnimation<T>? vmObject;
+    extends VMManagedBox<AlwaysStoppedAnimation<T>> {
+  final HydroTable table;
+  final HydroState hydroState;
+  final AlwaysStoppedAnimation<T> vmObject;
   VMManagedAlwaysStoppedAnimation({
     required this.table,
     required this.hydroState,
@@ -22,32 +22,35 @@ class VMManagedAlwaysStoppedAnimation<T>
           hydroState: hydroState,
           vmObject: vmObject,
         ) {
-    table!["value"] = makeLuaDartFunc(func: (List<dynamic> args) {
-      return [vmObject!.value];
+    table["value"] = makeLuaDartFunc(func: (List<dynamic> args) {
+      return [vmObject.value];
     });
   }
 }
 
-void loadAlwaysStoppedAnimation(
-    {required HydroState luaState, required HydroTable table}) {
-  registerBoxer<AlwaysStoppedAnimation<Color>>(boxer: (
-      {AlwaysStoppedAnimation<Color>? vmObject,
-      HydroState? hydroState,
-      HydroTable? table}) {
+void loadAlwaysStoppedAnimation({
+  required HydroState luaState,
+  required HydroTable table,
+}) {
+  registerBoxer<AlwaysStoppedAnimation<Color>>(boxer: ({
+    required AlwaysStoppedAnimation<Color> vmObject,
+    required HydroState hydroState,
+    required HydroTable table,
+  }) {
     return VMManagedAlwaysStoppedAnimation<Color>(
         vmObject: vmObject, table: table, hydroState: hydroState);
   });
 
   registerBoxer<AlwaysStoppedAnimation<double>>(boxer: (
-      {AlwaysStoppedAnimation<double>? vmObject,
-      HydroState? hydroState,
-      HydroTable? table}) {
+      {required AlwaysStoppedAnimation<double> vmObject,
+      required HydroState hydroState,
+      required HydroTable table}) {
     return VMManagedAlwaysStoppedAnimation<double>(
         vmObject: vmObject, table: table, hydroState: hydroState);
   });
 
   table["alwaysStoppedAnimation"] = makeLuaDartFunc(func: (List<dynamic> args) {
-    HydroTable? caller = args[0];
+    HydroTable caller = args[0];
     var targetRuntimeType = RuntimeTypes.values.firstWhere(
         (x) => x.toString().split(".")[1] == args[2]["displayName"]);
 
@@ -57,8 +60,8 @@ void loadAlwaysStoppedAnimation(
         break;
       case RuntimeTypes.Color:
         return [
-          maybeBoxObject<AlwaysStoppedAnimation<Color?>>(
-              object: AlwaysStoppedAnimation<Color?>(
+          maybeBoxObject<AlwaysStoppedAnimation<Color>>(
+              object: AlwaysStoppedAnimation<Color>(
                   maybeUnBoxAndBuildArgument<Color>(args[1],
                       parentState: luaState)),
               hydroState: luaState,
