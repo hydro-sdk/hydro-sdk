57cf2990bb6785a3af64bc2b4d5290e5d4f78674
Chris Gibb <chris.gibb@outlook.com>
2021-06-17 08:28:20 -0400

    fixup nullability

--- lib/cfr/builtins/libs/flutter/material/textTheme.dart
+++ lib/cfr/builtins/libs/flutter/material/textTheme.dart
@@ -5,10 +5,10 @@ import 'package:hydro_sdk/cfr/builtins/boxing/boxes.dart';
 import 'package:hydro_sdk/cfr/vm/table.dart';
 import 'package:hydro_sdk/hydroState.dart';
 
-class VMManagedTextTheme extends VMManagedBox<TextTheme?> {
-  final HydroTable? table;
-  final TextTheme? vmObject;
-  final HydroState? hydroState;
+class VMManagedTextTheme extends VMManagedBox<TextTheme> {
+  final HydroTable table;
+  final TextTheme vmObject;
+  final HydroState hydroState;
 
   VMManagedTextTheme({
     required this.table,
@@ -19,38 +19,43 @@ class VMManagedTextTheme extends VMManagedBox<TextTheme?> {
           vmObject: vmObject,
           hydroState: hydroState,
         ) {
-    table!["display1"] = maybeBoxObject<TextStyle?>(
-      object: vmObject!.display1,
-      hydroState: hydroState!,
+    table["display1"] = maybeBoxObject<TextStyle?>(
+      object: vmObject.display1,
+      hydroState: hydroState,
       table: HydroTable(),
     );
-    table!["headline"] = maybeBoxObject<TextStyle?>(
-      object: vmObject!.headline,
-      hydroState: hydroState!,
+    table["headline"] = maybeBoxObject<TextStyle?>(
+      object: vmObject.headline,
+      hydroState: hydroState,
       table: HydroTable(),
     );
-    table!["body1"] = maybeBoxObject<TextStyle?>(
-      object: vmObject!.body1,
-      hydroState: hydroState!,
+    table["body1"] = maybeBoxObject<TextStyle?>(
+      object: vmObject.body1,
+      hydroState: hydroState,
       table: HydroTable(),
     );
 
-    table!["body2"] = maybeBoxObject<TextStyle?>(
-      object: vmObject!.body2,
-      hydroState: hydroState!,
+    table["body2"] = maybeBoxObject<TextStyle?>(
+      object: vmObject.body2,
+      hydroState: hydroState,
       table: HydroTable(),
     );
-    table!["subtitle"] = maybeBoxObject<TextStyle?>(
-      object: vmObject!.subtitle,
-      hydroState: hydroState!,
+    table["subtitle"] = maybeBoxObject<TextStyle?>(
+      object: vmObject.subtitle,
+      hydroState: hydroState,
       table: HydroTable(),
     );
   }
 }
 
-void loadTextTheme({required HydroState hydroState}) {
-  registerBoxer(boxer: (
-      {TextTheme? vmObject, HydroState? hydroState, HydroTable? table}) {
+void loadTextTheme({
+  required HydroState hydroState,
+}) {
+  registerBoxer(boxer: ({
+    required TextTheme vmObject,
+    required HydroState hydroState,
+    required HydroTable table,
+  }) {
     return VMManagedTextTheme(
       vmObject: vmObject,
       hydroState: hydroState,
