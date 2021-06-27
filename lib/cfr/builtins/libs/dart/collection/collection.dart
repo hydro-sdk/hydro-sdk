be5feca342a6a3dec6068d2dbab116c15059ca05
Chris Gibb <chris.gibb@outlook.com>
2021-06-27 10:44:18 -0400

    reemit

--- lib/cfr/builtins/libs/dart/collection/collection.dart
+++ lib/cfr/builtins/libs/dart/collection/collection.dart
@@ -1,7 +1,8 @@
-import 'package:hydro_sdk/cfr/builtins/libs/dart/collection/list.dart';
 import 'package:meta/meta.dart';
 
 import 'package:hydro_sdk/cfr/builtins/libs/dart/collection/iterableBase.dart';
+import 'package:hydro_sdk/cfr/builtins/libs/dart/collection/list.dart';
+import 'package:hydro_sdk/cfr/builtins/libs/dart/collection/listBase.dart';
 import 'package:hydro_sdk/cfr/builtins/libs/dart/collection/listMixin.dart';
 import 'package:hydro_sdk/cfr/vm/table.dart';
 import 'package:hydro_sdk/hydroState.dart';
@@ -11,6 +12,7 @@ void loadcollection(
   final collection = HydroTable();
   table['collection'] = collection;
   loadIterableBase(table: collection, hydroState: hydroState);
+  loadListBase(table: collection, hydroState: hydroState);
   loadListMixin(table: collection, hydroState: hydroState);
   loadCoreList(hydroState: hydroState, table: collection);
 }
