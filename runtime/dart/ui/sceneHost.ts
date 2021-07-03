d464edf8ad219fdc636528b29076527659634970
Chris Gibb <chris.gibb@outlook.com>
2021-07-03 11:31:05 -0400

    reemit

--- runtime/dart/ui/sceneHost.ts
+++ runtime/dart/ui/sceneHost.ts
@@ -1,24 +1,72 @@
-
 declare const dart: {
-ui: {
-sceneHost: (this: void, sceneHost: ISceneHost, viewHolderToken: any, viewConnectedCallback? : () => void, viewDisconnectedCallback? : () => void, viewStateChangedCallback? : (: boolean) => void) => ISceneHost
-}
+    ui: {
+        sceneHost: (
+            this: void,
+            sceneHost: ISceneHost,
+            viewHolderToken: any,
+            viewConnectedCallback?: () => void,
+            viewDisconnectedCallback?: () => void,
+            viewStateChangedCallback?: (__: boolean) => void
+        ) => ISceneHost;
+    };
 };
-export interface ISceneHost
-
-{
-dispose: () => void;
-setProperties: (width: number, height: number, insetTop: number, insetRight: number, insetBottom: number, insetLeft: number, focusable: boolean) => void;
-}export class SceneHost
-
-{public constructor(viewHolderToken: any, viewConnectedCallback? : () => void, viewDisconnectedCallback? : () => void, viewStateChangedCallback? : (: boolean) => void){
-dart.ui.sceneHost(this, viewHolderToken, viewConnectedCallback, viewDisconnectedCallback, viewStateChangedCallback);}
+export interface ISceneHost {
+    dispose: () => void;
+    setProperties: (
+        width: number,
+        height: number,
+        insetTop: number,
+        insetRight: number,
+        insetBottom: number,
+        insetLeft: number,
+        focusable: boolean
+    ) => void;
+}
+export class SceneHost {
+    public constructor(
+        viewHolderToken: any,
+        viewConnectedCallback?: () => void,
+        viewDisconnectedCallback?: () => void,
+        viewStateChangedCallback?: (__: boolean) => void
+    ) {
+        dart.ui.sceneHost(
+            this,
+            viewHolderToken,
+            viewConnectedCallback,
+            viewDisconnectedCallback,
+            viewStateChangedCallback
+        );
+    }
     private readonly _dart_dispose: () => void = undefined as any;
-    private readonly _dart_setProperties: (width: number, height: number, insetTop: number, insetRight: number, insetBottom: number, insetLeft: number, focusable: boolean) => void = undefined as any;
-public dispose() : void {
-    return this._dart_dispose();
-}
-public setProperties(width: number, height: number, insetTop: number, insetRight: number, insetBottom: number, insetLeft: number, focusable: boolean) : void {
-    return this._dart_setProperties(width, height, insetTop, insetRight, insetBottom, insetLeft, focusable);
-}
+    private readonly _dart_setProperties: (
+        width: number,
+        height: number,
+        insetTop: number,
+        insetRight: number,
+        insetBottom: number,
+        insetLeft: number,
+        focusable: boolean
+    ) => void = undefined as any;
+    public dispose(): void {
+        return this._dart_dispose();
+    }
+    public setProperties(
+        width: number,
+        height: number,
+        insetTop: number,
+        insetRight: number,
+        insetBottom: number,
+        insetLeft: number,
+        focusable: boolean
+    ): void {
+        return this._dart_setProperties(
+            width,
+            height,
+            insetTop,
+            insetRight,
+            insetBottom,
+            insetLeft,
+            focusable
+        );
+    }
 }
