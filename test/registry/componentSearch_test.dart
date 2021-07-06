24bdbdbd08e6c50210e4e31ad4e2556044f52870
Chris Gibb <chris.gibb@outlook.com>
2021-07-06 10:12:52 -0400

    refactor component search test

--- test/registry/componentSearch_test.dart
+++ test/registry/componentSearch_test.dart
@@ -7,7 +7,6 @@ import 'package:uuid/uuid.dart';
 import 'package:hydro_sdk/registry/dto/createComponentDto.dart';
 import 'package:hydro_sdk/registry/dto/createProjectDto.dart';
 import 'package:hydro_sdk/registry/dto/createMockUserDto.dart';
-import 'package:hydro_sdk/registry/dto/loginUserDto.dart';
 import 'package:hydro_sdk/registry/dto/sessionDto.dart';
 import 'package:hydro_sdk/registry/registryApi.dart';
 import 'registryTestUrl.dart';
@@ -23,7 +22,6 @@ void main() {
       );
 
       final username = "test${Uuid().v4()}";
-      final password = Uuid().v4();
 
       final projectName = "test-project-${Uuid().v4()}";
       final projectDescription = "test project descrption ${Uuid().v4()}";
@@ -31,14 +29,15 @@ void main() {
       final componentName = "test-component-${Uuid().v4()}";
       final componentDescription = "test component descrption ${Uuid().v4()}";
 
-      final response = await api.createUser(
-          dto: CreateUserDto(
-        username: username,
-        password: password,
+      final response = await api.createMockUser(
+          dto: CreateMockUserDto(
+        displayName: username,
+        email: "${api.hash(Uuid().v4())}@example.com",
+        password: Uuid().v4(),
       ));
 
       expect(response, isNotNull);
-      expect(response, true);
+      expect(response, isNotEmpty);
 
       var createProjectResponse = await api.createProject(
         dto: CreateProjectDto(
@@ -50,22 +49,14 @@ void main() {
 
       expect(createProjectResponse, isNull);
 
-      final loginResponse = await api.login(
-        dto: LoginUserDto(
-          username: username,
-          password: password,
-        ),
-      );
-
-      expect(loginResponse, isNotNull);
-      expect(loginResponse?.authenticatedUser.username, username);
-
       createProjectResponse = await api.createProject(
         dto: CreateProjectDto(
           name: projectName,
           description: projectDescription,
         ),
-        sessionDto: loginResponse!,
+        sessionDto: SessionDto(
+          authToken: response!,
+        ),
       );
 
       expect(createProjectResponse, isNotNull);
@@ -79,7 +70,9 @@ void main() {
       expect(canUpdateProjectResponse, isNull);
 
       canUpdateProjectResponse = await api.canUpdateProjects(
-        sessionDto: loginResponse,
+        sessionDto: SessionDto(
+          authToken: response,
+        ),
       );
 
       var createdProject = canUpdateProjectResponse!
@@ -105,7 +98,9 @@ void main() {
           description: componentDescription,
           projectId: createProjectResponse.id,
         ),
-        sessionDto: loginResponse,
+        sessionDto: SessionDto(
+          authToken: response,
+        ),
       );
 
       expect(createComponentResponse, isNotNull);
@@ -113,7 +108,9 @@ void main() {
       expect(createComponentResponse.description, componentDescription);
 
       var canUpdateComponentResponse = await api.canUpdateComponents(
-        sessionDto: loginResponse,
+        sessionDto: SessionDto(
+          authToken: response,
+        ),
       );
 
       expect(canUpdateComponentResponse, isNotNull);
