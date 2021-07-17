ae094830e2b6d785329f69202c77059441428cef
Chris Gibb <chris.gibb@outlook.com>
2021-07-17 12:06:12 -0400

    test for basic function declaration with type formal

---
+++ test/swid/swidi/basicFunctionDeclarationWithTypeFormals_test.dart
@@ -0,0 +1,88 @@
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:hydro_sdk/swid/frontend/swidi/ast/swidiFunctionDeclaration.dart';
+import 'package:hydro_sdk/swid/frontend/swidi/ast/swidiInterface.dart';
+import 'package:hydro_sdk/swid/frontend/swidi/ast/swidiLibraryScopePrefix.dart';
+import 'package:hydro_sdk/swid/frontend/swidi/ast/swidiNullabilitySuffix.dart';
+import 'package:hydro_sdk/swid/frontend/swidi/ast/swidiReferenceDeclarationPrefix.dart';
+import 'package:hydro_sdk/swid/frontend/swidi/ast/swidiTypeFormal.dart';
+import 'package:hydro_sdk/swid/frontend/swidi/grammar/swidiGrammarDefinition.dart';
+import 'package:hydro_sdk/swid/frontend/swidi/grammar/swidiTypeListParser.dart';
+import 'package:hydro_sdk/swid/frontend/swidi/parser/swidiConstNumberParser.dart';
+import 'package:hydro_sdk/swid/frontend/swidi/parser/swidiConstParser.dart';
+import 'package:hydro_sdk/swid/frontend/swidi/parser/swidiConstStringParser.dart';
+import 'package:hydro_sdk/swid/frontend/swidi/parser/swidiFunctionDeclarationNamedParameterParser.dart';
+import 'package:hydro_sdk/swid/frontend/swidi/parser/swidiFunctionDeclarationOptionalParameterParser.dart';
+import 'package:hydro_sdk/swid/frontend/swidi/parser/swidiFunctionDeclarationParameterListParser.dart';
+import 'package:hydro_sdk/swid/frontend/swidi/parser/swidiFunctionDeclarationParser.dart';
+import 'package:hydro_sdk/swid/frontend/swidi/parser/swidiFunctionDeclarationPositionalParameterParser.dart';
+import 'package:hydro_sdk/swid/frontend/swidi/parser/swidiLibraryScopePrefixParser.dart';
+import 'package:hydro_sdk/swid/frontend/swidi/parser/swidiReferenceDeclarationPrefixParser.dart';
+import 'package:hydro_sdk/swid/frontend/swidi/parser/swidiSimpleDeclarationParser.dart';
+import 'package:hydro_sdk/swid/frontend/swidi/parser/swidiTypeArgumentListParser.dart';
+import 'package:hydro_sdk/swid/frontend/swidi/parser/swidiTypeFormalListDeclarationParser.dart';
+import 'package:hydro_sdk/swid/frontend/swidi/parser/swidiTypeFormalListParser.dart';
+import 'package:hydro_sdk/swid/frontend/swidi/parser/swidiTypeFormalParser.dart';
+import 'package:hydro_sdk/swid/frontend/swidi/parser/swidiTypeParser.dart';
+import 'lib/parserTestHarness.dart';
+
+class BasicFunctionDeclarationParser extends SwidiGrammarDefinition
+    with
+        SwidiLibraryScopePrefixParser,
+        SwidiReferenceDeclarationPrefixParser,
+        SwidiTypeListParser,
+        SwidiTypeArgumentListParser,
+        SwidiTypeParser,
+        SwidiConstNumberParser,
+        SwidiConstStringParser,
+        SwidiConstParser,
+        SwidiTypeFormalParser,
+        SwidiTypeFormalListParser,
+        SwidiTypeFormalListDeclarationParser,
+        SwidiSimpleDeclarationParser,
+        SwidiFunctionDeclarationOptionalParameterParser,
+        SwidiFunctionDeclarationPositionalParameterParser,
+        SwidiFunctionDeclarationNamedParameterParser,
+        SwidiFunctionDeclarationParameterListParser,
+        SwidiFunctionDeclarationParser {
+  const BasicFunctionDeclarationParser();
+}
+
+void main() {
+  LiveTestWidgetsFlutterBinding();
+  testWidgets('', (WidgetTester tester) async {
+    parserTestHarness(
+        input: const ParserTestHarnessInput.fromString(input: 'void foo<T extends "dart:core"::class::Object?>();'),
+        parser: const BasicFunctionDeclarationParser().build(
+            start: const BasicFunctionDeclarationParser().functionDeclaration),
+        result: const SwidiFunctionDeclaration(
+          typeFormals: [
+              SwidiTypeFormal(
+                name: "T",
+                bound: SwidiInterface(
+                  name: "Object?",
+                  libraryScopePrefix: SwidiLibraryScopePrefix(
+                    name: "dart:core",
+                  ),
+                  referenceDeclarationPrefix: SwidiReferenceDeclarationPrefix(
+                    name: "class",
+                  ),
+                  nullabilitySuffix: SwidiNullabilitySuffix.question,
+                  typeArguments: [],
+                ),
+              )
+            ],
+          returnType: SwidiInterface(
+            typeArguments: [],
+            name: "void",
+            libraryScopePrefix: SwidiLibraryScopePrefix.empty,
+            referenceDeclarationPrefix: SwidiReferenceDeclarationPrefix.empty,
+            nullabilitySuffix: SwidiNullabilitySuffix.none,
+          ),
+          name: "foo",
+          optionalParameters: [],
+          positionalParameters: [],
+          namedParameters: [],
+        ));
+  }, tags: "swid");
+}
