c091e3c84a6f3654efc6f52a7cb4c12890238c85
Chris Gibb <chris.gibb@outlook.com>
2021-07-17 12:10:10 -0400

    add test for class with method with type formal and bound

--- test/swid/swidi/full_test.dart
+++ test/swid/swidi/full_test.dart
@@ -1,14 +1,18 @@
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:hydro_sdk/swid/frontend/swidi/ast/swidiClass.dart';
+import 'package:hydro_sdk/swid/frontend/swidi/ast/swidiConst.dart';
+import 'package:hydro_sdk/swid/frontend/swidi/ast/swidiConstString.dart';
 import 'package:hydro_sdk/swid/frontend/swidi/ast/swidiDeclaration.dart';
 import 'package:hydro_sdk/swid/frontend/swidi/ast/swidiFunctionDeclaration.dart';
 import 'package:hydro_sdk/swid/frontend/swidi/ast/swidiInterface.dart';
 import 'package:hydro_sdk/swid/frontend/swidi/ast/swidiLibraryScopePrefix.dart';
 import 'package:hydro_sdk/swid/frontend/swidi/ast/swidiNamedParameter.dart';
 import 'package:hydro_sdk/swid/frontend/swidi/ast/swidiNullabilitySuffix.dart';
+import 'package:hydro_sdk/swid/frontend/swidi/ast/swidiOptionalParameter.dart';
 import 'package:hydro_sdk/swid/frontend/swidi/ast/swidiPositionalParameter.dart';
 import 'package:hydro_sdk/swid/frontend/swidi/ast/swidiReferenceDeclarationPrefix.dart';
+import 'package:hydro_sdk/swid/frontend/swidi/ast/swidiTypeFormal.dart';
 import 'package:hydro_sdk/swid/frontend/swidi/parser/swidiParser.dart';
 import 'lib/parserTestHarness.dart';
 
@@ -39,63 +43,153 @@ void main() {
       parser: const SwidiParser().build(),
       result: [
         const SwidiClass(
-            name: "IconData",
-            libraryScopePrefix: SwidiLibraryScopePrefix(
-                name: "package:flutter/src/widgets/icon_data.dart"),
-            methods: [
-              SwidiFunctionDeclaration(
-                name: "foo",
-                returnType: SwidiInterface(
-                  typeArguments: [],
-                  name: "void",
-                  libraryScopePrefix: SwidiLibraryScopePrefix.empty,
-                  referenceDeclarationPrefix:
-                      SwidiReferenceDeclarationPrefix(name: "void"),
-                  nullabilitySuffix: SwidiNullabilitySuffix.none,
+          name: "IconData",
+          libraryScopePrefix: SwidiLibraryScopePrefix(
+              name: "package:flutter/src/widgets/icon_data.dart"),
+          methods: [
+            SwidiFunctionDeclaration(
+              typeFormals: [],
+              name: "foo",
+              returnType: SwidiInterface(
+                typeArguments: [],
+                name: "void",
+                libraryScopePrefix: SwidiLibraryScopePrefix.empty,
+                referenceDeclarationPrefix:
+                    SwidiReferenceDeclarationPrefix(name: "void"),
+                nullabilitySuffix: SwidiNullabilitySuffix.none,
+              ),
+              optionalParameters: [],
+              positionalParameters: [
+                SwidiPositionalParameter(
+                    declaration: SwidiDeclaration(
+                        name: "bar",
+                        type: SwidiInterface(
+                          typeArguments: [],
+                          name: "int",
+                          libraryScopePrefix:
+                              SwidiLibraryScopePrefix(name: "dart:core"),
+                          referenceDeclarationPrefix:
+                              SwidiReferenceDeclarationPrefix(name: "class"),
+                          nullabilitySuffix: SwidiNullabilitySuffix.none,
+                        ))),
+              ],
+              namedParameters: [
+                SwidiNamedParameter(
+                    declaration: SwidiDeclaration(
+                        name: "baz",
+                        type: SwidiInterface(
+                          typeArguments: [],
+                          name: "int",
+                          libraryScopePrefix:
+                              SwidiLibraryScopePrefix(name: "dart:core"),
+                          referenceDeclarationPrefix:
+                              SwidiReferenceDeclarationPrefix(name: "class"),
+                          nullabilitySuffix: SwidiNullabilitySuffix.none,
+                        ))),
+                SwidiNamedParameter(
+                  declaration: SwidiDeclaration(
+                    name: "qux",
+                    type: SwidiInterface(
+                      typeArguments: [],
+                      name: "int?",
+                      libraryScopePrefix:
+                          SwidiLibraryScopePrefix(name: "dart:core"),
+                      referenceDeclarationPrefix:
+                          SwidiReferenceDeclarationPrefix(name: "class"),
+                      nullabilitySuffix: SwidiNullabilitySuffix.question,
+                    ),
+                  ),
+                )
+              ],
+            ),
+          ],
+        ),
+      ],
+    );
+
+    parserTestHarness(
+      input: const ParserTestHarnessInput.fromString(
+        input: """
+class "package:flutter/src/widgets/icon_data.dart"::IconData {
+  type::T foo<T extends "dart:core"::class::Object?>(
+    [
+      type::T<dynamic::dynamic>? default = @"foo bar default",
+    ]
+  );
+}
+""",
+      ),
+      parser: const SwidiParser().build(),
+      result: const [
+        SwidiClass(
+          name: "IconData",
+          libraryScopePrefix: SwidiLibraryScopePrefix(
+            name: "package:flutter/src/widgets/icon_data.dart",
+          ),
+          methods: [
+            SwidiFunctionDeclaration(
+              name: "foo",
+              typeFormals: [
+                SwidiTypeFormal(
+                  name: "T",
+                  bound: SwidiInterface(
+                    name: "Object?",
+                    libraryScopePrefix: SwidiLibraryScopePrefix(
+                      name: "dart:core",
+                    ),
+                    referenceDeclarationPrefix: SwidiReferenceDeclarationPrefix(
+                      name: "class",
+                    ),
+                    nullabilitySuffix: SwidiNullabilitySuffix.question,
+                    typeArguments: [],
+                  ),
+                )
+              ],
+              returnType: SwidiInterface(
+                name: "T",
+                libraryScopePrefix: SwidiLibraryScopePrefix.empty,
+                referenceDeclarationPrefix: SwidiReferenceDeclarationPrefix(
+                  name: "type",
                 ),
-                optionalParameters: [],
-                positionalParameters: [
-                  SwidiPositionalParameter(
-                      declaration: SwidiDeclaration(
-                          name: "bar",
-                          type: SwidiInterface(
-                            typeArguments: [],
-                            name: "int",
-                            libraryScopePrefix:
-                                SwidiLibraryScopePrefix(name: "dart:core"),
-                            referenceDeclarationPrefix:
-                                SwidiReferenceDeclarationPrefix(name: "class"),
-                            nullabilitySuffix: SwidiNullabilitySuffix.none,
-                          ))),
-                ],
-                namedParameters: [
-                  SwidiNamedParameter(
-                      declaration: SwidiDeclaration(
-                          name: "baz",
-                          type: SwidiInterface(
-                            typeArguments: [],
-                            name: "int",
-                            libraryScopePrefix:
-                                SwidiLibraryScopePrefix(name: "dart:core"),
-                            referenceDeclarationPrefix:
-                                SwidiReferenceDeclarationPrefix(name: "class"),
-                            nullabilitySuffix: SwidiNullabilitySuffix.none,
-                          ))),
-                  SwidiNamedParameter(
-                      declaration: SwidiDeclaration(
-                          name: "qux",
-                          type: SwidiInterface(
-                            typeArguments: [],
-                            name: "int?",
-                            libraryScopePrefix:
-                                SwidiLibraryScopePrefix(name: "dart:core"),
-                            referenceDeclarationPrefix:
-                                SwidiReferenceDeclarationPrefix(name: "class"),
-                            nullabilitySuffix: SwidiNullabilitySuffix.question,
-                          )))
-                ],
-              )
-            ]),
+                nullabilitySuffix: SwidiNullabilitySuffix.none,
+                typeArguments: [],
+              ),
+              positionalParameters: [],
+              optionalParameters: [
+                SwidiOptionalParameter(
+                  declaration: SwidiDeclaration(
+                    name: "default",
+                    defaultConstValue: SwidiConst.fromSwidiConstString(
+                      swidiConstString: SwidiConstString(
+                        value: "foo bar default",
+                      ),
+                    ),
+                    type: SwidiInterface(
+                      name: "T?",
+                      libraryScopePrefix: SwidiLibraryScopePrefix.empty,
+                      referenceDeclarationPrefix:
+                          SwidiReferenceDeclarationPrefix(name: "type"),
+                      nullabilitySuffix: SwidiNullabilitySuffix.question,
+                      typeArguments: [
+                        SwidiInterface(
+                          name: "dynamic",
+                          libraryScopePrefix: SwidiLibraryScopePrefix.empty,
+                          referenceDeclarationPrefix:
+                              SwidiReferenceDeclarationPrefix(
+                            name: "dynamic",
+                          ),
+                          nullabilitySuffix: SwidiNullabilitySuffix.none,
+                          typeArguments: [],
+                        )
+                      ],
+                    ),
+                  ),
+                )
+              ],
+              namedParameters: [],
+            )
+          ],
+        ),
       ],
     );
   }, tags: "swid");
