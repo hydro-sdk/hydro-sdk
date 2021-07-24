4b47bb625134476719826fac806664aa02f02a7f
Chris Gibb <chris.gibb@outlook.com>
2021-07-24 12:08:42 -0400

    factor out interfaces for top level lexers

--- lib/swid/frontend/swidi/grammar/lexers/iTopLevelDefinitionLexer.dart
+++ lib/swid/frontend/swidi/grammar/lexers/iTopLevelDefinitionLexer.dart
@@ -1,1 +1,7 @@
+import 'package:hydro_sdk/swid/frontend/swidi/grammar/lexers/iClassDefinitionLexer.dart';
 
+abstract class ITopLevelDefinitionLexer<T> implements IClassDefinitionLexer<T> {
+  const ITopLevelDefinitionLexer();
+
+  T topLevelDefinition();
+}
--- lib/swid/frontend/swidi/grammar/swidiGrammarDefinition.dart
+++ lib/swid/frontend/swidi/grammar/swidiGrammarDefinition.dart
@@ -1,5 +1,9 @@
 import 'package:petitparser/petitparser.dart';
 
+import 'package:hydro_sdk/swid/frontend/swidi/grammar/lexers/iClassDefinitionLexer.dart';
+import 'package:hydro_sdk/swid/frontend/swidi/grammar/lexers/iCompilationUnitLexer.dart';
+import 'package:hydro_sdk/swid/frontend/swidi/grammar/lexers/iStartLexer.dart';
+import 'package:hydro_sdk/swid/frontend/swidi/grammar/lexers/iTopLevelDefinitionLexer.dart';
 import 'package:hydro_sdk/swid/frontend/swidi/grammar/swidiConstGrammarDefinition.dart';
 import 'package:hydro_sdk/swid/frontend/swidi/grammar/swidiDeclarationGrammarDefinition.dart';
 import 'package:hydro_sdk/swid/frontend/swidi/grammar/swidiFunctionDeclarationNamedParameterGrammarDefinition.dart';
@@ -22,15 +26,24 @@ class SwidiGrammarDefinition extends GrammarDefinition
         SwidiFunctionDeclarationPositionalParameterGrammarDefinition,
         SwidiFunctionDeclarationOptionalParameterGrammarDefinition,
         SwidiFunctionDeclarationNamedParameterGrammarDefinition,
-        SwidiFunctionGrammarDefinition {
+        SwidiFunctionGrammarDefinition
+    implements
+        IClassDefinitionLexer,
+        ITopLevelDefinitionLexer,
+        ICompilationUnitLexer,
+        IStartLexer {
   const SwidiGrammarDefinition();
 
+  @override
   Parser start() => ref0(compilationUnit).end();
 
+  @override
   Parser compilationUnit() => ref0(topLevelDefinition).star();
 
+  @override
   Parser topLevelDefinition() => ref0(classDefinition);
 
+  @override
   Parser classDefinition() =>
       ref0(lexicalAbstract).optional() &
       ref0(lexicalClass) &
