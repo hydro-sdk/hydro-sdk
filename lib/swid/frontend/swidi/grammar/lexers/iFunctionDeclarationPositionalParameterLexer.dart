af33066425b8e49cb7fe6d46162a287cb5faed96
Chris Gibb <chris.gibb@outlook.com>
2021-07-24 11:53:42 -0400

    factor out lexer interfaces for function declarations with positional parameters

--- lib/swid/frontend/swidi/grammar/lexers/iFunctionDeclarationPositionalParameterLexer.dart
+++ lib/swid/frontend/swidi/grammar/lexers/iFunctionDeclarationPositionalParameterLexer.dart
@@ -1,1 +1,8 @@
+import 'package:hydro_sdk/swid/frontend/swidi/grammar/lexers/iSimpleDeclarationLexer.dart';
 
+abstract class IFunctionDeclarationPositionalParameterLexer<T>
+    implements ISimpleDeclarationLexer<T> {
+  const IFunctionDeclarationPositionalParameterLexer();
+
+  T functionDeclarationPositionalParameter();
+}
--- lib/swid/frontend/swidi/grammar/lexers/iFunctionDeclarationPositionalParameterListForm3Lexer.dart
+++ lib/swid/frontend/swidi/grammar/lexers/iFunctionDeclarationPositionalParameterListForm3Lexer.dart
@@ -1,1 +1,11 @@
+import 'package:hydro_sdk/swid/frontend/swidi/grammar/lexers/iFunctionDeclarationPositionalParameterLexer.dart';
+import 'package:hydro_sdk/swid/frontend/swidi/grammar/lexers/iFunctionDeclarationPositionalParameterTailLexer.dart';
 
+abstract class IFunctionDeclarationPositionalParameterListForm3Lexer<T>
+    implements
+        IFunctionDeclarationPositionalParameterLexer<T>,
+        IFunctionDeclarationPositionalParameterTailLexer<T> {
+  const IFunctionDeclarationPositionalParameterListForm3Lexer();
+
+  T functionDeclarationPositionalParameterListForm3();
+}
