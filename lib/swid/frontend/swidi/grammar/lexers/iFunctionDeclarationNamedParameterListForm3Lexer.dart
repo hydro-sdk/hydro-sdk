abc06629bd99b9801053c9045cf48253e74092e0
Chris Gibb <chris.gibb@outlook.com>
2021-07-24 11:11:09 -0400

    factor out lexer interfaces for function declarations with named parameters

--- lib/swid/frontend/swidi/grammar/lexers/iFunctionDeclarationNamedParameterListForm3Lexer.dart
+++ lib/swid/frontend/swidi/grammar/lexers/iFunctionDeclarationNamedParameterListForm3Lexer.dart
@@ -1,1 +1,11 @@
+import 'package:hydro_sdk/swid/frontend/swidi/grammar/lexers/iFunctionDeclarationNamedParameterLexer.dart';
+import 'package:hydro_sdk/swid/frontend/swidi/grammar/lexers/iFunctionDeclarationNamedParameterTailLexer.dart';
 
+abstract class IFunctionDeclarationNamedParameterListForm3Lexer<T>
+    implements
+        IFunctionDeclarationNamedParameterLexer<T>,
+        IFunctionDeclarationNamedParameterTailLexer<T> {
+  const IFunctionDeclarationNamedParameterListForm3Lexer();
+
+  T functionDeclarationNamedParameterListForm3();
+}
--- lib/swid/frontend/swidi/grammar/swidiFunctionDeclarationNamedParameterGrammarDefinition.dart
+++ lib/swid/frontend/swidi/grammar/swidiFunctionDeclarationNamedParameterGrammarDefinition.dart
@@ -1,6 +1,11 @@
 import 'package:petitparser/definition.dart';
 import 'package:petitparser/petitparser.dart';
 
+import 'package:hydro_sdk/swid/frontend/swidi/grammar/lexers/iFunctionDeclarationNamedParameterLexer.dart';
+import 'package:hydro_sdk/swid/frontend/swidi/grammar/lexers/iFunctionDeclarationNamedParameterListForm1Lexer.dart';
+import 'package:hydro_sdk/swid/frontend/swidi/grammar/lexers/iFunctionDeclarationNamedParameterListForm2Lexer.dart';
+import 'package:hydro_sdk/swid/frontend/swidi/grammar/lexers/iFunctionDeclarationNamedParameterListForm3Lexer.dart';
+import 'package:hydro_sdk/swid/frontend/swidi/grammar/lexers/iFunctionDeclarationNamedParameterTailLexer.dart';
 import 'package:hydro_sdk/swid/frontend/swidi/grammar/swidiDeclarationGrammarDefinition.dart';
 import 'package:hydro_sdk/swid/frontend/swidi/grammar/swidiGrammarTokenizer.dart';
 
@@ -8,23 +13,33 @@ mixin SwidiFunctionDeclarationNamedParameterGrammarDefinition
     on
         GrammarDefinition,
         SwidiGrammarTokenizer,
-        SwidiDeclarationGrammarDefinition {
+        SwidiDeclarationGrammarDefinition
+    implements
+        IFunctionDeclarationNamedParameterLexer,
+        IFunctionDeclarationNamedParameterTailLexer,
+        IFunctionDeclarationNamedParameterListForm1Lexer,
+        IFunctionDeclarationNamedParameterListForm2Lexer,
+        IFunctionDeclarationNamedParameterListForm3Lexer {
+  @override
   Parser functionDeclarationNamedParameterListForm1() =>
       ref1(token, "{") &
       ref0(functionDeclarationNamedParameter) &
       ref1(token, ",") &
       ref1(token, "}");
 
+  @override
   Parser functionDeclarationNamedParameterListForm2() =>
       ref1(token, "{") &
       ref0(functionDeclarationNamedParameter) &
       ref0(functionDeclarationNamedParameterTail).optional() &
       ref1(token, "}");
 
+  @override
   Parser functionDeclarationNamedParameterTail() => (ref1(token, ",") &
       ref0(functionDeclarationNamedParameter) &
       ref0(functionDeclarationNamedParameterTail).optional());
 
+  @override
   Parser functionDeclarationNamedParameterListForm3() =>
       ref1(token, "{") &
       ref0(functionDeclarationNamedParameter) &
@@ -32,6 +47,7 @@ mixin SwidiFunctionDeclarationNamedParameterGrammarDefinition
       ref1(token, ",") &
       ref1(token, "}");
 
+  @override
   Parser functionDeclarationNamedParameter() =>
       ref0(declarationWithDefaultConstValue) | ref0(simpleDeclaration);
 }
