e9c8627a85f45b7eed108c512b4445030b0f4990
Chris Gibb <chris.gibb@outlook.com>
2021-07-25 09:43:02 -0400

    list explicit dependencies of class parser

--- lib/swid/frontend/swidi/parser/swidiClassParser.dart
+++ lib/swid/frontend/swidi/parser/swidiClassParser.dart
@@ -1,3 +1,7 @@
+import 'package:hydro_sdk/swid/frontend/swidi/grammar/lexers/iFunctionDeclarationLexer.dart';
+import 'package:hydro_sdk/swid/frontend/swidi/grammar/lexers/iLibraryScopePrefixLexer.dart';
+import 'package:hydro_sdk/swid/frontend/swidi/parser/parsers/iFunctionDeclarationParser.dart';
+import 'package:hydro_sdk/swid/frontend/swidi/parser/parsers/iLibraryScopePrefixParser.dart';
 import 'package:petitparser/petitparser.dart';
 
 import 'package:hydro_sdk/swid/frontend/swidi/ast/swidiClass.dart';
@@ -9,7 +13,13 @@ import 'package:hydro_sdk/swid/frontend/swidi/parser/parsers/iClassParser.dart';
 import 'package:hydro_sdk/swid/frontend/swidi/parser/util/collectTokens.dart';
 
 mixin SwidiClassParser on SwidiGrammarDefinition
-    implements IClassLexer, IClassParser<Parser<SwidiClass>> {
+    implements
+        IClassLexer,
+        ILibraryScopePrefixLexer,
+        IFunctionDeclarationLexer,
+        IClassParser<Parser<SwidiClass>>,
+        ILibraryScopePrefixParser<Parser<SwidiLibraryScopePrefix>>,
+        IFunctionDeclarationParser<Parser<SwidiFunctionDeclaration>> {
   @override
   Parser<SwidiClass> classDefinition() => super.classDefinition().map((x) {
         final name = collectTokens<Token>(x);
--- test/swid/swidi/emptyClass_test.dart
+++ test/swid/swidi/emptyClass_test.dart
@@ -4,9 +4,57 @@ import 'package:hydro_sdk/swid/frontend/swidi/ast/swidiClass.dart';
 import 'package:hydro_sdk/swid/frontend/swidi/ast/swidiLibraryScopePrefix.dart';
 import 'package:hydro_sdk/swid/frontend/swidi/grammar/swidiGrammarDefinition.dart';
 import 'package:hydro_sdk/swid/frontend/swidi/parser/swidiClassParser.dart';
+import 'package:hydro_sdk/swid/frontend/swidi/parser/swidiConstFunctionInvocationParser.dart';
+import 'package:hydro_sdk/swid/frontend/swidi/parser/swidiConstNamedParameterListParser.dart';
+import 'package:hydro_sdk/swid/frontend/swidi/parser/swidiConstNamedParameterParser.dart';
+import 'package:hydro_sdk/swid/frontend/swidi/parser/swidiConstNumberParser.dart';
+import 'package:hydro_sdk/swid/frontend/swidi/parser/swidiConstParameterListParser.dart';
+import 'package:hydro_sdk/swid/frontend/swidi/parser/swidiConstParser.dart';
+import 'package:hydro_sdk/swid/frontend/swidi/parser/swidiConstPositionalParameterListParser.dart';
+import 'package:hydro_sdk/swid/frontend/swidi/parser/swidiConstStringParser.dart';
+import 'package:hydro_sdk/swid/frontend/swidi/parser/swidiDeclarationWithConstValueParser.dart';
+import 'package:hydro_sdk/swid/frontend/swidi/parser/swidiFunctionDeclarationNamedParameterParser.dart';
+import 'package:hydro_sdk/swid/frontend/swidi/parser/swidiFunctionDeclarationOptionalParameterParser.dart';
+import 'package:hydro_sdk/swid/frontend/swidi/parser/swidiFunctionDeclarationParameterListParser.dart';
+import 'package:hydro_sdk/swid/frontend/swidi/parser/swidiFunctionDeclarationParser.dart';
+import 'package:hydro_sdk/swid/frontend/swidi/parser/swidiFunctionDeclarationPositionalParameterParser.dart';
+import 'package:hydro_sdk/swid/frontend/swidi/parser/swidiLibraryScopePrefixParser.dart';
+import 'package:hydro_sdk/swid/frontend/swidi/parser/swidiReferenceDeclarationPrefixParser.dart';
+import 'package:hydro_sdk/swid/frontend/swidi/parser/swidiSimpleDeclarationParser.dart';
+import 'package:hydro_sdk/swid/frontend/swidi/parser/swidiTypeArgumentListParser.dart';
+import 'package:hydro_sdk/swid/frontend/swidi/parser/swidiTypeFormalListDeclarationParser.dart';
+import 'package:hydro_sdk/swid/frontend/swidi/parser/swidiTypeFormalListParser.dart';
+import 'package:hydro_sdk/swid/frontend/swidi/parser/swidiTypeFormalParser.dart';
+import 'package:hydro_sdk/swid/frontend/swidi/parser/swidiTypeListParser.dart';
+import 'package:hydro_sdk/swid/frontend/swidi/parser/swidiTypeParser.dart';
 import 'lib/parserTestHarness.dart';
 
-class BasicClassParser extends SwidiGrammarDefinition with SwidiClassParser {
+class BasicClassParser extends SwidiGrammarDefinition
+    with
+        SwidiClassParser,
+        SwidiLibraryScopePrefixParser,
+        SwidiReferenceDeclarationPrefixParser,
+        SwidiTypeListParser,
+        SwidiTypeArgumentListParser,
+        SwidiTypeParser,
+        SwidiConstNumberParser,
+        SwidiConstStringParser,
+        SwidiConstParser,
+        SwidiConstFunctionInvocationParser,
+        SwidiConstParameterListParser,
+        SwidiConstPositionalParameterListParser,
+        SwidiConstNamedParameterParser,
+        SwidiConstNamedParameterListParser,
+        SwidiDeclarationWithConstValueParser,
+        SwidiSimpleDeclarationParser,
+        SwidiFunctionDeclarationOptionalParameterParser,
+        SwidiFunctionDeclarationPositionalParameterParser,
+        SwidiFunctionDeclarationNamedParameterParser,
+        SwidiFunctionDeclarationParameterListParser,
+        SwidiTypeFormalParser,
+        SwidiTypeFormalListParser,
+        SwidiTypeFormalListDeclarationParser,
+        SwidiFunctionDeclarationParser {
   const BasicClassParser();
 }
 
