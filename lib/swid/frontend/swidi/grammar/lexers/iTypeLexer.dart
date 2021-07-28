54cb5c02244df8618ff9de4f98a1c7e2676a85bd
Chris Gibb <chris.gibb@outlook.com>
2021-07-27 22:00:56 -0400

    test for annotated type

--- lib/swid/frontend/swidi/grammar/lexers/iTypeLexer.dart
+++ lib/swid/frontend/swidi/grammar/lexers/iTypeLexer.dart
@@ -1,9 +1,11 @@
+import 'package:hydro_sdk/swid/frontend/swidi/grammar/lexers/iAnnotationListLexer.dart';
 import 'package:hydro_sdk/swid/frontend/swidi/grammar/lexers/iLibraryScopePrefixLexer.dart';
 import 'package:hydro_sdk/swid/frontend/swidi/grammar/lexers/iQualifiedLexer.dart';
 import 'package:hydro_sdk/swid/frontend/swidi/grammar/lexers/iReferenceDeclarationPrefixLexer.dart';
 
 abstract class ITypeLexer<T>
     implements
+        IAnnotationListLexer<T>,
         ILibraryScopePrefixLexer<T>,
         IReferenceDeclarationPrefixLexer<T>,
         IQualifiedLexer<T> {
