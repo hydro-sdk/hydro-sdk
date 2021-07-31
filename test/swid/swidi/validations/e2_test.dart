3677f5fed745ad853f250eca8bca63e8611d5b6d
Chris Gibb <chris.gibb@outlook.com>
2021-07-31 12:16:47 -0400

    expand e2 validation test

--- test/swid/swidi/validations/e2_test.dart
+++ test/swid/swidi/validations/e2_test.dart
@@ -22,5 +22,64 @@ class "package:flutter/src/widgets/icon_data.dart"::IconData {
         swidiValidationError: SwidiValidationError.e2,
       ),
     );
+
+    validatorTestHarness(
+      input: """
+class "package:flutter/src/widgets/icon_data.dart"::IconData {
+    void::void foo(
+      [[ 2 ]]
+      "dart:core"::class::int bar, {
+        "dart:core"::class::int baz,
+        "dart:core"::class::int? qux,
+    });
+  }
+""",
+      expectation: const SwidiClassValidationState.invalid(
+        swidiValidationError: SwidiValidationError.e2,
+      ),
+    );
+
+    validatorTestHarness(
+      input: """
+class "package:flutter/src/widgets/icon_data.dart"::IconData {
+    void::void foo(
+      "dart:core"::class::int bar, {
+        [[ 2 ]]
+        "dart:core"::class::int baz,
+        [[ 3 ]]
+        "dart:core"::class::int? qux,
+    });
+  }
+""",
+      expectation: const SwidiClassValidationState.invalid(
+        swidiValidationError: SwidiValidationError.e2,
+      ),
+    );
+
+    validatorTestHarness(
+      input: """
+class "package:flutter/src/widgets/icon_data.dart"::IconData {
+    void::void foo(
+      [
+        [[ 1 ]]
+        "dart:core"::class::int bar
+        ]);
+  }
+""",
+      expectation: const SwidiClassValidationState.invalid(
+        swidiValidationError: SwidiValidationError.e2,
+      ),
+    );
+
+    validatorTestHarness(
+      input: """
+class "package:flutter/src/widgets/icon_data.dart"::IconData {
+    void::void foo<T extends [[ 2 ]] "dart:core"::class::Object?>();
+  }
+""",
+      expectation: const SwidiClassValidationState.invalid(
+        swidiValidationError: SwidiValidationError.e2,
+      ),
+    );
   }, tags: "swid");
 }
