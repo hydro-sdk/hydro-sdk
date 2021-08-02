7f1687640bbf5c5cb81d246428591f3b2d939aec
Chris Gibb <chris.gibb@outlook.com>
2021-08-02 16:10:35 -0400

    skip reference collection if explicitly specified

--- lib/swid/ir/util/collectAllReferences.dart
+++ lib/swid/ir/util/collectAllReferences.dart
@@ -1,3 +1,5 @@
+import 'package:collection/collection.dart' show IterableExtension;
+
 import 'package:hydro_sdk/swid/ir/constPrimitives.dart';
 import 'package:hydro_sdk/swid/ir/swidInterface.dart';
 import 'package:hydro_sdk/swid/ir/swidType.dart';
@@ -7,126 +9,139 @@ List<SwidInterface> collectAllReferences({
   required SwidType swidType,
 }) =>
     ([
-      ...swidType.when<List<SwidInterface>>(
-          fromSwidInterface: (val) => [
-                val,
-                ...((List<List<SwidInterface>> elements) => elements.isNotEmpty
-                    ? elements.reduce((value, element) => [
-                          ...value,
-                          ...element,
-                        ])
-                    : <SwidInterface>[])(val.typeArguments
-                    .map((x) => collectAllReferences(
-                          swidType: x,
-                        ))
-                    .toList())
-              ]..removeWhere((x) => x == dartUnknownInterface),
-          fromSwidClass: (val) => ([
-                ...(val.constructorType != null
-                    ? collectAllReferences(
-                        swidType: SwidType.fromSwidFunctionType(
-                        swidFunctionType: val.constructorType!,
-                      ))
-                    : <SwidInterface>[]),
-                ...(val.extendedClass != null
-                    ? [
-                        SwidInterface.fromSwidClass(
-                            swidClass: val.extendedClass!),
-                      ]
-                    : <SwidInterface>[]),
-                ...val.implementedClasses
-                    .map((x) => SwidInterface.fromSwidClass(
-                          swidClass: x,
-                        ))
-                    .toList(),
-                ...val.mixedInClasses
-                    .map((x) => SwidInterface.fromSwidClass(
-                          swidClass: x,
-                        ))
-                    .toList(),
-                ...((List<List<SwidInterface>> elements) => elements.isNotEmpty
-                    ? elements.reduce((value, element) => [
-                          ...value,
-                          ...element,
-                        ])
-                    : <SwidInterface>[])(val.factoryConstructors
-                    .map((x) => collectAllReferences(
+      ...(swidType.declarationModifiers.ignoredAnalyses
+                  .firstWhereOrNull((x) => x == "referenceCollection") ==
+              null
+          ? swidType.when<List<SwidInterface>>(
+              fromSwidInterface: (val) => [
+                    val,
+                    ...((List<List<SwidInterface>> elements) =>
+                        elements.isNotEmpty
+                            ? elements.reduce((value, element) => [
+                                  ...value,
+                                  ...element,
+                                ])
+                            : <SwidInterface>[])(val.typeArguments
+                        .map((x) => collectAllReferences(
+                              swidType: x,
+                            ))
+                        .toList())
+                  ]..removeWhere((x) => x == dartUnknownInterface),
+              fromSwidClass: (val) => ([
+                    ...(val.constructorType != null
+                        ? collectAllReferences(
                             swidType: SwidType.fromSwidFunctionType(
-                          swidFunctionType: x,
-                        )))
-                    .toList()),
-                ...((List<List<SwidInterface>> elements) => elements.isNotEmpty
-                    ? elements.reduce((value, element) => [
-                          ...value,
-                          ...element,
-                        ])
-                    : <SwidInterface>[])(val.staticMethods
-                    .map((x) => collectAllReferences(
-                            swidType: SwidType.fromSwidFunctionType(
-                          swidFunctionType: x,
-                        )))
-                    .toList()),
-                ...((List<List<SwidInterface>> elements) => elements.isNotEmpty
-                    ? elements.reduce((value, element) => [
-                          ...value,
-                          ...element,
-                        ])
-                    : <SwidInterface>[])(val.methods
-                    .map((x) => collectAllReferences(
-                            swidType: SwidType.fromSwidFunctionType(
-                          swidFunctionType: x,
-                        )))
-                    .toList()),
-                ...((List<List<SwidInterface>> elements) => elements.isNotEmpty
-                    ? elements.reduce((value, element) => [
-                          ...value,
-                          ...element,
-                        ])
-                    : <SwidInterface>[])(val.instanceFieldDeclarations.entries
-                    .map((x) => collectAllReferences(
-                          swidType: x.value,
-                        ))
-                    .toList()),
-              ])
-                ..removeWhere((x) => x == dartUnknownInterface),
-          fromSwidDefaultFormalParameter: (val) =>
-              collectReferencesFromStaticConst(
-                swidStaticConst: val.value,
-              )..removeWhere((x) => x == dartUnknownInterface),
-          fromSwidFunctionType: (val) => ([
-                ...((List<List<SwidInterface>> elements) => elements.isNotEmpty
-                    ? elements.reduce((value, element) => [
-                          ...value,
-                          ...element,
-                        ])
-                    : <SwidInterface>[])(val.namedParameterTypes.entries
-                    .map((x) => collectAllReferences(
-                          swidType: x.value,
-                        ))
-                    .toList()),
-                ...((List<List<SwidInterface>> elements) => elements.isNotEmpty
-                    ? elements.reduce((value, element) => [
-                          ...value,
-                          ...element,
-                        ])
-                    : <SwidInterface>[])(val.normalParameterTypes
-                    .map((x) => collectAllReferences(
-                          swidType: x,
-                        ))
-                    .toList()),
-                ...((List<List<SwidInterface>> elements) => elements.isNotEmpty
-                    ? elements.reduce((value, element) => [
-                          ...value,
-                          ...element,
-                        ])
-                    : <SwidInterface>[])(val.optionalParameterTypes
-                    .map((x) => collectAllReferences(
-                          swidType: x,
-                        ))
-                    .toList()),
-                ...collectAllReferences(swidType: val.returnType),
-              ])
-                ..removeWhere((x) => x == dartUnknownInterface))
+                            swidFunctionType: val.constructorType!,
+                          ))
+                        : <SwidInterface>[]),
+                    ...(val.extendedClass != null
+                        ? [
+                            SwidInterface.fromSwidClass(
+                                swidClass: val.extendedClass!),
+                          ]
+                        : <SwidInterface>[]),
+                    ...val.implementedClasses
+                        .map((x) => SwidInterface.fromSwidClass(
+                              swidClass: x,
+                            ))
+                        .toList(),
+                    ...val.mixedInClasses
+                        .map((x) => SwidInterface.fromSwidClass(
+                              swidClass: x,
+                            ))
+                        .toList(),
+                    ...((List<List<SwidInterface>> elements) =>
+                        elements.isNotEmpty
+                            ? elements.reduce((value, element) => [
+                                  ...value,
+                                  ...element,
+                                ])
+                            : <SwidInterface>[])(val.factoryConstructors
+                        .map((x) => collectAllReferences(
+                                swidType: SwidType.fromSwidFunctionType(
+                              swidFunctionType: x,
+                            )))
+                        .toList()),
+                    ...((List<List<SwidInterface>> elements) =>
+                        elements.isNotEmpty
+                            ? elements.reduce((value, element) => [
+                                  ...value,
+                                  ...element,
+                                ])
+                            : <SwidInterface>[])(val.staticMethods
+                        .map((x) => collectAllReferences(
+                                swidType: SwidType.fromSwidFunctionType(
+                              swidFunctionType: x,
+                            )))
+                        .toList()),
+                    ...((List<List<SwidInterface>> elements) =>
+                        elements.isNotEmpty
+                            ? elements.reduce((value, element) => [
+                                  ...value,
+                                  ...element,
+                                ])
+                            : <SwidInterface>[])(val.methods
+                        .map((x) => collectAllReferences(
+                                swidType: SwidType.fromSwidFunctionType(
+                              swidFunctionType: x,
+                            )))
+                        .toList()),
+                    ...((List<List<SwidInterface>> elements) =>
+                            elements.isNotEmpty
+                                ? elements.reduce((value, element) => [
+                                      ...value,
+                                      ...element,
+                                    ])
+                                : <SwidInterface>[])(
+                        val.instanceFieldDeclarations.entries
+                            .map((x) => collectAllReferences(
+                                  swidType: x.value,
+                                ))
+                            .toList()),
+                  ])
+                    ..removeWhere((x) => x == dartUnknownInterface),
+              fromSwidDefaultFormalParameter: (val) =>
+                  collectReferencesFromStaticConst(
+                    swidStaticConst: val.value,
+                  )..removeWhere((x) => x == dartUnknownInterface),
+              fromSwidFunctionType: (val) => ([
+                    ...((List<List<SwidInterface>> elements) =>
+                        elements.isNotEmpty
+                            ? elements.reduce((value, element) => [
+                                  ...value,
+                                  ...element,
+                                ])
+                            : <SwidInterface>[])(val.namedParameterTypes.entries
+                        .map((x) => collectAllReferences(
+                              swidType: x.value,
+                            ))
+                        .toList()),
+                    ...((List<List<SwidInterface>> elements) =>
+                        elements.isNotEmpty
+                            ? elements.reduce((value, element) => [
+                                  ...value,
+                                  ...element,
+                                ])
+                            : <SwidInterface>[])(val.normalParameterTypes
+                        .map((x) => collectAllReferences(
+                              swidType: x,
+                            ))
+                        .toList()),
+                    ...((List<List<SwidInterface>> elements) =>
+                        elements.isNotEmpty
+                            ? elements.reduce((value, element) => [
+                                  ...value,
+                                  ...element,
+                                ])
+                            : <SwidInterface>[])(val.optionalParameterTypes
+                        .map((x) => collectAllReferences(
+                              swidType: x,
+                            ))
+                        .toList()),
+                    ...collectAllReferences(swidType: val.returnType),
+                  ])
+                    ..removeWhere((x) => x == dartUnknownInterface))
+          : [])
     ]
         .fold<List<SwidInterface>>(
             <SwidInterface>[],
