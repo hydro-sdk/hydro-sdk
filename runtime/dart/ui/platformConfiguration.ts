1fd6961f651dcb58e264a21dcdd2bf06fd44f993
Chris Gibb <chris.gibb@outlook.com>
2021-08-03 08:37:36 -0400

    reemit

--- runtime/dart/ui/platformConfiguration.ts
+++ runtime/dart/ui/platformConfiguration.ts
@@ -1,48 +1,93 @@
-
-import { IAccessibilityFeatures,AccessibilityFeatures } from "./accessibilityFeatures";
-import { IList,List } from "../core/list";
-import { ILocale } from "./locale";
+import { IList, List } from "../core/list";
+import {
+    AccessibilityFeatures,
+    IAccessibilityFeatures,
+} from "./accessibilityFeatures";
 import { Brightness } from "./brightness";
+import { ILocale } from "./locale";
 declare const dart: {
-ui: {
-platformConfiguration: (this: void, platformConfiguration: IPlatformConfiguration, props : { accessibilityFeatures : IAccessibilityFeatures, alwaysUse24HourFormat : boolean, defaultRouteName? : string | undefined, locales : IList<ILocale>, platformBrightness : Brightness, semanticsEnabled : boolean, textScaleFactor : number, }) => IPlatformConfiguration
-}
+    ui: {
+        platformConfiguration: (
+            this: void,
+            platformConfiguration: IPlatformConfiguration,
+            props: {
+                accessibilityFeatures: IAccessibilityFeatures;
+                alwaysUse24HourFormat: boolean;
+                defaultRouteName?: string | undefined;
+                locales: IList<ILocale>;
+                platformBrightness: Brightness;
+                semanticsEnabled: boolean;
+                textScaleFactor: number;
+            }
+        ) => IPlatformConfiguration;
+    };
 };
-export interface IPlatformConfiguration
-
-{
-accessibilityFeatures: IAccessibilityFeatures;
-alwaysUse24HourFormat: boolean;
-semanticsEnabled: boolean;
-platformBrightness: Brightness;
-textScaleFactor: number;
-locales: IList<ILocale>;
-defaultRouteName: string | undefined;
-copyWith: ( props : { accessibilityFeatures? : IAccessibilityFeatures | undefined, alwaysUse24HourFormat? : boolean | undefined, defaultRouteName? : string | undefined, locales? : IList<ILocale> | undefined, platformBrightness? : Brightness | undefined, semanticsEnabled? : boolean | undefined, textScaleFactor? : number | undefined, }) => IPlatformConfiguration;
-}export class PlatformConfiguration
-
-{    public readonly accessibilityFeatures: IAccessibilityFeatures = undefined as any;
+export interface IPlatformConfiguration {
+    accessibilityFeatures: IAccessibilityFeatures;
+    alwaysUse24HourFormat: boolean;
+    semanticsEnabled: boolean;
+    platformBrightness: Brightness;
+    textScaleFactor: number;
+    locales: IList<ILocale>;
+    defaultRouteName: string | undefined;
+    copyWith: (props: {
+        accessibilityFeatures?: IAccessibilityFeatures | undefined;
+        alwaysUse24HourFormat?: boolean | undefined;
+        defaultRouteName?: string | undefined;
+        locales?: IList<ILocale> | undefined;
+        platformBrightness?: Brightness | undefined;
+        semanticsEnabled?: boolean | undefined;
+        textScaleFactor?: number | undefined;
+    }) => IPlatformConfiguration;
+}
+export class PlatformConfiguration {
+    public readonly accessibilityFeatures: IAccessibilityFeatures =
+        undefined as any;
     public readonly alwaysUse24HourFormat: boolean = undefined as any;
     public readonly semanticsEnabled: boolean = undefined as any;
     public readonly platformBrightness: Brightness = undefined as any;
     public readonly textScaleFactor: number = undefined as any;
     public readonly locales: IList<ILocale> = undefined as any;
     public readonly defaultRouteName: string | undefined = undefined as any;
-public constructor( props : { accessibilityFeatures? : IAccessibilityFeatures, alwaysUse24HourFormat? : boolean, defaultRouteName? : string | undefined, locales? : IList<ILocale>, platformBrightness? : Brightness, semanticsEnabled? : boolean, textScaleFactor? : number, }){
-dart.ui.platformConfiguration(this, {
-...platformConfigurationDefaultProps,
-...props
-});}
-    private readonly _dart_copyWith: ( props : { accessibilityFeatures? : IAccessibilityFeatures | undefined, alwaysUse24HourFormat? : boolean | undefined, defaultRouteName? : string | undefined, locales? : IList<ILocale> | undefined, platformBrightness? : Brightness | undefined, semanticsEnabled? : boolean | undefined, textScaleFactor? : number | undefined, }) => IPlatformConfiguration = undefined as any;
-public copyWith( props : { accessibilityFeatures? : IAccessibilityFeatures | undefined, alwaysUse24HourFormat? : boolean | undefined, defaultRouteName? : string | undefined, locales? : IList<ILocale> | undefined, platformBrightness? : Brightness | undefined, semanticsEnabled? : boolean | undefined, textScaleFactor? : number | undefined, }) : IPlatformConfiguration {
-    return this._dart_copyWith(props);
-}
+    public constructor(props: {
+        accessibilityFeatures: IAccessibilityFeatures;
+        alwaysUse24HourFormat?: boolean;
+        defaultRouteName?: string | undefined;
+        locales?: IList<ILocale>;
+        platformBrightness?: Brightness;
+        semanticsEnabled?: boolean;
+        textScaleFactor?: number;
+    }) {
+        dart.ui.platformConfiguration(this, {
+            ...platformConfigurationDefaultProps,
+            ...props,
+        });
+    }
+    private readonly _dart_copyWith: (props: {
+        accessibilityFeatures?: IAccessibilityFeatures | undefined;
+        alwaysUse24HourFormat?: boolean | undefined;
+        defaultRouteName?: string | undefined;
+        locales?: IList<ILocale> | undefined;
+        platformBrightness?: Brightness | undefined;
+        semanticsEnabled?: boolean | undefined;
+        textScaleFactor?: number | undefined;
+    }) => IPlatformConfiguration = undefined as any;
+    public copyWith(props: {
+        accessibilityFeatures?: IAccessibilityFeatures | undefined;
+        alwaysUse24HourFormat?: boolean | undefined;
+        defaultRouteName?: string | undefined;
+        locales?: IList<ILocale> | undefined;
+        platformBrightness?: Brightness | undefined;
+        semanticsEnabled?: boolean | undefined;
+        textScaleFactor?: number | undefined;
+    }): IPlatformConfiguration {
+        return this._dart_copyWith(props);
+    }
 }
 const platformConfigurationDefaultProps = {
-    accessibilityFeatures: (),
     alwaysUse24HourFormat: false,
     locales: List.fromArray([]),
     platformBrightness: Brightness.light,
     semanticsEnabled: false,
-    textScaleFactor: 1.0
+    textScaleFactor: 1.0,
 };
