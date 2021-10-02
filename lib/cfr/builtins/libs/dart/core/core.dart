6cf5284b0afb4ea93990ed23b348c9c1d489f8ec
ibrahim khaled <76872994+ibrahimkhiq@users.noreply.github.com>
2021-10-02 17:15:24 +0300

    Update core.dart
    add print function

--- lib/cfr/builtins/libs/dart/core/core.dart
+++ lib/cfr/builtins/libs/dart/core/core.dart
@@ -13,6 +13,8 @@ import 'package:hydro_sdk/cfr/builtins/libs/dart/core/sink.dart';
 import 'package:hydro_sdk/cfr/builtins/libs/dart/core/stackTrace.dart';
 import 'package:hydro_sdk/cfr/builtins/libs/dart/core/stringSink.dart';
 import 'package:hydro_sdk/cfr/builtins/libs/dart/core/symbol.dart';
+import 'package:hydro_sdk/cfr/builtins/libs/dart/core/print.dart';
+
 import 'package:hydro_sdk/cfr/runtimeSupport.dart';
 
 void loadcore({required HydroState hydroState, required HydroTable table}) {
@@ -33,4 +35,5 @@ void loadcore({required HydroState hydroState, required HydroTable table}) {
   loadError(table: core, hydroState: hydroState);
   loadStackTrace(table: core, hydroState: hydroState);
   loadDuration(hydroState: hydroState, table: core);
+  loadPrint(core);
 }
