38573299eb38c565e134456035355f7443a831f9
Chris Gibb <chris.gibb@outlook.com>
2021-10-06 08:22:05 -0400

    start ast for function types

---
+++ lib/swid/frontend/swidi/ast/swidiFunctionType.dart
@@ -0,0 +1,80 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:hydro_sdk/swid/frontend/swidi/ast/swidiAnnotation.dart';
+import 'package:hydro_sdk/swid/frontend/swidi/ast/swidiFunctionTypeNamedArgument.dart';
+import 'package:hydro_sdk/swid/frontend/swidi/ast/swidiInterface.dart';
+import 'package:hydro_sdk/swid/frontend/swidi/ast/swidiPositionalParameter.dart';
+import 'package:hydro_sdk/swid/util/hashComparableMixin.dart';
+import 'package:hydro_sdk/swid/util/hashKeyMixin.dart';
+import 'package:hydro_sdk/swid/util/iCopyable.dart';
+
+part 'swidiFunctionType.freezed.dart';
+
+@freezed
+class SwidiFunctionType
+    with
+        _$SwidiFunctionType,
+        HashKeyMixin<SwidiFunctionType>,
+        HashComparableMixin<SwidiFunctionType>
+    implements
+        ICopyable<SwidiFunctionType,
+            $SwidiFunctionTypeCopyWith<SwidiFunctionType>> {
+  SwidiFunctionType._();
+
+  factory SwidiFunctionType({
+    required final SwidiInterface returnType,
+    required final List<SwidiPositionalParameter> positionalParameters,
+    required final List<SwidiInterface> optionalParameters,
+    required final List<SwidiFunctionTypeNamedArgument> namedParameters,
+    required final List<SwidiInterface> typeArguments,
+    required final List<SwidiAnnotation> annotations,
+  }) = _$SwidiFunctionTypeCtor;
+
+  factory SwidiFunctionType._clone({
+    required final SwidiFunctionType swidiFunctionType,
+    final SwidiInterface? returnType,
+    final List<SwidiPositionalParameter>? positionalParameters,
+    final List<SwidiInterface>? optionalParameters,
+    final List<SwidiFunctionTypeNamedArgument>? namedParameters,
+    final List<SwidiInterface>? typeArguments,
+    final List<SwidiAnnotation>? annotations,
+  }) =>
+      SwidiFunctionType(
+        returnType: returnType ?? swidiFunctionType.returnType,
+        positionalParameters:
+            positionalParameters ?? swidiFunctionType.positionalParameters,
+        optionalParameters:
+            optionalParameters ?? swidiFunctionType.optionalParameters,
+        namedParameters: namedParameters ?? swidiFunctionType.namedParameters,
+        typeArguments: typeArguments ?? swidiFunctionType.typeArguments,
+        annotations: annotations ?? swidiFunctionType.annotations,
+      );
+
+  @override
+  List<int> get hashableParts => [
+        ...returnType.hashableParts,
+        ...positionalParameters.hashableParts,
+        ...optionalParameters.hashableParts,
+        ...namedParameters.hashableParts,
+        ...typeArguments.hashableParts,
+        ...annotations.hashableParts,
+      ];
+
+  @override
+  SwidiFunctionType clone({
+    final SwidiInterface? returnType,
+    final List<SwidiPositionalParameter>? positionalParameters,
+    final List<SwidiInterface>? optionalParameters,
+    final List<SwidiFunctionTypeNamedArgument>? namedParameters,
+    final List<SwidiInterface>? typeArguments,
+    final List<SwidiAnnotation>? annotations,
+  }) =>
+      SwidiFunctionType._clone(
+        swidiFunctionType: this,
+        returnType: returnType,
+        positionalParameters: positionalParameters,
+        optionalParameters: optionalParameters,
+        namedParameters: namedParameters,
+        typeArguments: typeArguments,
+        annotations: annotations,
+      );
+}
---
+++ lib/swid/frontend/swidi/ast/swidiFunctionType.freezed.dart
@@ -0,0 +1,295 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: unused_element, deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies, annotate_overrides
+
+part of 'swidiFunctionType.dart';
+
+// **************************************************************************
+// FreezedGenerator
+// **************************************************************************
+
+T _$identity<T>(T value) => value;
+
+final _privateConstructorUsedError = UnsupportedError(
+    'It seems like you constructed your class using `MyClass._()`. This constructor is only meant to be used by freezed and you are not supposed to need it nor use it.\nPlease check the documentation here for more informations: https://github.com/rrousselGit/freezed#custom-getters-and-methods');
+
+/// @nodoc
+class _$SwidiFunctionTypeTearOff {
+  const _$SwidiFunctionTypeTearOff();
+
+  _$SwidiFunctionTypeCtor call(
+      {required SwidiInterface returnType,
+      required List<SwidiPositionalParameter> positionalParameters,
+      required List<SwidiInterface> optionalParameters,
+      required List<SwidiFunctionTypeNamedArgument> namedParameters,
+      required List<SwidiInterface> typeArguments,
+      required List<SwidiAnnotation> annotations}) {
+    return _$SwidiFunctionTypeCtor(
+      returnType: returnType,
+      positionalParameters: positionalParameters,
+      optionalParameters: optionalParameters,
+      namedParameters: namedParameters,
+      typeArguments: typeArguments,
+      annotations: annotations,
+    );
+  }
+}
+
+/// @nodoc
+const $SwidiFunctionType = _$SwidiFunctionTypeTearOff();
+
+/// @nodoc
+mixin _$SwidiFunctionType {
+  SwidiInterface get returnType => throw _privateConstructorUsedError;
+  List<SwidiPositionalParameter> get positionalParameters =>
+      throw _privateConstructorUsedError;
+  List<SwidiInterface> get optionalParameters =>
+      throw _privateConstructorUsedError;
+  List<SwidiFunctionTypeNamedArgument> get namedParameters =>
+      throw _privateConstructorUsedError;
+  List<SwidiInterface> get typeArguments => throw _privateConstructorUsedError;
+  List<SwidiAnnotation> get annotations => throw _privateConstructorUsedError;
+
+  @JsonKey(ignore: true)
+  $SwidiFunctionTypeCopyWith<SwidiFunctionType> get copyWith =>
+      throw _privateConstructorUsedError;
+}
+
+/// @nodoc
+abstract class $SwidiFunctionTypeCopyWith<$Res> {
+  factory $SwidiFunctionTypeCopyWith(
+          SwidiFunctionType value, $Res Function(SwidiFunctionType) then) =
+      _$SwidiFunctionTypeCopyWithImpl<$Res>;
+  $Res call(
+      {SwidiInterface returnType,
+      List<SwidiPositionalParameter> positionalParameters,
+      List<SwidiInterface> optionalParameters,
+      List<SwidiFunctionTypeNamedArgument> namedParameters,
+      List<SwidiInterface> typeArguments,
+      List<SwidiAnnotation> annotations});
+
+  $SwidiInterfaceCopyWith<$Res> get returnType;
+}
+
+/// @nodoc
+class _$SwidiFunctionTypeCopyWithImpl<$Res>
+    implements $SwidiFunctionTypeCopyWith<$Res> {
+  _$SwidiFunctionTypeCopyWithImpl(this._value, this._then);
+
+  final SwidiFunctionType _value;
+  // ignore: unused_field
+  final $Res Function(SwidiFunctionType) _then;
+
+  @override
+  $Res call({
+    Object? returnType = freezed,
+    Object? positionalParameters = freezed,
+    Object? optionalParameters = freezed,
+    Object? namedParameters = freezed,
+    Object? typeArguments = freezed,
+    Object? annotations = freezed,
+  }) {
+    return _then(_value.copyWith(
+      returnType: returnType == freezed
+          ? _value.returnType
+          : returnType // ignore: cast_nullable_to_non_nullable
+              as SwidiInterface,
+      positionalParameters: positionalParameters == freezed
+          ? _value.positionalParameters
+          : positionalParameters // ignore: cast_nullable_to_non_nullable
+              as List<SwidiPositionalParameter>,
+      optionalParameters: optionalParameters == freezed
+          ? _value.optionalParameters
+          : optionalParameters // ignore: cast_nullable_to_non_nullable
+              as List<SwidiInterface>,
+      namedParameters: namedParameters == freezed
+          ? _value.namedParameters
+          : namedParameters // ignore: cast_nullable_to_non_nullable
+              as List<SwidiFunctionTypeNamedArgument>,
+      typeArguments: typeArguments == freezed
+          ? _value.typeArguments
+          : typeArguments // ignore: cast_nullable_to_non_nullable
+              as List<SwidiInterface>,
+      annotations: annotations == freezed
+          ? _value.annotations
+          : annotations // ignore: cast_nullable_to_non_nullable
+              as List<SwidiAnnotation>,
+    ));
+  }
+
+  @override
+  $SwidiInterfaceCopyWith<$Res> get returnType {
+    return $SwidiInterfaceCopyWith<$Res>(_value.returnType, (value) {
+      return _then(_value.copyWith(returnType: value));
+    });
+  }
+}
+
+/// @nodoc
+abstract class _$$SwidiFunctionTypeCtorCopyWith<$Res>
+    implements $SwidiFunctionTypeCopyWith<$Res> {
+  factory _$$SwidiFunctionTypeCtorCopyWith(_$SwidiFunctionTypeCtor value,
+          $Res Function(_$SwidiFunctionTypeCtor) then) =
+      __$$SwidiFunctionTypeCtorCopyWithImpl<$Res>;
+  @override
+  $Res call(
+      {SwidiInterface returnType,
+      List<SwidiPositionalParameter> positionalParameters,
+      List<SwidiInterface> optionalParameters,
+      List<SwidiFunctionTypeNamedArgument> namedParameters,
+      List<SwidiInterface> typeArguments,
+      List<SwidiAnnotation> annotations});
+
+  @override
+  $SwidiInterfaceCopyWith<$Res> get returnType;
+}
+
+/// @nodoc
+class __$$SwidiFunctionTypeCtorCopyWithImpl<$Res>
+    extends _$SwidiFunctionTypeCopyWithImpl<$Res>
+    implements _$$SwidiFunctionTypeCtorCopyWith<$Res> {
+  __$$SwidiFunctionTypeCtorCopyWithImpl(_$SwidiFunctionTypeCtor _value,
+      $Res Function(_$SwidiFunctionTypeCtor) _then)
+      : super(_value, (v) => _then(v as _$SwidiFunctionTypeCtor));
+
+  @override
+  _$SwidiFunctionTypeCtor get _value => super._value as _$SwidiFunctionTypeCtor;
+
+  @override
+  $Res call({
+    Object? returnType = freezed,
+    Object? positionalParameters = freezed,
+    Object? optionalParameters = freezed,
+    Object? namedParameters = freezed,
+    Object? typeArguments = freezed,
+    Object? annotations = freezed,
+  }) {
+    return _then(_$SwidiFunctionTypeCtor(
+      returnType: returnType == freezed
+          ? _value.returnType
+          : returnType // ignore: cast_nullable_to_non_nullable
+              as SwidiInterface,
+      positionalParameters: positionalParameters == freezed
+          ? _value.positionalParameters
+          : positionalParameters // ignore: cast_nullable_to_non_nullable
+              as List<SwidiPositionalParameter>,
+      optionalParameters: optionalParameters == freezed
+          ? _value.optionalParameters
+          : optionalParameters // ignore: cast_nullable_to_non_nullable
+              as List<SwidiInterface>,
+      namedParameters: namedParameters == freezed
+          ? _value.namedParameters
+          : namedParameters // ignore: cast_nullable_to_non_nullable
+              as List<SwidiFunctionTypeNamedArgument>,
+      typeArguments: typeArguments == freezed
+          ? _value.typeArguments
+          : typeArguments // ignore: cast_nullable_to_non_nullable
+              as List<SwidiInterface>,
+      annotations: annotations == freezed
+          ? _value.annotations
+          : annotations // ignore: cast_nullable_to_non_nullable
+              as List<SwidiAnnotation>,
+    ));
+  }
+}
+
+/// @nodoc
+
+class _$_$SwidiFunctionTypeCtor extends _$SwidiFunctionTypeCtor {
+  _$_$SwidiFunctionTypeCtor(
+      {required this.returnType,
+      required this.positionalParameters,
+      required this.optionalParameters,
+      required this.namedParameters,
+      required this.typeArguments,
+      required this.annotations})
+      : super._();
+
+  @override
+  final SwidiInterface returnType;
+  @override
+  final List<SwidiPositionalParameter> positionalParameters;
+  @override
+  final List<SwidiInterface> optionalParameters;
+  @override
+  final List<SwidiFunctionTypeNamedArgument> namedParameters;
+  @override
+  final List<SwidiInterface> typeArguments;
+  @override
+  final List<SwidiAnnotation> annotations;
+
+  @override
+  String toString() {
+    return 'SwidiFunctionType(returnType: $returnType, positionalParameters: $positionalParameters, optionalParameters: $optionalParameters, namedParameters: $namedParameters, typeArguments: $typeArguments, annotations: $annotations)';
+  }
+
+  @override
+  bool operator ==(dynamic other) {
+    return identical(this, other) ||
+        (other is _$SwidiFunctionTypeCtor &&
+            (identical(other.returnType, returnType) ||
+                const DeepCollectionEquality()
+                    .equals(other.returnType, returnType)) &&
+            (identical(other.positionalParameters, positionalParameters) ||
+                const DeepCollectionEquality().equals(
+                    other.positionalParameters, positionalParameters)) &&
+            (identical(other.optionalParameters, optionalParameters) ||
+                const DeepCollectionEquality()
+                    .equals(other.optionalParameters, optionalParameters)) &&
+            (identical(other.namedParameters, namedParameters) ||
+                const DeepCollectionEquality()
+                    .equals(other.namedParameters, namedParameters)) &&
+            (identical(other.typeArguments, typeArguments) ||
+                const DeepCollectionEquality()
+                    .equals(other.typeArguments, typeArguments)) &&
+            (identical(other.annotations, annotations) ||
+                const DeepCollectionEquality()
+                    .equals(other.annotations, annotations)));
+  }
+
+  @override
+  int get hashCode =>
+      runtimeType.hashCode ^
+      const DeepCollectionEquality().hash(returnType) ^
+      const DeepCollectionEquality().hash(positionalParameters) ^
+      const DeepCollectionEquality().hash(optionalParameters) ^
+      const DeepCollectionEquality().hash(namedParameters) ^
+      const DeepCollectionEquality().hash(typeArguments) ^
+      const DeepCollectionEquality().hash(annotations);
+
+  @JsonKey(ignore: true)
+  @override
+  _$$SwidiFunctionTypeCtorCopyWith<_$SwidiFunctionTypeCtor> get copyWith =>
+      __$$SwidiFunctionTypeCtorCopyWithImpl<_$SwidiFunctionTypeCtor>(
+          this, _$identity);
+}
+
+abstract class _$SwidiFunctionTypeCtor extends SwidiFunctionType {
+  factory _$SwidiFunctionTypeCtor(
+      {required SwidiInterface returnType,
+      required List<SwidiPositionalParameter> positionalParameters,
+      required List<SwidiInterface> optionalParameters,
+      required List<SwidiFunctionTypeNamedArgument> namedParameters,
+      required List<SwidiInterface> typeArguments,
+      required List<SwidiAnnotation> annotations}) = _$_$SwidiFunctionTypeCtor;
+  _$SwidiFunctionTypeCtor._() : super._();
+
+  @override
+  SwidiInterface get returnType => throw _privateConstructorUsedError;
+  @override
+  List<SwidiPositionalParameter> get positionalParameters =>
+      throw _privateConstructorUsedError;
+  @override
+  List<SwidiInterface> get optionalParameters =>
+      throw _privateConstructorUsedError;
+  @override
+  List<SwidiFunctionTypeNamedArgument> get namedParameters =>
+      throw _privateConstructorUsedError;
+  @override
+  List<SwidiInterface> get typeArguments => throw _privateConstructorUsedError;
+  @override
+  List<SwidiAnnotation> get annotations => throw _privateConstructorUsedError;
+  @override
+  @JsonKey(ignore: true)
+  _$$SwidiFunctionTypeCtorCopyWith<_$SwidiFunctionTypeCtor> get copyWith =>
+      throw _privateConstructorUsedError;
+}
