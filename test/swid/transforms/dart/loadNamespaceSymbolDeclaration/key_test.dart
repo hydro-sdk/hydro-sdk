eddbc6cafd348340d76a10a7f288b4b5da85f6f2
Chris Gibb <chris.gibb@outlook.com>
2021-11-10 19:53:55 -0500

    replace json test fixture

--- test/swid/transforms/dart/loadNamespaceSymbolDeclaration/key_test.dart
+++ test/swid/transforms/dart/loadNamespaceSymbolDeclaration/key_test.dart
@@ -1,18 +1,99 @@
-import 'dart:convert';
-import 'dart:io';
-
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:hydro_sdk/swid/backend/dart/dartLoadNamespaceSymbolDeclaration.dart';
 import 'package:hydro_sdk/swid/ir/swidClass.dart';
+import 'package:hydro_sdk/swid/ir/swidDeclarationModifiers.dart';
+import 'package:hydro_sdk/swid/ir/swidFunctionType.dart';
+import 'package:hydro_sdk/swid/ir/swidInterface.dart';
+import 'package:hydro_sdk/swid/ir/swidNullabilitySuffix.dart';
+import 'package:hydro_sdk/swid/ir/swidReferenceDeclarationKind.dart';
+import 'package:hydro_sdk/swid/ir/swidType.dart';
 import 'package:hydro_sdk/swid/swars/cachingPipeline.dart';
 import 'package:hydro_sdk/swid/swars/pipelineNoopCacheMgr.dart';
 
 void main() {
   LiveTestWidgetsFlutterBinding();
   testWidgets('', (WidgetTester tester) async {
-    var keyClass = SwidClass.fromJson(
-        json.decode(File("test/swid/res/Key.json").readAsStringSync()));
+    final keyClass = SwidClass(
+      name: "Key",
+      nullabilitySuffix: SwidNullabilitySuffix.none,
+      originalPackagePath: "package:flutter/src/foundation/key.dart",
+      constructorType: SwidFunctionType(
+        name: "",
+        nullabilitySuffix: SwidNullabilitySuffix.none,
+        originalPackagePath: "",
+        namedParameterTypes: {},
+        namedDefaults: {},
+        normalParameterNames: [
+          "value",
+        ],
+        normalParameterTypes: [
+          SwidType.fromSwidInterface(
+            swidInterface: SwidInterface(
+              name: "String",
+              nullabilitySuffix: SwidNullabilitySuffix.none,
+              originalPackagePath: "dart:core",
+              typeArguments: [],
+              referenceDeclarationKind:
+                  SwidReferenceDeclarationKind.classElement,
+              declarationModifiers: SwidDeclarationModifiers.empty(),
+            ),
+          )
+        ],
+        optionalParameterNames: [],
+        optionalParameterTypes: [],
+        returnType: SwidType.fromSwidInterface(
+          swidInterface: SwidInterface(
+            name: "Key",
+            nullabilitySuffix: SwidNullabilitySuffix.none,
+            originalPackagePath: "package:flutter/src/foundation/key.dart",
+            typeArguments: [],
+            referenceDeclarationKind: SwidReferenceDeclarationKind.classElement,
+            declarationModifiers: SwidDeclarationModifiers.empty(),
+          ),
+        ),
+        isFactory: true,
+        typeFormals: [],
+        declarationModifiers: SwidDeclarationModifiers.empty(),
+      ),
+      generativeConstructors: [],
+      factoryConstructors: [
+        SwidFunctionType(
+          name: "empty",
+          nullabilitySuffix: SwidNullabilitySuffix.none,
+          originalPackagePath: "",
+          namedParameterTypes: {},
+          namedDefaults: {},
+          normalParameterNames: [],
+          normalParameterTypes: [],
+          optionalParameterNames: [],
+          optionalParameterTypes: [],
+          returnType: SwidType.fromSwidInterface(
+            swidInterface: SwidInterface(
+              name: "Key",
+              nullabilitySuffix: SwidNullabilitySuffix.none,
+              originalPackagePath: "package:flutter/src/foundation/key.dart",
+              typeArguments: [],
+              referenceDeclarationKind:
+                  SwidReferenceDeclarationKind.classElement,
+              declarationModifiers: SwidDeclarationModifiers.empty(),
+            ),
+          ),
+          isFactory: true,
+          typeFormals: [],
+          declarationModifiers: SwidDeclarationModifiers.empty(),
+        )
+      ],
+      staticMethods: [],
+      methods: [],
+      staticConstFieldDeclarations: [],
+      instanceFieldDeclarations: {},
+      declarationModifiers: SwidDeclarationModifiers.empty(),
+      mixedInClasses: [],
+      implementedClasses: [],
+      isMixin: false,
+      typeFormals: [],
+    );
 
     expect(
         CachingPipeline(
