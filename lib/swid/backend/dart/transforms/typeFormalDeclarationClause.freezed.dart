75a4f020555b70f43c939073b20c530ef2123cc0
Chris Gibb <chris.gibb@outlook.com>
2021-11-12 17:41:11 -0500

    start boilerplate new transform

---
+++ lib/swid/backend/dart/transforms/typeFormalDeclarationClause.freezed.dart
@@ -0,0 +1,168 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: unused_element, deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies, annotate_overrides
+
+part of 'typeFormalDeclarationClause.dart';
+
+// **************************************************************************
+// FreezedGenerator
+// **************************************************************************
+
+T _$identity<T>(T value) => value;
+
+final _privateConstructorUsedError = UnsupportedError(
+    'It seems like you constructed your class using `MyClass._()`. This constructor is only meant to be used by freezed and you are not supposed to need it nor use it.\nPlease check the documentation here for more informations: https://github.com/rrousselGit/freezed#custom-getters-and-methods');
+
+/// @nodoc
+class _$TypeFormalDeclarationClauseTearOff {
+  const _$TypeFormalDeclarationClauseTearOff();
+
+  _$TypeFormalDeclarationClauseCtor call(
+      {required SwidTypeFormal swidTypeFormal}) {
+    return _$TypeFormalDeclarationClauseCtor(
+      swidTypeFormal: swidTypeFormal,
+    );
+  }
+}
+
+/// @nodoc
+const $TypeFormalDeclarationClause = _$TypeFormalDeclarationClauseTearOff();
+
+/// @nodoc
+mixin _$TypeFormalDeclarationClause {
+  SwidTypeFormal get swidTypeFormal => throw _privateConstructorUsedError;
+
+  @JsonKey(ignore: true)
+  $TypeFormalDeclarationClauseCopyWith<TypeFormalDeclarationClause>
+      get copyWith => throw _privateConstructorUsedError;
+}
+
+/// @nodoc
+abstract class $TypeFormalDeclarationClauseCopyWith<$Res> {
+  factory $TypeFormalDeclarationClauseCopyWith(
+          TypeFormalDeclarationClause value,
+          $Res Function(TypeFormalDeclarationClause) then) =
+      _$TypeFormalDeclarationClauseCopyWithImpl<$Res>;
+  $Res call({SwidTypeFormal swidTypeFormal});
+
+  $SwidTypeFormalCopyWith<$Res> get swidTypeFormal;
+}
+
+/// @nodoc
+class _$TypeFormalDeclarationClauseCopyWithImpl<$Res>
+    implements $TypeFormalDeclarationClauseCopyWith<$Res> {
+  _$TypeFormalDeclarationClauseCopyWithImpl(this._value, this._then);
+
+  final TypeFormalDeclarationClause _value;
+  // ignore: unused_field
+  final $Res Function(TypeFormalDeclarationClause) _then;
+
+  @override
+  $Res call({
+    Object? swidTypeFormal = freezed,
+  }) {
+    return _then(_value.copyWith(
+      swidTypeFormal: swidTypeFormal == freezed
+          ? _value.swidTypeFormal
+          : swidTypeFormal // ignore: cast_nullable_to_non_nullable
+              as SwidTypeFormal,
+    ));
+  }
+
+  @override
+  $SwidTypeFormalCopyWith<$Res> get swidTypeFormal {
+    return $SwidTypeFormalCopyWith<$Res>(_value.swidTypeFormal, (value) {
+      return _then(_value.copyWith(swidTypeFormal: value));
+    });
+  }
+}
+
+/// @nodoc
+abstract class _$$TypeFormalDeclarationClauseCtorCopyWith<$Res>
+    implements $TypeFormalDeclarationClauseCopyWith<$Res> {
+  factory _$$TypeFormalDeclarationClauseCtorCopyWith(
+          _$TypeFormalDeclarationClauseCtor value,
+          $Res Function(_$TypeFormalDeclarationClauseCtor) then) =
+      __$$TypeFormalDeclarationClauseCtorCopyWithImpl<$Res>;
+  @override
+  $Res call({SwidTypeFormal swidTypeFormal});
+
+  @override
+  $SwidTypeFormalCopyWith<$Res> get swidTypeFormal;
+}
+
+/// @nodoc
+class __$$TypeFormalDeclarationClauseCtorCopyWithImpl<$Res>
+    extends _$TypeFormalDeclarationClauseCopyWithImpl<$Res>
+    implements _$$TypeFormalDeclarationClauseCtorCopyWith<$Res> {
+  __$$TypeFormalDeclarationClauseCtorCopyWithImpl(
+      _$TypeFormalDeclarationClauseCtor _value,
+      $Res Function(_$TypeFormalDeclarationClauseCtor) _then)
+      : super(_value, (v) => _then(v as _$TypeFormalDeclarationClauseCtor));
+
+  @override
+  _$TypeFormalDeclarationClauseCtor get _value =>
+      super._value as _$TypeFormalDeclarationClauseCtor;
+
+  @override
+  $Res call({
+    Object? swidTypeFormal = freezed,
+  }) {
+    return _then(_$TypeFormalDeclarationClauseCtor(
+      swidTypeFormal: swidTypeFormal == freezed
+          ? _value.swidTypeFormal
+          : swidTypeFormal // ignore: cast_nullable_to_non_nullable
+              as SwidTypeFormal,
+    ));
+  }
+}
+
+/// @nodoc
+
+class _$_$TypeFormalDeclarationClauseCtor
+    extends _$TypeFormalDeclarationClauseCtor {
+  _$_$TypeFormalDeclarationClauseCtor({required this.swidTypeFormal})
+      : super._();
+
+  @override
+  final SwidTypeFormal swidTypeFormal;
+
+  @override
+  String toString() {
+    return 'TypeFormalDeclarationClause(swidTypeFormal: $swidTypeFormal)';
+  }
+
+  @override
+  bool operator ==(dynamic other) {
+    return identical(this, other) ||
+        (other is _$TypeFormalDeclarationClauseCtor &&
+            (identical(other.swidTypeFormal, swidTypeFormal) ||
+                const DeepCollectionEquality()
+                    .equals(other.swidTypeFormal, swidTypeFormal)));
+  }
+
+  @override
+  int get hashCode =>
+      runtimeType.hashCode ^
+      const DeepCollectionEquality().hash(swidTypeFormal);
+
+  @JsonKey(ignore: true)
+  @override
+  _$$TypeFormalDeclarationClauseCtorCopyWith<_$TypeFormalDeclarationClauseCtor>
+      get copyWith => __$$TypeFormalDeclarationClauseCtorCopyWithImpl<
+          _$TypeFormalDeclarationClauseCtor>(this, _$identity);
+}
+
+abstract class _$TypeFormalDeclarationClauseCtor
+    extends TypeFormalDeclarationClause {
+  factory _$TypeFormalDeclarationClauseCtor(
+          {required SwidTypeFormal swidTypeFormal}) =
+      _$_$TypeFormalDeclarationClauseCtor;
+  _$TypeFormalDeclarationClauseCtor._() : super._();
+
+  @override
+  SwidTypeFormal get swidTypeFormal => throw _privateConstructorUsedError;
+  @override
+  @JsonKey(ignore: true)
+  _$$TypeFormalDeclarationClauseCtorCopyWith<_$TypeFormalDeclarationClauseCtor>
+      get copyWith => throw _privateConstructorUsedError;
+}
