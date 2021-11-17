93f5bb1fd344cdc99e25696ee8f9b5f18a7655c8
Chris Gibb <chris.gibb@outlook.com>
2021-11-17 08:05:38 -0500

    initial super method invocations

--- lib/swid/backend/dart/dartRtManagedClassDeclaration.dart
+++ lib/swid/backend/dart/dartRtManagedClassDeclaration.dart
@@ -19,6 +19,7 @@ import 'package:code_builder/code_builder.dart'
 import 'package:collection/collection.dart' show IterableExtension;
 import 'package:dart_style/dart_style.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:hydro_sdk/swid/backend/dart/transforms/superMethodInvocation.dart';
 import 'package:tuple/tuple.dart';
 
 import 'package:hydro_sdk/swid/backend/dart/dartBindInstanceField.dart';
@@ -516,6 +517,13 @@ class DartRTManagedClassDeclaration
                           ..returns = refer(x.returnType.displayName)
                           ..body = Block.of(
                             [
+                              Code(
+                                pipeline.reduceFromTerm(
+                                  SuperMethodInvocation(
+                                    swidFunctionType: x,
+                                  ),
+                                ),
+                              ),
                               Code(
                                   "Closure closure = table[\"${transformAccessorName(swidFunctionType: transformTstlMethodNames(swidFunctionType: x)).name}\"];"),
                               Code(
---
+++ lib/swid/backend/dart/transforms/superMethodInvocation.freezed.dart
@@ -0,0 +1,163 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: unused_element, deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies, annotate_overrides
+
+part of 'superMethodInvocation.dart';
+
+// **************************************************************************
+// FreezedGenerator
+// **************************************************************************
+
+T _$identity<T>(T value) => value;
+
+final _privateConstructorUsedError = UnsupportedError(
+    'It seems like you constructed your class using `MyClass._()`. This constructor is only meant to be used by freezed and you are not supposed to need it nor use it.\nPlease check the documentation here for more informations: https://github.com/rrousselGit/freezed#custom-getters-and-methods');
+
+/// @nodoc
+class _$SuperMethodInvocationTearOff {
+  const _$SuperMethodInvocationTearOff();
+
+  _$SuperMethodInvocationCtor call(
+      {required SwidFunctionType swidFunctionType}) {
+    return _$SuperMethodInvocationCtor(
+      swidFunctionType: swidFunctionType,
+    );
+  }
+}
+
+/// @nodoc
+const $SuperMethodInvocation = _$SuperMethodInvocationTearOff();
+
+/// @nodoc
+mixin _$SuperMethodInvocation {
+  SwidFunctionType get swidFunctionType => throw _privateConstructorUsedError;
+
+  @JsonKey(ignore: true)
+  $SuperMethodInvocationCopyWith<SuperMethodInvocation> get copyWith =>
+      throw _privateConstructorUsedError;
+}
+
+/// @nodoc
+abstract class $SuperMethodInvocationCopyWith<$Res> {
+  factory $SuperMethodInvocationCopyWith(SuperMethodInvocation value,
+          $Res Function(SuperMethodInvocation) then) =
+      _$SuperMethodInvocationCopyWithImpl<$Res>;
+  $Res call({SwidFunctionType swidFunctionType});
+
+  $SwidFunctionTypeCopyWith<$Res> get swidFunctionType;
+}
+
+/// @nodoc
+class _$SuperMethodInvocationCopyWithImpl<$Res>
+    implements $SuperMethodInvocationCopyWith<$Res> {
+  _$SuperMethodInvocationCopyWithImpl(this._value, this._then);
+
+  final SuperMethodInvocation _value;
+  // ignore: unused_field
+  final $Res Function(SuperMethodInvocation) _then;
+
+  @override
+  $Res call({
+    Object? swidFunctionType = freezed,
+  }) {
+    return _then(_value.copyWith(
+      swidFunctionType: swidFunctionType == freezed
+          ? _value.swidFunctionType
+          : swidFunctionType // ignore: cast_nullable_to_non_nullable
+              as SwidFunctionType,
+    ));
+  }
+
+  @override
+  $SwidFunctionTypeCopyWith<$Res> get swidFunctionType {
+    return $SwidFunctionTypeCopyWith<$Res>(_value.swidFunctionType, (value) {
+      return _then(_value.copyWith(swidFunctionType: value));
+    });
+  }
+}
+
+/// @nodoc
+abstract class _$$SuperMethodInvocationCtorCopyWith<$Res>
+    implements $SuperMethodInvocationCopyWith<$Res> {
+  factory _$$SuperMethodInvocationCtorCopyWith(
+          _$SuperMethodInvocationCtor value,
+          $Res Function(_$SuperMethodInvocationCtor) then) =
+      __$$SuperMethodInvocationCtorCopyWithImpl<$Res>;
+  @override
+  $Res call({SwidFunctionType swidFunctionType});
+
+  @override
+  $SwidFunctionTypeCopyWith<$Res> get swidFunctionType;
+}
+
+/// @nodoc
+class __$$SuperMethodInvocationCtorCopyWithImpl<$Res>
+    extends _$SuperMethodInvocationCopyWithImpl<$Res>
+    implements _$$SuperMethodInvocationCtorCopyWith<$Res> {
+  __$$SuperMethodInvocationCtorCopyWithImpl(_$SuperMethodInvocationCtor _value,
+      $Res Function(_$SuperMethodInvocationCtor) _then)
+      : super(_value, (v) => _then(v as _$SuperMethodInvocationCtor));
+
+  @override
+  _$SuperMethodInvocationCtor get _value =>
+      super._value as _$SuperMethodInvocationCtor;
+
+  @override
+  $Res call({
+    Object? swidFunctionType = freezed,
+  }) {
+    return _then(_$SuperMethodInvocationCtor(
+      swidFunctionType: swidFunctionType == freezed
+          ? _value.swidFunctionType
+          : swidFunctionType // ignore: cast_nullable_to_non_nullable
+              as SwidFunctionType,
+    ));
+  }
+}
+
+/// @nodoc
+
+class _$_$SuperMethodInvocationCtor extends _$SuperMethodInvocationCtor {
+  _$_$SuperMethodInvocationCtor({required this.swidFunctionType}) : super._();
+
+  @override
+  final SwidFunctionType swidFunctionType;
+
+  @override
+  String toString() {
+    return 'SuperMethodInvocation(swidFunctionType: $swidFunctionType)';
+  }
+
+  @override
+  bool operator ==(dynamic other) {
+    return identical(this, other) ||
+        (other is _$SuperMethodInvocationCtor &&
+            (identical(other.swidFunctionType, swidFunctionType) ||
+                const DeepCollectionEquality()
+                    .equals(other.swidFunctionType, swidFunctionType)));
+  }
+
+  @override
+  int get hashCode =>
+      runtimeType.hashCode ^
+      const DeepCollectionEquality().hash(swidFunctionType);
+
+  @JsonKey(ignore: true)
+  @override
+  _$$SuperMethodInvocationCtorCopyWith<_$SuperMethodInvocationCtor>
+      get copyWith => __$$SuperMethodInvocationCtorCopyWithImpl<
+          _$SuperMethodInvocationCtor>(this, _$identity);
+}
+
+abstract class _$SuperMethodInvocationCtor extends SuperMethodInvocation {
+  factory _$SuperMethodInvocationCtor(
+          {required SwidFunctionType swidFunctionType}) =
+      _$_$SuperMethodInvocationCtor;
+  _$SuperMethodInvocationCtor._() : super._();
+
+  @override
+  SwidFunctionType get swidFunctionType => throw _privateConstructorUsedError;
+  @override
+  @JsonKey(ignore: true)
+  _$$SuperMethodInvocationCtorCopyWith<_$SuperMethodInvocationCtor>
+      get copyWith => throw _privateConstructorUsedError;
+}
