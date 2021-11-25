f6f0cf7584c788b4fb3ad3b705f82795953bd2b4
Chris Gibb <chris.gibb@outlook.com>
2021-11-25 17:41:28 -0500

    start rewriting class merging as transforms

---
+++ lib/swid/ir/util/swarsTermSwidClassResultMixin.dart
@@ -0,0 +1,13 @@
+import 'package:hydro_sdk/swid/ir/swidClass.dart';
+import 'package:hydro_sdk/swid/swars/iSwarsTerm.dart';
+import 'package:hydro_sdk/swid/swars/swarsTermJsonTransformableResultMixin.dart';
+import 'package:meta/meta.dart';
+
+mixin SwarsTermSwidClassResultMixin<T extends Object, U extends Object,
+        V extends SwidClass> on SwarsTermJsonTransformableResultMixin<T, U, V>
+    implements ISwarsTermJsonTransformableResult<T, U, V>, ISwarsTerm<T, U, V> {
+  @nonVirtual
+  @override
+  V termResultDeserializer(final Map<String, dynamic> json) =>
+      SwidClass.fromJson(json) as V;
+}
