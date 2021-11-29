ada27229f467e955b28f7e2ff60ccae60d869252
Chris Gibb <chris.gibb@outlook.com>
2021-11-29 18:48:46 -0500

    less convoluted test case

---
+++ test/swid/transforms/instantiateTypeArgumentsToLowestBound/parentDataWidget_test.dart
@@ -0,0 +1,140 @@
+import 'package:collection/collection.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:hydro_sdk/swid/ir/swidClass.dart';
+import 'package:hydro_sdk/swid/ir/swidDeclarationModifiers.dart';
+import 'package:hydro_sdk/swid/ir/swidFunctionType.dart';
+import 'package:hydro_sdk/swid/ir/swidInterface.dart';
+import 'package:hydro_sdk/swid/ir/swidNullabilitySuffix.dart';
+import 'package:hydro_sdk/swid/ir/swidReferenceDeclarationKind.dart';
+import 'package:hydro_sdk/swid/ir/swidType.dart';
+import 'package:hydro_sdk/swid/ir/swidTypeFormal.dart';
+import 'package:hydro_sdk/swid/ir/swidTypeFormalBound.dart';
+import 'package:hydro_sdk/swid/ir/swidTypeFormalValue.dart';
+import 'package:hydro_sdk/swid/ir/transforms/instantiateTypeArgumentsToLowestBound.dart';
+import 'package:hydro_sdk/swid/swars/cachingPipeline.dart';
+import 'package:hydro_sdk/swid/swars/pipelineNoopCacheMgr.dart';
+
+void main() {
+  LiveTestWidgetsFlutterBinding();
+  testWidgets('', (WidgetTester tester) async {
+    final ir = SwidClass(
+      name: "ParentDataWidget",
+      nullabilitySuffix: SwidNullabilitySuffix.none,
+      originalPackagePath: "package:flutter/src/widgets/framework.dart",
+      constructorType: null,
+      generativeConstructors: [],
+      factoryConstructors: [],
+      staticMethods: [],
+      methods: [
+        SwidFunctionType(
+          name: "createElement",
+          nullabilitySuffix: SwidNullabilitySuffix.none,
+          originalPackagePath: "package:flutter/src/widgets/framework.dart",
+          namedParameterTypes: {},
+          namedDefaults: {},
+          normalParameterNames: [],
+          normalParameterTypes: [],
+          optionalParameterNames: [],
+          optionalParameterTypes: [],
+          returnType: SwidType.fromSwidInterface(
+            swidInterface: SwidInterface(
+              name: "ParentDataElement<T>",
+              nullabilitySuffix: SwidNullabilitySuffix.none,
+              originalPackagePath: "package:flutter/src/widgets/framework.dart",
+              typeArguments: [
+                SwidType.fromSwidInterface(
+                  swidInterface: SwidInterface(
+                    name: "T",
+                    nullabilitySuffix: SwidNullabilitySuffix.none,
+                    originalPackagePath:
+                        "package:flutter/src/widgets/framework.dart",
+                    typeArguments: [],
+                    referenceDeclarationKind:
+                        SwidReferenceDeclarationKind.typeParameterType,
+                    declarationModifiers: SwidDeclarationModifiers.empty(),
+                  ),
+                )
+              ],
+              referenceDeclarationKind:
+                  SwidReferenceDeclarationKind.classElement,
+              declarationModifiers: SwidDeclarationModifiers.empty(),
+            ),
+          ),
+          isFactory: false,
+          typeFormals: [],
+          declarationModifiers: SwidDeclarationModifiers.empty(),
+        )
+      ],
+      staticConstFieldDeclarations: [],
+      instanceFieldDeclarations: {},
+      declarationModifiers: SwidDeclarationModifiers.empty(),
+      mixedInClasses: [],
+      implementedClasses: [],
+      isMixin: false,
+      typeFormals: [
+        SwidTypeFormal(
+          value: SwidTypeFormalValue.fromString(
+            string: "T",
+          ),
+          swidReferenceDeclarationKind:
+              SwidReferenceDeclarationKind.typeParameterType,
+          swidTypeFormalBound: SwidTypeFormalBound.fromSwidInterface(
+            swidInterface: SwidInterface(
+              name: "ParentData",
+              nullabilitySuffix: SwidNullabilitySuffix.none,
+              originalPackagePath: "package:flutter/src/rendering/object.dart",
+              typeArguments: [],
+              referenceDeclarationKind:
+                  SwidReferenceDeclarationKind.classElement,
+              declarationModifiers: SwidDeclarationModifiers.empty(),
+            ),
+          ),
+        )
+      ],
+    );
+
+    final instantiatedReturnType = SwidInterface(
+      name: "ParentDataElement<T>",
+      nullabilitySuffix: SwidNullabilitySuffix.none,
+      originalPackagePath: "package:flutter/src/widgets/framework.dart",
+      typeArguments: [
+        SwidType.fromSwidInterface(
+            swidInterface: SwidInterface(
+          name: "ParentData",
+          nullabilitySuffix: SwidNullabilitySuffix.none,
+          originalPackagePath: "package:flutter/src/rendering/object.dart",
+          typeArguments: [],
+          referenceDeclarationKind: SwidReferenceDeclarationKind.classElement,
+          declarationModifiers: SwidDeclarationModifiers.empty(),
+        ))
+      ],
+      referenceDeclarationKind: SwidReferenceDeclarationKind.classElement,
+      declarationModifiers: SwidDeclarationModifiers.empty(),
+    );
+
+    final instantiatedIr = CachingPipeline(
+      cacheMgr: const PipelineNoopCacheMgr(),
+    )
+        .reduceFromTerm(
+          InstantiateTypeArgumentsToLowestBound(
+            swidType: SwidType.fromSwidClass(
+              swidClass: ir,
+            ),
+          ),
+        )
+        .maybeWhen(
+          fromSwidClass: (val) => val,
+          orElse: () => null,
+        );
+
+    expect(
+      instantiatedIr?.methods
+          .firstWhereOrNull((x) => x.name == "createElement")
+          ?.returnType,
+      SwidType.fromSwidInterface(
+        swidInterface: instantiatedReturnType,
+      ),
+    );
+  }, tags: "swid");
+}
