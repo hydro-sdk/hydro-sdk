8f3009277d41231b8947b23572fc0845d2d4ce3b
Chris Gibb <chris.gibb@outlook.com>
2021-12-01 17:49:15 -0500

    analysis for inherited must call super annotation

---
+++ lib/swid/ir/analyses/hasInheritedMustCallSuperAnnotation.dart
@@ -0,0 +1,88 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+import 'package:hydro_sdk/swid/ir/swidClass.dart';
+import 'package:hydro_sdk/swid/ir/swidFunctionType.dart';
+import 'package:hydro_sdk/swid/swars/iSwarsPipeline.dart';
+import 'package:hydro_sdk/swid/swars/swarsAnalysisMixin.dart';
+import 'package:hydro_sdk/swid/swars/swarsTermBoolResultMixin.dart';
+import 'package:hydro_sdk/swid/swars/swarsTermResult.dart';
+import 'package:hydro_sdk/swid/util/hashComparableMixin.dart';
+import 'package:hydro_sdk/swid/util/hashKeyMixin.dart';
+
+part 'hasInheritedMustCallSuperAnnotation.freezed.dart';
+
+@freezed
+class HasInheritedMustCallSuperAnnotation
+    with
+        _$HasInheritedMustCallSuperAnnotation,
+        HashKeyMixin<HasInheritedMustCallSuperAnnotation>,
+        HashComparableMixin<HasInheritedMustCallSuperAnnotation>,
+        SwarsAnalysisMixin<
+            HasInheritedMustCallSuperAnnotation,
+            $HasInheritedMustCallSuperAnnotationCopyWith<
+                HasInheritedMustCallSuperAnnotation>,
+            bool>,
+        SwarsTermBoolResultMixin {
+  HasInheritedMustCallSuperAnnotation._();
+
+  factory HasInheritedMustCallSuperAnnotation({
+    required final SwidFunctionType swidFunctionType,
+    required final SwidClass swidClass,
+  }) = _$HasInheritedMustCallSuperAnnotationCtor;
+
+  @override
+  String get cacheGroup => "hasInheritedMustCallSuperAnnotation";
+
+  @override
+  late final List<int> hashableParts = [
+    ...swidFunctionType.hashKey.hashableParts,
+    ...swidClass.hashKey.hashableParts,
+  ];
+
+  @override
+  HasInheritedMustCallSuperAnnotation clone({
+    final SwidFunctionType? swidFunctionType,
+    final SwidClass? swidClass,
+  }) =>
+      HasInheritedMustCallSuperAnnotation(
+        swidFunctionType: swidFunctionType ?? this.swidFunctionType,
+        swidClass: swidClass ?? this.swidClass,
+      );
+
+  @override
+  ISwarsTermResult<bool> analyze({
+    required final ISwarsPipeline pipeline,
+  }) =>
+      SwarsTermResult.fromBool(
+        swidClass.methods.firstWhereOrNull(
+                  (x) =>
+                      x.name == swidFunctionType.name &&
+                      x.declarationModifiers.hasMustCallSuper,
+                ) !=
+                null ||
+            (swidClass.extendedClass != null
+                ? pipeline.reduceFromTerm(
+                    HasInheritedMustCallSuperAnnotation(
+                      swidClass: swidClass.extendedClass!,
+                      swidFunctionType: swidFunctionType,
+                    ),
+                  )
+                : false) ||
+            [
+                  ...swidClass.implementedClasses,
+                  ...swidClass.mixedInClasses,
+                ]
+                    .map(
+                      (x) => pipeline.reduceFromTerm(
+                        HasInheritedMustCallSuperAnnotation(
+                          swidClass: x,
+                          swidFunctionType: swidFunctionType,
+                        ),
+                      ),
+                    )
+                    .firstWhereOrNull(
+                      (x) => x == true,
+                    ) !=
+                null,
+      );
+}
---
+++ lib/swid/ir/analyses/hasInheritedMustCallSuperAnnotation.freezed.dart
@@ -0,0 +1,206 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: unused_element, deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies, annotate_overrides
+
+part of 'hasInheritedMustCallSuperAnnotation.dart';
+
+// **************************************************************************
+// FreezedGenerator
+// **************************************************************************
+
+T _$identity<T>(T value) => value;
+
+final _privateConstructorUsedError = UnsupportedError(
+    'It seems like you constructed your class using `MyClass._()`. This constructor is only meant to be used by freezed and you are not supposed to need it nor use it.\nPlease check the documentation here for more informations: https://github.com/rrousselGit/freezed#custom-getters-and-methods');
+
+/// @nodoc
+class _$HasInheritedMustCallSuperAnnotationTearOff {
+  const _$HasInheritedMustCallSuperAnnotationTearOff();
+
+  _$HasInheritedMustCallSuperAnnotationCtor call(
+      {required SwidFunctionType swidFunctionType,
+      required SwidClass swidClass}) {
+    return _$HasInheritedMustCallSuperAnnotationCtor(
+      swidFunctionType: swidFunctionType,
+      swidClass: swidClass,
+    );
+  }
+}
+
+/// @nodoc
+const $HasInheritedMustCallSuperAnnotation =
+    _$HasInheritedMustCallSuperAnnotationTearOff();
+
+/// @nodoc
+mixin _$HasInheritedMustCallSuperAnnotation {
+  SwidFunctionType get swidFunctionType => throw _privateConstructorUsedError;
+  SwidClass get swidClass => throw _privateConstructorUsedError;
+
+  @JsonKey(ignore: true)
+  $HasInheritedMustCallSuperAnnotationCopyWith<
+          HasInheritedMustCallSuperAnnotation>
+      get copyWith => throw _privateConstructorUsedError;
+}
+
+/// @nodoc
+abstract class $HasInheritedMustCallSuperAnnotationCopyWith<$Res> {
+  factory $HasInheritedMustCallSuperAnnotationCopyWith(
+          HasInheritedMustCallSuperAnnotation value,
+          $Res Function(HasInheritedMustCallSuperAnnotation) then) =
+      _$HasInheritedMustCallSuperAnnotationCopyWithImpl<$Res>;
+  $Res call({SwidFunctionType swidFunctionType, SwidClass swidClass});
+
+  $SwidFunctionTypeCopyWith<$Res> get swidFunctionType;
+  $SwidClassCopyWith<$Res> get swidClass;
+}
+
+/// @nodoc
+class _$HasInheritedMustCallSuperAnnotationCopyWithImpl<$Res>
+    implements $HasInheritedMustCallSuperAnnotationCopyWith<$Res> {
+  _$HasInheritedMustCallSuperAnnotationCopyWithImpl(this._value, this._then);
+
+  final HasInheritedMustCallSuperAnnotation _value;
+  // ignore: unused_field
+  final $Res Function(HasInheritedMustCallSuperAnnotation) _then;
+
+  @override
+  $Res call({
+    Object? swidFunctionType = freezed,
+    Object? swidClass = freezed,
+  }) {
+    return _then(_value.copyWith(
+      swidFunctionType: swidFunctionType == freezed
+          ? _value.swidFunctionType
+          : swidFunctionType // ignore: cast_nullable_to_non_nullable
+              as SwidFunctionType,
+      swidClass: swidClass == freezed
+          ? _value.swidClass
+          : swidClass // ignore: cast_nullable_to_non_nullable
+              as SwidClass,
+    ));
+  }
+
+  @override
+  $SwidFunctionTypeCopyWith<$Res> get swidFunctionType {
+    return $SwidFunctionTypeCopyWith<$Res>(_value.swidFunctionType, (value) {
+      return _then(_value.copyWith(swidFunctionType: value));
+    });
+  }
+
+  @override
+  $SwidClassCopyWith<$Res> get swidClass {
+    return $SwidClassCopyWith<$Res>(_value.swidClass, (value) {
+      return _then(_value.copyWith(swidClass: value));
+    });
+  }
+}
+
+/// @nodoc
+abstract class _$$HasInheritedMustCallSuperAnnotationCtorCopyWith<$Res>
+    implements $HasInheritedMustCallSuperAnnotationCopyWith<$Res> {
+  factory _$$HasInheritedMustCallSuperAnnotationCtorCopyWith(
+          _$HasInheritedMustCallSuperAnnotationCtor value,
+          $Res Function(_$HasInheritedMustCallSuperAnnotationCtor) then) =
+      __$$HasInheritedMustCallSuperAnnotationCtorCopyWithImpl<$Res>;
+  @override
+  $Res call({SwidFunctionType swidFunctionType, SwidClass swidClass});
+
+  @override
+  $SwidFunctionTypeCopyWith<$Res> get swidFunctionType;
+  @override
+  $SwidClassCopyWith<$Res> get swidClass;
+}
+
+/// @nodoc
+class __$$HasInheritedMustCallSuperAnnotationCtorCopyWithImpl<$Res>
+    extends _$HasInheritedMustCallSuperAnnotationCopyWithImpl<$Res>
+    implements _$$HasInheritedMustCallSuperAnnotationCtorCopyWith<$Res> {
+  __$$HasInheritedMustCallSuperAnnotationCtorCopyWithImpl(
+      _$HasInheritedMustCallSuperAnnotationCtor _value,
+      $Res Function(_$HasInheritedMustCallSuperAnnotationCtor) _then)
+      : super(_value,
+            (v) => _then(v as _$HasInheritedMustCallSuperAnnotationCtor));
+
+  @override
+  _$HasInheritedMustCallSuperAnnotationCtor get _value =>
+      super._value as _$HasInheritedMustCallSuperAnnotationCtor;
+
+  @override
+  $Res call({
+    Object? swidFunctionType = freezed,
+    Object? swidClass = freezed,
+  }) {
+    return _then(_$HasInheritedMustCallSuperAnnotationCtor(
+      swidFunctionType: swidFunctionType == freezed
+          ? _value.swidFunctionType
+          : swidFunctionType // ignore: cast_nullable_to_non_nullable
+              as SwidFunctionType,
+      swidClass: swidClass == freezed
+          ? _value.swidClass
+          : swidClass // ignore: cast_nullable_to_non_nullable
+              as SwidClass,
+    ));
+  }
+}
+
+/// @nodoc
+
+class _$_$HasInheritedMustCallSuperAnnotationCtor
+    extends _$HasInheritedMustCallSuperAnnotationCtor {
+  _$_$HasInheritedMustCallSuperAnnotationCtor(
+      {required this.swidFunctionType, required this.swidClass})
+      : super._();
+
+  @override
+  final SwidFunctionType swidFunctionType;
+  @override
+  final SwidClass swidClass;
+
+  @override
+  String toString() {
+    return 'HasInheritedMustCallSuperAnnotation(swidFunctionType: $swidFunctionType, swidClass: $swidClass)';
+  }
+
+  @override
+  bool operator ==(dynamic other) {
+    return identical(this, other) ||
+        (other is _$HasInheritedMustCallSuperAnnotationCtor &&
+            (identical(other.swidFunctionType, swidFunctionType) ||
+                const DeepCollectionEquality()
+                    .equals(other.swidFunctionType, swidFunctionType)) &&
+            (identical(other.swidClass, swidClass) ||
+                const DeepCollectionEquality()
+                    .equals(other.swidClass, swidClass)));
+  }
+
+  @override
+  int get hashCode =>
+      runtimeType.hashCode ^
+      const DeepCollectionEquality().hash(swidFunctionType) ^
+      const DeepCollectionEquality().hash(swidClass);
+
+  @JsonKey(ignore: true)
+  @override
+  _$$HasInheritedMustCallSuperAnnotationCtorCopyWith<
+          _$HasInheritedMustCallSuperAnnotationCtor>
+      get copyWith => __$$HasInheritedMustCallSuperAnnotationCtorCopyWithImpl<
+          _$HasInheritedMustCallSuperAnnotationCtor>(this, _$identity);
+}
+
+abstract class _$HasInheritedMustCallSuperAnnotationCtor
+    extends HasInheritedMustCallSuperAnnotation {
+  factory _$HasInheritedMustCallSuperAnnotationCtor(
+          {required SwidFunctionType swidFunctionType,
+          required SwidClass swidClass}) =
+      _$_$HasInheritedMustCallSuperAnnotationCtor;
+  _$HasInheritedMustCallSuperAnnotationCtor._() : super._();
+
+  @override
+  SwidFunctionType get swidFunctionType => throw _privateConstructorUsedError;
+  @override
+  SwidClass get swidClass => throw _privateConstructorUsedError;
+  @override
+  @JsonKey(ignore: true)
+  _$$HasInheritedMustCallSuperAnnotationCtorCopyWith<
+          _$HasInheritedMustCallSuperAnnotationCtor>
+      get copyWith => throw _privateConstructorUsedError;
+}
