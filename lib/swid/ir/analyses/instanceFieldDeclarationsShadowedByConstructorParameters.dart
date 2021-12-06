d3019e6f90aeb227ceac558cfe60c8c8f0a4e3bd
Chris Gibb <chris.gibb@outlook.com>
2021-12-06 17:29:03 -0500

    initial analysis for shadowed instance fields

---
+++ lib/swid/ir/analyses/instanceFieldDeclarationsShadowedByConstructorParameters.dart
@@ -0,0 +1,86 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+import 'package:hydro_sdk/swid/ir/swidClass.dart';
+import 'package:hydro_sdk/swid/ir/swidFunctionType.dart';
+import 'package:hydro_sdk/swid/swars/iSwarsPipeline.dart';
+import 'package:hydro_sdk/swid/swars/swarsAnalysisMixin.dart';
+import 'package:hydro_sdk/swid/swars/swarsTermJsonTransformableListResultMixin.dart';
+import 'package:hydro_sdk/swid/swars/swarsTermResult.dart';
+import 'package:hydro_sdk/swid/util/hashComparableMixin.dart';
+import 'package:hydro_sdk/swid/util/hashKeyMixin.dart';
+
+part 'instanceFieldDeclarationsShadowedByConstructorParameters.freezed.dart';
+
+@freezed
+class InstanceFieldDeclarationsShadowedByConstructorParameters
+    with
+        _$InstanceFieldDeclarationsShadowedByConstructorParameters,
+        HashKeyMixin<InstanceFieldDeclarationsShadowedByConstructorParameters>,
+        HashComparableMixin<
+            InstanceFieldDeclarationsShadowedByConstructorParameters>,
+        SwarsAnalysisMixin<
+            InstanceFieldDeclarationsShadowedByConstructorParameters,
+            $InstanceFieldDeclarationsShadowedByConstructorParametersCopyWith<
+                InstanceFieldDeclarationsShadowedByConstructorParameters>,
+            List<SwidFunctionType>>,
+        SwarsTermJsonTransformableListResultMixin {
+  InstanceFieldDeclarationsShadowedByConstructorParameters._();
+
+  factory InstanceFieldDeclarationsShadowedByConstructorParameters({
+    required final SwidClass swidClass,
+  }) = _$InstanceFieldDeclarationsShadowedByConstructorParametersCtor;
+
+  @override
+  String get cacheGroup =>
+      "instanceFieldDeclarationsShadowedByConstructorParameters";
+
+  @override
+  late final List<int> hashableParts = [
+    ...swidClass.hashKey.hashableParts,
+  ];
+
+  @override
+  InstanceFieldDeclarationsShadowedByConstructorParameters clone({
+    final SwidClass? swidClass,
+  }) =>
+      InstanceFieldDeclarationsShadowedByConstructorParameters(
+        swidClass: swidClass ?? this.swidClass,
+      );
+
+  @override
+  SwidFunctionType termResultDeserializer(final Map<String, dynamic> json) =>
+      SwidFunctionType.fromJson(json);
+
+  @override
+  ISwarsTermResult<List<SwidFunctionType>> analyze({
+    required final ISwarsPipeline pipeline,
+  }) =>
+      SwarsTermResult.fromList(
+        swidClass.constructorType != null
+            ? swidClass.instanceFieldDeclarations.entries
+                .where(
+                  (x) =>
+                      swidClass.constructorType!.normalParameterNames
+                              .firstWhereOrNull(
+                            (k) => x.key == k,
+                          ) !=
+                          null ||
+                      swidClass.constructorType!.namedParameterTypes.entries
+                              .firstWhereOrNull(
+                            (k) => x.key == k.key,
+                          ) !=
+                          null,
+                )
+                .map(
+                  (x) => x.value.when(
+                    fromSwidInterface: (_) => null,
+                    fromSwidClass: (_) => null,
+                    fromSwidDefaultFormalParameter: (_) => null,
+                    fromSwidFunctionType: (val) => val,
+                  ),
+                )
+                .whereNotNull()
+                .toList()
+            : [],
+      );
+}
---
+++ lib/swid/ir/analyses/instanceFieldDeclarationsShadowedByConstructorParameters.freezed.dart
@@ -0,0 +1,200 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: unused_element, deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies, annotate_overrides
+
+part of 'instanceFieldDeclarationsShadowedByConstructorParameters.dart';
+
+// **************************************************************************
+// FreezedGenerator
+// **************************************************************************
+
+T _$identity<T>(T value) => value;
+
+final _privateConstructorUsedError = UnsupportedError(
+    'It seems like you constructed your class using `MyClass._()`. This constructor is only meant to be used by freezed and you are not supposed to need it nor use it.\nPlease check the documentation here for more informations: https://github.com/rrousselGit/freezed#custom-getters-and-methods');
+
+/// @nodoc
+class _$InstanceFieldDeclarationsShadowedByConstructorParametersTearOff {
+  const _$InstanceFieldDeclarationsShadowedByConstructorParametersTearOff();
+
+  _$InstanceFieldDeclarationsShadowedByConstructorParametersCtor call(
+      {required SwidClass swidClass}) {
+    return _$InstanceFieldDeclarationsShadowedByConstructorParametersCtor(
+      swidClass: swidClass,
+    );
+  }
+}
+
+/// @nodoc
+const $InstanceFieldDeclarationsShadowedByConstructorParameters =
+    _$InstanceFieldDeclarationsShadowedByConstructorParametersTearOff();
+
+/// @nodoc
+mixin _$InstanceFieldDeclarationsShadowedByConstructorParameters {
+  SwidClass get swidClass => throw _privateConstructorUsedError;
+
+  @JsonKey(ignore: true)
+  $InstanceFieldDeclarationsShadowedByConstructorParametersCopyWith<
+          InstanceFieldDeclarationsShadowedByConstructorParameters>
+      get copyWith => throw _privateConstructorUsedError;
+}
+
+/// @nodoc
+abstract class $InstanceFieldDeclarationsShadowedByConstructorParametersCopyWith<
+    $Res> {
+  factory $InstanceFieldDeclarationsShadowedByConstructorParametersCopyWith(
+          InstanceFieldDeclarationsShadowedByConstructorParameters value,
+          $Res Function(
+                  InstanceFieldDeclarationsShadowedByConstructorParameters)
+              then) =
+      _$InstanceFieldDeclarationsShadowedByConstructorParametersCopyWithImpl<
+          $Res>;
+  $Res call({SwidClass swidClass});
+
+  $SwidClassCopyWith<$Res> get swidClass;
+}
+
+/// @nodoc
+class _$InstanceFieldDeclarationsShadowedByConstructorParametersCopyWithImpl<
+        $Res>
+    implements
+        $InstanceFieldDeclarationsShadowedByConstructorParametersCopyWith<
+            $Res> {
+  _$InstanceFieldDeclarationsShadowedByConstructorParametersCopyWithImpl(
+      this._value, this._then);
+
+  final InstanceFieldDeclarationsShadowedByConstructorParameters _value;
+  // ignore: unused_field
+  final $Res Function(InstanceFieldDeclarationsShadowedByConstructorParameters)
+      _then;
+
+  @override
+  $Res call({
+    Object? swidClass = freezed,
+  }) {
+    return _then(_value.copyWith(
+      swidClass: swidClass == freezed
+          ? _value.swidClass
+          : swidClass // ignore: cast_nullable_to_non_nullable
+              as SwidClass,
+    ));
+  }
+
+  @override
+  $SwidClassCopyWith<$Res> get swidClass {
+    return $SwidClassCopyWith<$Res>(_value.swidClass, (value) {
+      return _then(_value.copyWith(swidClass: value));
+    });
+  }
+}
+
+/// @nodoc
+abstract class _$$InstanceFieldDeclarationsShadowedByConstructorParametersCtorCopyWith<
+        $Res>
+    implements
+        $InstanceFieldDeclarationsShadowedByConstructorParametersCopyWith<
+            $Res> {
+  factory _$$InstanceFieldDeclarationsShadowedByConstructorParametersCtorCopyWith(
+          _$InstanceFieldDeclarationsShadowedByConstructorParametersCtor value,
+          $Res Function(
+                  _$InstanceFieldDeclarationsShadowedByConstructorParametersCtor)
+              then) =
+      __$$InstanceFieldDeclarationsShadowedByConstructorParametersCtorCopyWithImpl<
+          $Res>;
+  @override
+  $Res call({SwidClass swidClass});
+
+  @override
+  $SwidClassCopyWith<$Res> get swidClass;
+}
+
+/// @nodoc
+class __$$InstanceFieldDeclarationsShadowedByConstructorParametersCtorCopyWithImpl<
+        $Res>
+    extends _$InstanceFieldDeclarationsShadowedByConstructorParametersCopyWithImpl<
+        $Res>
+    implements
+        _$$InstanceFieldDeclarationsShadowedByConstructorParametersCtorCopyWith<
+            $Res> {
+  __$$InstanceFieldDeclarationsShadowedByConstructorParametersCtorCopyWithImpl(
+      _$InstanceFieldDeclarationsShadowedByConstructorParametersCtor _value,
+      $Res Function(
+              _$InstanceFieldDeclarationsShadowedByConstructorParametersCtor)
+          _then)
+      : super(
+            _value,
+            (v) => _then(v
+                as _$InstanceFieldDeclarationsShadowedByConstructorParametersCtor));
+
+  @override
+  _$InstanceFieldDeclarationsShadowedByConstructorParametersCtor get _value =>
+      super._value
+          as _$InstanceFieldDeclarationsShadowedByConstructorParametersCtor;
+
+  @override
+  $Res call({
+    Object? swidClass = freezed,
+  }) {
+    return _then(_$InstanceFieldDeclarationsShadowedByConstructorParametersCtor(
+      swidClass: swidClass == freezed
+          ? _value.swidClass
+          : swidClass // ignore: cast_nullable_to_non_nullable
+              as SwidClass,
+    ));
+  }
+}
+
+/// @nodoc
+
+class _$_$InstanceFieldDeclarationsShadowedByConstructorParametersCtor
+    extends _$InstanceFieldDeclarationsShadowedByConstructorParametersCtor {
+  _$_$InstanceFieldDeclarationsShadowedByConstructorParametersCtor(
+      {required this.swidClass})
+      : super._();
+
+  @override
+  final SwidClass swidClass;
+
+  @override
+  String toString() {
+    return 'InstanceFieldDeclarationsShadowedByConstructorParameters(swidClass: $swidClass)';
+  }
+
+  @override
+  bool operator ==(dynamic other) {
+    return identical(this, other) ||
+        (other is _$InstanceFieldDeclarationsShadowedByConstructorParametersCtor &&
+            (identical(other.swidClass, swidClass) ||
+                const DeepCollectionEquality()
+                    .equals(other.swidClass, swidClass)));
+  }
+
+  @override
+  int get hashCode =>
+      runtimeType.hashCode ^ const DeepCollectionEquality().hash(swidClass);
+
+  @JsonKey(ignore: true)
+  @override
+  _$$InstanceFieldDeclarationsShadowedByConstructorParametersCtorCopyWith<
+          _$InstanceFieldDeclarationsShadowedByConstructorParametersCtor>
+      get copyWith =>
+          __$$InstanceFieldDeclarationsShadowedByConstructorParametersCtorCopyWithImpl<
+                  _$InstanceFieldDeclarationsShadowedByConstructorParametersCtor>(
+              this, _$identity);
+}
+
+abstract class _$InstanceFieldDeclarationsShadowedByConstructorParametersCtor
+    extends InstanceFieldDeclarationsShadowedByConstructorParameters {
+  factory _$InstanceFieldDeclarationsShadowedByConstructorParametersCtor(
+          {required SwidClass swidClass}) =
+      _$_$InstanceFieldDeclarationsShadowedByConstructorParametersCtor;
+  _$InstanceFieldDeclarationsShadowedByConstructorParametersCtor._()
+      : super._();
+
+  @override
+  SwidClass get swidClass => throw _privateConstructorUsedError;
+  @override
+  @JsonKey(ignore: true)
+  _$$InstanceFieldDeclarationsShadowedByConstructorParametersCtorCopyWith<
+          _$InstanceFieldDeclarationsShadowedByConstructorParametersCtor>
+      get copyWith => throw _privateConstructorUsedError;
+}
