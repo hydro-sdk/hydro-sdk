6c0e6a7ca5432f84bd1e819f2a7c7698ee60b807
chgibb <chris.gibb@outlook.com>
2022-02-10 08:33:48 -0500

    add test for marking class references

--- test/swid/transforms/markClassReferences/unmodifiableSet_test.dart
+++ test/swid/transforms/markClassReferences/unmodifiableSet_test.dart
@@ -1,6 +1,5 @@
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:hydro_sdk/swid/backend/ts/tsClassConstructorImplementation.dart';
 import 'package:hydro_sdk/swid/ir/constPrimitives.dart';
 import 'package:hydro_sdk/swid/ir/swidClass.dart';
 import 'package:hydro_sdk/swid/ir/swidDeclarationModifiers.dart';
@@ -11,7 +10,6 @@ import 'package:hydro_sdk/swid/ir/swidReferenceDeclarationKind.dart';
 import 'package:hydro_sdk/swid/ir/swidType.dart';
 import 'package:hydro_sdk/swid/ir/swidTypeArgumentType.dart';
 import 'package:hydro_sdk/swid/ir/transforms/markClassReferences.dart';
-import 'package:hydro_sdk/swid/ir/transforms/rewriteReferences.dart';
 import 'package:hydro_sdk/swid/swars/cachingPipeline.dart';
 import 'package:hydro_sdk/swid/swars/pipelineNoopCacheMgr.dart';
 
@@ -106,32 +104,34 @@ void main() {
       cacheMgr: const PipelineNoopCacheMgr(),
     );
 
-    final res = TsClassConstructorImplementation(
-      swidClass: pipeline
-          .reduceFromTerm(
-            RewriteReferences(
-              swidType: pipeline.reduceFromTerm(
-                MarkClassReferences(
-                  swidType: SwidType.fromSwidClass(
-                    swidClass: ir,
-                  ),
-                ),
-              ),
+    final res = pipeline
+        .reduceFromTerm(
+          MarkClassReferences(
+            swidType: SwidType.fromSwidClass(
+              swidClass: ir,
             ),
-          )
-          .maybeWhen(
-            fromSwidClass: (val) => val,
-            orElse: () => dartUnknownClass,
           ),
+        )
+        .maybeWhen(
+          fromSwidClass: (val) => val,
+          orElse: () => dartUnknownClass,
+        );
+
+    final first = res.constructorType!.normalParameterTypes.first.maybeWhen(
+      fromSwidInterface: (val) => val,
+      orElse: () => dartUnknownInterface,
     );
 
-    expect(
-        pipeline.reduceFromTerm(
-          res,
-        ),
-        """
-public constructor(_map: IMap<E, null>){
-dart.collection._unmodifiableSet(this, _map);}
-""");
+    expect(first.typeArguments.length, 2);
+    expect(first.name, "Map<E,Null>");
+    expect(first.element, isNotNull);
+
+    final firstElement = first.element?.maybeWhen(
+      fromSwidInterfaceElement: (val) => val,
+      orElse: () => null,
+    );
+
+    expect(firstElement, isNotNull);
+    expect(firstElement?.isClassReference, true);
   }, tags: "swid");
 }
