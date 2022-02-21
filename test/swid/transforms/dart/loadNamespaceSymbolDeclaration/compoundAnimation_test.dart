8e30d7a28ea12ce562a85353a24985455c64f148
chgibb <chris.gibb@outlook.com>
2022-02-21 17:33:08 -0500

    add test for instantiating nested type arguments in namespace symbol declarations

---
+++ test/swid/transforms/dart/loadNamespaceSymbolDeclaration/compoundAnimation_test.dart
@@ -0,0 +1,222 @@
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:hydro_sdk/swid/backend/dart/dartLoadNamespaceSymbolDeclaration.dart';
+import 'package:hydro_sdk/swid/ir/swidClass.dart';
+import 'package:hydro_sdk/swid/ir/swidDeclarationModifiers.dart';
+import 'package:hydro_sdk/swid/ir/swidElement.dart';
+import 'package:hydro_sdk/swid/ir/swidFunctionType.dart';
+import 'package:hydro_sdk/swid/ir/swidInterface.dart';
+import 'package:hydro_sdk/swid/ir/swidNullabilitySuffix.dart';
+import 'package:hydro_sdk/swid/ir/swidReferenceDeclarationKind.dart';
+import 'package:hydro_sdk/swid/ir/swidType.dart';
+import 'package:hydro_sdk/swid/ir/swidTypeArgumentElement.dart';
+import 'package:hydro_sdk/swid/ir/swidTypeArgumentType.dart';
+import 'package:hydro_sdk/swid/swars/cachingPipeline.dart';
+import 'package:hydro_sdk/swid/swars/pipelineNoopCacheMgr.dart';
+
+void main() {
+  LiveTestWidgetsFlutterBinding();
+  testWidgets('', (WidgetTester tester) async {
+    final ir = SwidClass(
+      name: "CompoundAnimation",
+      nullabilitySuffix: SwidNullabilitySuffix.none,
+      originalPackagePath: "package:flutter/src/animation/animations.dart",
+      constructorType: SwidFunctionType(
+        name: "",
+        nullabilitySuffix: SwidNullabilitySuffix.none,
+        originalPackagePath: "",
+        namedParameterTypes: {
+          "first": SwidType.fromSwidInterface(
+            swidInterface: SwidInterface(
+              name: "Animation<T>",
+              nullabilitySuffix: SwidNullabilitySuffix.none,
+              originalPackagePath:
+                  "package:flutter/src/animation/animation.dart",
+              typeArguments: [
+                SwidTypeArgumentType(
+                  type: SwidType.fromSwidInterface(
+                    swidInterface: SwidInterface(
+                      name: "T",
+                      nullabilitySuffix: SwidNullabilitySuffix.none,
+                      originalPackagePath:
+                          "package:flutter/src/animation/animations.dart",
+                      typeArguments: [],
+                      referenceDeclarationKind:
+                          SwidReferenceDeclarationKind.typeParameterType,
+                      declarationModifiers: SwidDeclarationModifiers.empty(),
+                    ),
+                  ),
+                  element: SwidElement.fromSwidTypeArgumentElement(
+                    swidTypeArgumentElement: SwidTypeArgumentElement(
+                      bound: SwidType.fromSwidInterface(
+                        swidInterface: SwidInterface(
+                          name: "dynamic",
+                          nullabilitySuffix: SwidNullabilitySuffix.none,
+                          originalPackagePath: "dart:core",
+                          typeArguments: [],
+                          referenceDeclarationKind:
+                              SwidReferenceDeclarationKind.dynamicType,
+                          declarationModifiers:
+                              SwidDeclarationModifiers.empty(),
+                        ),
+                      ),
+                    ),
+                  ),
+                )
+              ],
+              referenceDeclarationKind:
+                  SwidReferenceDeclarationKind.classElement,
+              declarationModifiers: SwidDeclarationModifiers.empty(),
+            ),
+          ),
+          "next": SwidType.fromSwidInterface(
+            swidInterface: SwidInterface(
+              name: "Animation<T>",
+              nullabilitySuffix: SwidNullabilitySuffix.none,
+              originalPackagePath:
+                  "package:flutter/src/animation/animation.dart",
+              typeArguments: [
+                SwidTypeArgumentType(
+                  type: SwidType.fromSwidInterface(
+                    swidInterface: SwidInterface(
+                      name: "T",
+                      nullabilitySuffix: SwidNullabilitySuffix.none,
+                      originalPackagePath:
+                          "package:flutter/src/animation/animations.dart",
+                      typeArguments: [],
+                      referenceDeclarationKind:
+                          SwidReferenceDeclarationKind.typeParameterType,
+                      declarationModifiers: SwidDeclarationModifiers.empty(),
+                    ),
+                  ),
+                  element: SwidElement.fromSwidTypeArgumentElement(
+                    swidTypeArgumentElement: SwidTypeArgumentElement(
+                      bound: SwidType.fromSwidInterface(
+                        swidInterface: SwidInterface(
+                          name: "dynamic",
+                          nullabilitySuffix: SwidNullabilitySuffix.none,
+                          originalPackagePath: "dart:core",
+                          typeArguments: [],
+                          referenceDeclarationKind:
+                              SwidReferenceDeclarationKind.dynamicType,
+                          declarationModifiers:
+                              SwidDeclarationModifiers.empty(),
+                        ),
+                      ),
+                    ),
+                  ),
+                )
+              ],
+              referenceDeclarationKind:
+                  SwidReferenceDeclarationKind.classElement,
+              declarationModifiers: SwidDeclarationModifiers.empty(),
+            ),
+          )
+        },
+        namedDefaults: {},
+        normalParameterNames: [],
+        normalParameterTypes: [],
+        optionalParameterNames: [],
+        optionalParameterTypes: [],
+        returnType: SwidType.fromSwidInterface(
+          swidInterface: SwidInterface(
+            name: "CompoundAnimation<T>",
+            nullabilitySuffix: SwidNullabilitySuffix.none,
+            originalPackagePath:
+                "package:flutter/src/animation/animations.dart",
+            typeArguments: [
+              SwidTypeArgumentType(
+                type: SwidType.fromSwidInterface(
+                  swidInterface: SwidInterface(
+                    name: "T",
+                    nullabilitySuffix: SwidNullabilitySuffix.none,
+                    originalPackagePath:
+                        "package:flutter/src/animation/animations.dart",
+                    typeArguments: [],
+                    referenceDeclarationKind:
+                        SwidReferenceDeclarationKind.typeParameterType,
+                    declarationModifiers: SwidDeclarationModifiers.empty(),
+                  ),
+                ),
+                element: SwidElement.fromSwidTypeArgumentElement(
+                  swidTypeArgumentElement: SwidTypeArgumentElement(
+                    bound: SwidType.fromSwidInterface(
+                      swidInterface: SwidInterface(
+                        name: "dynamic",
+                        nullabilitySuffix: SwidNullabilitySuffix.none,
+                        originalPackagePath: "dart:core",
+                        typeArguments: [],
+                        referenceDeclarationKind:
+                            SwidReferenceDeclarationKind.dynamicType,
+                        declarationModifiers: SwidDeclarationModifiers.empty(),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+            referenceDeclarationKind: SwidReferenceDeclarationKind.classElement,
+            declarationModifiers: SwidDeclarationModifiers.empty(),
+          ),
+        ),
+        isFactory: false,
+        typeFormals: [],
+        declarationModifiers: SwidDeclarationModifiers.empty(),
+      ),
+      generativeConstructors: [],
+      factoryConstructors: [],
+      staticMethods: [],
+      methods: [],
+      staticConstFieldDeclarations: [],
+      instanceFieldDeclarations: {},
+      declarationModifiers: SwidDeclarationModifiers.empty(),
+      mixedInClasses: [],
+      implementedClasses: [],
+      isMixin: false,
+      typeFormals: [],
+    );
+
+    final pipeline = CachingPipeline(
+      cacheMgr: const PipelineNoopCacheMgr(),
+    );
+
+    final res = pipeline.reduceFromTerm(
+      DartLoadNamespaceSymbolDeclaration(
+        swidClass: ir,
+      ),
+    );
+
+    expect(res, """
+void loadCompoundAnimation(
+    {required _36c2.HydroState hydroState, required _36c2.HydroTable table}) {
+  table[\'compoundAnimation\'] = _36c2.makeLuaDartFunc(
+      func: (_fac9.List<_fac9.dynamic> luaCallerArguments) {
+    return [
+      RTManagedCompoundAnimation(
+          table: luaCallerArguments[0],
+          hydroState: hydroState,
+          first: _36c2.maybeUnBoxAndBuildArgument<
+                  _86ca.Animation<_fac9.dynamic>, _fac9.dynamic>(
+              luaCallerArguments.length >= 2
+                  ? luaCallerArguments[1][\'first\']
+                  : null,
+              parentState: hydroState),
+          next: _36c2.maybeUnBoxAndBuildArgument<_86ca.Animation<_fac9.dynamic>,
+                  _fac9.dynamic>(
+              luaCallerArguments.length >= 2
+                  ? luaCallerArguments[1][\'next\']
+                  : null,
+              parentState: hydroState))
+    ];
+  });
+  _36c2.registerBoxer<_77a7.CompoundAnimation>(boxer: (
+      {required _77a7.CompoundAnimation vmObject,
+      required _36c2.HydroState hydroState,
+      required _36c2.HydroTable table}) {
+    return VMManagedCompoundAnimation(
+        vmObject: vmObject, hydroState: hydroState, table: table);
+  });
+}
+""");
+  }, tags: "swid");
+}
